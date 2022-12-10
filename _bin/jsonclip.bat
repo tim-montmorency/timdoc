995a0da858812cc565cba0790a745e87c8948232
ZmotriN <zmotrin@gmail.com>
2022-12-10 07:21:52 -0500

    Add: Video Snapshot with FFMPEG

---
+++ _bin/jsonclip.bat
@@ -0,0 +1,13 @@
+@echo off
+set ffmpeg=%~dp0ffmpeg.exe
+set mediainfo=%~dp0mediainfo.exe
+
+set jpegoutput=%~d1%~p1%~n1.jpg
+set jsonoutput=%~d1%~p1%~n1.json
+
+%mediainfo% %1 --output=JSON > %jsonoutput%
+
+%ffmpeg% -hide_banner -loglevel error -ss 1 -y -i %1 -an -vframes 1 %jpegoutput% 
+
+echo Output JSON: %jsonoutput%
+echo Output JPEG: %jpegoutput%
--- _bin/videojson.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set mediainfo=%~dp0mediainfo.exe
-set output=%~d1%~p1%~n1.json
-%mediainfo% %1 --output=JSON > %output%
-echo Output file: %output%
