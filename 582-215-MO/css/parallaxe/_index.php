add2f2b5511e5a412ce3c928cab5e44c99dd13c3
ZmotriN <zmotrin@gmail.com>
2022-12-11 10:24:08 -0500

    Parallaxe Forward

---
+++ 582-215-MO/css/parallaxe/_index.php
@@ -0,0 +1,11 @@
+<?php print_header(); ?>
+
+
+<p>Une parallaxe consiste à créer un effet de profondeur en déplaçant des éléments à des vitesses différentes. Ainsi, les éléments se déplaçant plus rapidement sont perçus comme étant plus près, alors que ceux se déplaçant plus lentement sont perçus comme étant plus loin.</p>
+<p>Walt Disney fut l'un des pionniers à ce niveau grâce à son invention le MultiPlane. Cette technologie permet de déplacer à des vitesses différentes plusieurs éléments d'un dessin animé recréant ainsi l'aspect de profondeur perçu dans la vie réelle.</p>
+
+<clip src="videos/walt-disneys-multiplane.mp4"></clip>
+
+<p>Le même principe est applicable en CSS.</p>
+
+<?php print_footer(); ?>
