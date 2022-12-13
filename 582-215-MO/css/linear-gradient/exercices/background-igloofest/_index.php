b07fa321c3619db7ed3d0c15ccc7e9a9b21b5914
ZmotriN <zmotrin@gmail.com>
2022-12-13 15:20:06 -0500

    Linear-gradient exercice forward

---
+++ 582-215-MO/css/linear-gradient/exercices/background-igloofest/_index.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Background - Igloofest
+ * @icon     images/thumb.jpg
+ * @abstract Compléter la mise en page d’un carton promotionnel de l’Igloofest.
+ */
+?>
+
+<p>Pour cet exercice, vous devez compléter la mise en page d’un carton promotionnel de l’Igloofest.<br><br>Aperçu du résultat 👇</p>
+
+<img src="images/resultat.jpg">
+
+<grostitre>Matériel</grostitre>
