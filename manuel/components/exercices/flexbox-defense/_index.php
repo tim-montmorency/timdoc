f880fd3102251bda4a1dac7c7f4f3907e9035dbb
ZmotriN <zmotrin@gmail.com>
2022-12-14 18:03:17 -0500

    Manuel: Composantes

---
+++ manuel/components/exercices/flexbox-defense/_index.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Flexbox Defense - Niveaux 1 à 9
+ * @icon     images/thumb.jpg
+ * @url      http://www.flexboxdefense.com/
+ * @abstract Complète les 9 premiers niveaux de Flexbox Defense.
+ */
+?>
+<script>location.href = '<?php echo $PAGE->url; ?>';</script>
