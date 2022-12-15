a81be8b30e4e94c8c953af72e3bef5ed2c032f1b
ZmotriN <zmotrin@gmail.com>
2022-12-15 07:23:49 -0500

    Esthetique

--- manuel/installation/_index.php
+++ manuel/installation/_index.php
@@ -9,6 +9,7 @@
 
 <p>Une fois le projet cloné (<span class="inline-code">gh repo clone tim-montmorency/cours</span>), <strong><em>Timdoc</em></strong> est très simple à installer. Il suffit d'ajouter le dossier <em>_bin/</em> dans vos paths d'environnement. Voir <a href="../pxpros/">PXPros</a>.</p>
 
+<dots></dots>
 
 <grostitre>Extensions VS Code</grostitre>
 
