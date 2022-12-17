4a02b3ca5e03f55490d473bc9bc9ee0890f734e6
ZmotriN <zmotrin@gmail.com>
2022-12-17 16:02:30 -0500

    Page d'accueil

--- _index.php
+++ _index.php
@@ -1,8 +1,44 @@
 <?php 
 /**
- * @type     list
- * @title    Tim-Montmorency
- * @icon     images/icon.png
+ * @type     article
+ * @title    Timdoc
+ * @icon     shared/images/icon.png
  * @abstract Documentation Web
  */
 ?>
+
+
+<p>Timdoc est un système de documentation en ligne conçu spécifiquement pour la technique d'intégration multimédia du Collège Montmorency. Il permet de créer facilement et rapidement des exercices et notes de cours interactives grâce à une collection de composantes faites sur mesure et prêtes à utiliser</p>
+
+<dots></dots>
+
+<grostitre>Technologies</grostitre>
+
+<ul>
+    <li>HTML5, SASS/SCSS, Javascript</li>
+    <li>PXPros <em>(pré-processeur PHP)</em></li>
+    <li>VueJS 3 <em>(composantes)</em></li>
+    <li>Highlight.js <em>(surbrillance du code)</em></li>
+    <li>Video.js <em>(lecteur vidéo)</em></li>
+    <li>Howler.js <em>(lecteur audio)</em></li>
+    <li>FFMPEG <em>(transcode média)</em></li>
+    <li>MediaInfoLib <em>(informations média)</em></li>
+    <li>AudioWaveForm <em>(ondes audio en image)</em></li>
+    <li>JQ <em>(JSON en ligne de commande)</em></li>
+</ul>
+
+<dots></dots>
+
+<grostitre>Comment ça marche?</grostitre>
+<doclink spacer="true" href="manuel/">Manuel d'utilisation</doclink>
+<dots></dots>
+
+<grostitre>Contributeurs</grostitre>
+<ul>
+    <li>Simon Arnold <em>(inspiration)</em></li>
+    <li>Maxime Larrivée-Roy <em>(concepteur et développeur principal)</em></li>
+    <li>Marie-Michelle Oullet <em>(support émotionnel)</em></li>
+</ul>
+
+<dots></dots>
+
