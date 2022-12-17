3a65434d43f2229c87eb450e986457f1f72c81bf
ZmotriN <zmotrin@gmail.com>
2022-12-17 03:59:14 -0500

    Manuel: Audioplayer

---
+++ manuel/audios/tools/audacity/_index.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * @type     tool
+ * @title    Audacity
+ * @icon     images/thumb.jpg
+ * @url      https://www.audacityteam.org/
+ * @abstract Audacity est un logiciel d'enregistrement de son numérique et d'édition de sources audionumériques sous différents formats.
+ */
+?>
+<script>location.href = '<?php echo $PAGE->url; ?>';</script>
