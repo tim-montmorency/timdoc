a9759a21834548b644474fb3cad9812c99943f84
ZmotriN <zmotrin@gmail.com>
2022-12-23 11:21:00 -0500

    Darkmode: some fixes

--- manuel/tests/_index.php
+++ manuel/tests/_index.php
@@ -12,6 +12,23 @@
 <doclink href="files/nordic_metal_band.zip">Nordic Metal Band Font</doclink>
 
 
+<highlight lang="html">&lt;swiper&gt;
+    album/avant-marijuana-bibi.jpg
+    album/carey-price-gun.jpg
+&lt;/swiper&gt;
+</highlight>
+
+<swiper>
+    album/avant-marijuana-bibi.jpg
+    album/carey-price-gun.jpg
+    album/fuck-you-bernie-phoque.jpg
+    album/guy-behind-tree-coccinelle.jpg
+    album/iggypop-citrouille.jpg
+    album/steven-guilbault-arnold-cop15.jpg
+    album/pierre-yves-mcsween-fete-party-noel.jpg
+    album/manon-massee-forced-wok-noel.jpg
+</swiper>
+
 
 <swiper>
     album/avant-marijuana-bibi.jpg
