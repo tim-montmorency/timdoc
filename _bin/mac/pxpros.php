1add7e5c4f3c02be1c1e108250358a82c0a2f9ed
ZmotriN <zmotrin@gmail.com>
2023-01-05 12:06:06 -0500

    PXPros: add header docblock

--- _bin/mac/pxpros.php
+++ _bin/mac/pxpros.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * PXPros
+ *
+ * MacOS / Linux port of PXPros pre-processor.
+ *
+ * PHP version 7
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @author     Maxime Larrivée-Roy <maxime.lroy@cmontmorency.qc.ca>
+ * @copyright  2023 Collège Montmorency
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link       https://github.com/tim-montmorency/timdoc/tree/main/_bin/mac
+ */
 
 const S  = "/";
 const RN = "\n";
