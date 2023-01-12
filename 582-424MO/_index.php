c4f43bafcdce69f2a36c3c76165e6447de9725f9
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-01-12 14:26:10 -0500

    Retesté le rebuild mais je ne vois toujours pas mes sous-pages (mac)

--- 582-424MO/_index.php
+++ 582-424MO/_index.php
@@ -3,7 +3,7 @@
  * @type     list
  * @title    Optimisation Web 582-424MO
  * @icon     images/icon.png
- * @abstract Optimisation Web - Techniques d'intégration multimédia - Collège Montmorency
+ * @abstract Optimisation Web - Techniques d'intégration multimédia du Collège Montmorency
  */
 
  print_children();
