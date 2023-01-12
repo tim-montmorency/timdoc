eb94c3e2c7fb04b387f6f89270d498f09bb97d9b
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-01-11 22:31:39 -0500

    Ajout du 582-424MO de base

---
+++ 582-424MO/_index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @type     list
+ * @title    Optimisation Web 582-424MO
+ * @icon     images/icon.png
+ * @abstract Optimisation Web - Techniques d'intégration multimédia - Collège Montmorency
+ */
+
+ echo "super";
+
+ print_children();
---
+++ 582-424MO/bootstrap/_index.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * @type     article
+ * @title    Bootstrap
+ * @icon     images/icon.png
+ * @abstract Un cadriel qui vous sauve du temps!
+ */
+?>
+
+abc
