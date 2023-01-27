640cc41ba7b6b8f684f3338a1cdfc0c671564a87
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-27 18:45:36 -0500

    Intégration web: Float

---
+++ 582-215MO/css/float/exercices/rupi-kaur/_index.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Float - Rupi Kaur
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice vous devrez mettre en forme un poème de Rupi Kaur: une poétesse, écrivaine et féministe canadienne, née en Inde.
+ */
+?>
+
+<p class="spacer">Pour cet exercice vous devrez mettre en forme un poème de <a target="_blank" href="https://rupikaur.com/">Rupi Kaur</a>: une poétesse, écrivaine et féministe canadienne, née en Inde.</p>
