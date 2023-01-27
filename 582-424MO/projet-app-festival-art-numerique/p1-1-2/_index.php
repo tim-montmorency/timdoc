c05d2b7241d81e5ae044b2ba1e78879637342664
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-01-26 20:45:01 -0500

    Projet 1.1 instructions (1.1.1 et 1.1.2)

---
+++ 582-424MO/projet-app-festival-art-numerique/p1-1-2/_index.php
@@ -0,0 +1,57 @@
+ <?php
+
+  /**
+   * @type     article
+   * @title    P1.1.2 - Design et styles
+   * @icon     images/icon.svg
+   * @abstract Design de base sur les composantes Bootstrap et guide de styles
+   */
+  ?>
+
+ <grostitre>Guide de styles communs</grostitre>
+ <p>Consultez les <a href="https://getbootstrap.com/docs/5.3/examples/">modèles communs de mise en page et les exemples de composantes Bootstrap</a>. Vous pouvez aussi consulter <a href="https://themes.getbootstrap.com/">des thèmes Bootstrap </a> vous inspirer et voir à quel point c'est personalisable.</p>
+ <warning>Ce n'est pas un travail de design, alors ne passez pas 20h là dessus. Je vous offre simplement l'opportunité de personnaliser un peu le style de base de Bootstrap.</warning>
+ <checklist>
+   Aller sur un site de palette de couleurs tels que <a href="https://coolors.co/">Coolors</a> (voir le wiki pour la liste des outils de palettes de couleurs) pour choisir une palette de 5 couleurs. VOtre palette doit contenir au moins une couleur très foncée et une couleur très pâle qui servira de couleur de texte et d'arrière plan de base. Les autres couleurs vous permetttront de d'ajouter des accents de couleur au besoin (boutons, liens, navigation, footer , etc).
+   Aller sur un site de typo (polices de caractère) afin de choisir 1 à 2 typos pour le texte de votre projet (voir le wiki pour la liste des sites où aller chercher des typos gratuites). Si vous en choisissez 2, ce sera une pour les titres et une très distincte et plus lisible pour les petits caractères pour le contenu textuel courant.
+   Choisir le logo de l'événement sélectionné ou en resessiner un autre, comme un rebranding, à votre guise.
+   Créer la version favion de votre logo, qui s'affichera bien dans un format carré.
+ </checklist>
+
+
+
+ <dots></dots>
+
+ <grostitre>Design de la landing page</grostitre>
+ <p>Travail individuel pour la personne qui est désigée à la landing page. Vous pouvez bien sur collaborer pour vous assurer que vos styles s'alignent bien ensemble.</p>
+ <br>
+ <p>Aperçu du résultat pour la section héro et 2 exemples de la section fonctionnalités 👇</p>
+ <img src="../images/projet1_demo_page_entiere.png" alt="aperçu de la page entière"><br>
+ <p> À l'aide de Figma ou XD, design la section héro de la landing page. Suivre le système de grille Bootstrap (12 colonnes).</p>
+ <checklist>
+   <strong>Barre de navigation:</strong> S'en tenir au style de barre de navigation Bootstrap, avec votre typo et vos couleurs.
+   <strong>Secton héros:</strong> Styliser votre section héros avec votre typo et vos couleurs. Vous n'avez pas beaucoup de choix créatif à faire, ça doit sensiblement ressembler à la démo ci-haut.
+   <strong>Secton héros:</strong> Créer un cadre pour accueillir le design de l'app mais laisser l'intérieur vide. Il peut avoir l'air d'un cadre de téléphone mobile ou encore un cadre plus simplifié comme dans l'exemple fourni ci-haut.
+   <strong>Section fonctionnalités:</strong> Design la section fonctionnalité. Tu peux t'inspirer des 2 exemples de section fonctionnalités ci-haut ou la présenter autrement en allant t'inspirer des exemple Bootstrap. Il y aura 6 fonctioinnalités, tu peux utiliser du Lorem Ipsum pour le moment.
+   <strong>Couleurs:</strong> Assures-toi d'alterner de couleur de fond, de section en section pour bien les diviser visuellement. N'hésite pas à utiliser beaucoup de marge et d'espaces blancs. Il set déconseillé d'utiliser des couleurs très vives pour l'arrière plan d'un site. Tiens toi-en à des couleurs trèes claires ou trèes foncées et sobres. Tu pourras utiliser les couleurs plus vives de la palette de couleurs pour des éléments graphiques ponctuels dans ta page comme le menu, les liens, les boutons, les icones etc.
+ </checklist>
+
+
+ <dots></dots>
+ <grostitre>Design de l'interface de l'appli</grostitre>
+ <p>Travail individuel pour la personne qui est désigée à la l'appli. Vous pouvez bien sur collaborer pour vous assurer que vos styles s'alignent bien ensemble.</p>
+ <br>
+ <p>Aperçu du résultat pour la section héro et 2 exemples de la section fonctionnalités 👇</p>
+ <img src="../images/projet1_demo_app_entiere.png" alt="aperçu de l'appli"><br>
+
+ <p>Voici ce que vous devez designer à l'aide de Figma ou XD. Suivre le système de grille Bootstrap (12 colonnes).</p>
+ <checklist>
+   <strong>Barre de navigation:</strong> S'en tenir au style de barre de navigation Bootstrap, avec votre typo et vos couleurs. Pour l'appli, la barre contiendra l'icone du burger menu, le logo de l'événement, ainsi qu'un avatar ou une photo de l'utilisateur.
+   <strong>Barre de recherche:</strong> Quel aura le look de votre champs de formulaire telle que la barre de recherche?
+   <strong>Filtre d'oeuvres:</strong> Une série de bouton ou lien et le tout sera défilable horizontablement.
+   <strong>Section oeuvres en vedette:</strong> Une série de carte Bootstrap que tu peux personnaliser et le tout sera défilable horizontablement.
+   <strong>Section autres oeuvres:</strong> Une série de cartes Bootstrap que tu peux personnaliser et le tout sera défilable verticalement.
+ </checklist>
+
+
+ <br> <br> <br> <br> <br>
