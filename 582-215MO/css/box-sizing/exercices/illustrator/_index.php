edd9e7b17e092242e4e9ef34f609f2b259c60390
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-27 16:44:35 -0500

    Intégration web: Box-sizing Illustrator

---
+++ 582-215MO/css/box-sizing/exercices/illustrator/_index.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Box-sizing - Illustrator
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice vous devez compléter une mise en page déjà entamée d’un écran de démarrage du logiciel Adobe Illustrator.
+ */
+?>
