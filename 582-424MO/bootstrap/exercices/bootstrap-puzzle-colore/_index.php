1808778151d3f02224cea487cb0113d92d8e42ea
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-01-26 21:48:23 -0500

    exercice puzzle coloré

--- 582-424MO/bootstrap/exercices/bootstrap-puzzle-colore/_index.php
+++ 582-424MO/bootstrap/exercices/bootstrap-puzzle-colore/_index.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * @type     exercice
  * @title    Bootstrap - Puzzle coloré
@@ -7,4 +8,40 @@
  */
 ?>
 
-<p class="spacer">Pour cet exercice...</p>
+<p class="spacer">Pour cet exercice, vous devrez recréer plusieurs mise en page existante à l’aide de la grille Bootstrap</p>
+
+<grostitre>Matériel</grostitre>
+
+<doclink href="https://codepen.io/tim-momo/pen/vYadgvZ">Pen de départ</doclink>
+
+<warning>Vous n’avez qu’à modifier le HTML <em>(nul besoin de modifier le CSS ou le JavaScript)</em>.</warning>
+
+<p>Bootstrap 5 est déjà inclus dans le Pen.</p>
+
+
+<grostitre>Requis globaux </grostitre>
+
+<ul>
+    <li>Pour chaque étape, faites un&nbsp;<em>fork</em>&nbsp;du Pen d'origine afin de repartir du début à chaque&nbsp;fois.</li>
+    <li>Nommer chacune de ces copies en fonction du nom de l'étape actuelle. Par exemple, pour l'étape 1, nommer votre pen:&nbsp;<code>Bootstrap - Étape 1</code></li>
+    <li>Ajouter dans le HTML les classes Bootstrap nécessaires afin de reproduire la mise en page&nbsp;demandée.</li>
+</ul>
+
+<dots></dots>
+<grostitre>Étapes</grostitre>
+
+
+
+
+<checklist>
+Étape 1: Faites en sorte, peu importe la résolution d'écran, de recréer la grille suivante. <img src="images/bootstrap-ex01.png">
+Étape 2: Recréez l'affichage suivant en respectant le changement de mise en page apparaissant au breakpoint <code>xl</code> <img src="images/bootstrap-ex02.png">
+Étape 3: Recréez l'affichage suivant en respectant le changement de mise en page apparaissant au breakpoint <code>xl</code> <img src="images/bootstrap-ex03.png">
+Étape 4: Recréez l'affichage suivant en respectant les changements de mise en page apparaissant aux breakpoints <code>md</code> et <code>xl</code> <img src="images/bootstrap-ex04.png">
+Étape 5: Recréez l'affichage suivant en respectant les changements de mise en page apparaissant aux breakpoints <code>md</code> et <code>xl</code>. À partir de cette étape, vous aurez besoin des <a target="_blank" href="../../grille/#rangees">options d'alignement des rangées</a> afin de réussir certains exercices. <img src="images/bootstrap-ex05.png">
+Étape 6: Recréez l'affichage suivant en respectant les changements de mise en page apparaissant aux breakpoints&nbsp;<code>md</code> et <code>xl</code> <img src="images/bootstrap-ex06.png">
+Étape 7:  Recréez l'affichage suivant en respectant les changements de mise en page apparaissant aux breakpoints&nbsp;<code>md</code> et <code>xl</code> <img src="images/bootstrap-ex07.png">
+Étape 8: Recréez l'affichage suivant en respectant le changement de mise en page apparaissant au breakpoint <code>md</code>. Afin de réussir cette mise en page, vous devrez vous aventurer dans <a href="https://getbootstrap.com/docs/5.0/layout/columns/#order-classes" target="_blank">la documentation de Bootstrap</a> afin de vous renseigner à propos d'<code>order</code>. <img src="images/bootstrap-ex08.png">
+Étape 9: Recréez l'affichage suivant en respectant les changements de mise en page apparaissant aux breakpoints&nbsp;<code>md</code> et <code>xl</code>. Afin de réussir cette mise en page, vous devrez vous aventurer dans <a href="https://getbootstrap.com/docs/5.0/layout/columns/#offsetting-columns" target="_blank">la documentation de Bootstrap</a> afin de vous renseigner à propos d'<code>offset</code>. <img src="images/bootstrap-ex09.png">
+</checklist>
+
