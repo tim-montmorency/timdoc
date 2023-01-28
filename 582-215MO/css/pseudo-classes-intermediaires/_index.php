40f48dacad232e83735c032bd35a2ee18947d0f8
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-28 07:34:28 -0500

    CSS: Listing items title smaller

--- 582-215MO/css/pseudo-classes-intermediaires/_index.php
+++ 582-215MO/css/pseudo-classes-intermediaires/_index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @type     article
- * @title    Pseudo-classes avancées
+ * @title    Pseudo-classes intermédiaires
  * @icon     images/icon.png
  * @abstract first-child, last-child, only-child, nth
  */
