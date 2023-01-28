ba5872c2f457c4c479f2dc2725783adc095f148e
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-28 09:21:57 -0500

    Intégration web: Sélecteurs avancés (complete)

--- 582-215MO/css/selecteurs-avances/exercices/css-diner/_index.php
+++ 582-215MO/css/selecteurs-avances/exercices/css-diner/_index.php
@@ -1,10 +1,10 @@
 <?php 
 /**
  * @type     exercice
- * @title    CSS Diner - Niveaux 15 à 19
+ * @title    CSS Diner - Niveaux 9 à 14
  * @icon     images/thumb.jpg
  * @url      https://flukeout.github.io/
- * @abstract Complétez les niveaux 15 à 19 de CSS Diner 🍎
+ * @abstract Complétez les niveaux 9 à 14 de CSS Diner 🍎
  */
 ?>
 <script>location.href = '<?php echo $PAGE->url; ?>';</script>
