4b484b4497c583420e9334db27837870799ddcc1
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-28 07:31:45 -0500

    Intégration web: Pseudo-classes - Bouton

--- 582-215MO/css/pseudo-classes/exercices/bouton/_index.php
+++ 582-215MO/css/pseudo-classes/exercices/bouton/_index.php
@@ -17,3 +17,41 @@
 
 
 <grostitre>Matériel</grostitre>
+
+<doclink href="https://codepen.io/tim-momo/pen/NWBzVbo?editors=1100">Pen de départ</doclink>
+
+<h3>Couleurs 🎨</h3>
+
+<color>#90fcf9</color>
+<color>#b1fdfb</color>
+<color>#856084</color>
+
+<dots></dots>
+
+
+<grostitre>Requis</grostitre>
+
+<checklist>
+  Par défaut, la couleur de fond du bouton doit être turquoise.
+  Au survole, la couleur de fond du bouton doit devenir turquoise pâle afin d'inciter l'usager à cliquer dessus.
+  Lorsqu'il est cliqué, la propriété <span class="inline-code">top</span> du bouton doit avoir une valeur 15px afin de donner l'impression qu'il est enfoncé.
+  Lorsqu'il reçoit le focus, le bouton doit avoir une bordure mauve de 5px.
+</checklist>
+
+<dots></dots>
+
+
+<grostitre>Notes de cours 📚</grostitre>
+
+<doclink href="../../">Pseudo-classes</doclink>
+
+<dots></dots>
+
+
+
+
+
+
+
+
+
