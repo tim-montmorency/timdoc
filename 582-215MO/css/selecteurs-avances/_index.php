cbac338f31df7d2cd83a2e0b7980434ebed26370
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-28 08:42:41 -0500

    Intégration web: Sélecteurs avancés

---
+++ 582-215MO/css/selecteurs-avances/_index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @type     article
+ * @title    Sélecteurs avancés
+ * @icon     images/icon.png
+ * @abstract groupes, *, >, ~, +, [attr]
+ */
+?>
+
+<grostitre>Groupes</grostitre>
+
+<p>Lorsque des sélecteurs sont séparés par une virgule <incode>,</incode> les éléments correspondant à <strong>l'un ou l'autre des sélecteurs</strong> sont retournés.</p>
+
+<p>Par exemple, la règle suivante:</p>
+
+<highlight lang="css">#no1, #no2 { ... }</highlight>
+
+<p>Permet de sélectionner les éléments avec l'id <incode>#no1</incode> et <incode>#no2</incode> afin de leur appliquer un style commun.</p>
+
+
+
+
+
+
+
