95d46059e7567739a33c6c0493a2d0bf8c489b81
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-28 19:26:29 -0500

    Interactivité web: Linear-gradient

---
+++ 582-215MO/css/linear-gradient/exercices/igloofest/_index.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Linear-gradient - Igloofest
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devez compléter la mise en page d’un carton promotionnel de l’Igloofest.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous devez compléter la mise en page d’un carton promotionnel de l’<a target="_blank" href="https://igloofest.ca/fr">Igloofest</a>.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<img src="images/background-igloofest-resultat.jpg">
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+
+
+
+
+
+
+
+
+
+
+
