f0395cb6bf75d71dffa4064b3ed90eca0f77479c
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-29 09:35:11 -0500

    Intégration web: @font-face

---
+++ 582-215MO/css/font-face/tools/online-font-converter/_index.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * @type     tool
+ * @title    Online Font Converter
+ * @icon     images/thumb.jpg
+ * @url      https://onlinefontconverter.com/
+ * @abstract Convertir une fonte dans un format spécifique.
+ */
+?>
+<script>location.href = '<?php echo $PAGE->url; ?>';</script>
