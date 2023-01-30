5824b25f99e5ddd321b9470877a2a50d73050868
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-30 09:06:45 -0500

    Interactivité web: @media part 1

---
+++ 582-215MO/css/media-queries/exercices/minions/_index.php
@@ -0,0 +1,46 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Media queries - Minions
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devrez aider les Minions à se positionner adéquatement en fonction de la largeur de l’écran.
+ */
+?>
+
+
+<p class="spacer">Pour cet exercice, vous devrez aider les Minions à se positionner adéquatement en fonction de la largeur de l’écran.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="videos/media-queries-minions.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
