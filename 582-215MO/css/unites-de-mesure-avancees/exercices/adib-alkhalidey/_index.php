6650fc19efbaee55f9067e1ee6f28e32a89bb9a7
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-30 10:07:27 -0500

    Intégration web: Unités de mesure avancées

---
+++ 582-215MO/css/unites-de-mesure-avancees/exercices/adib-alkhalidey/_index.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    vw & vh - Adib Alkhalidey
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devez styliser un article concernant l’humoriste et ancien étudiant du collège Montmorency Adib Alkhalidey.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous devez styliser un article concernant l’humoriste et ancien étudiant du collège Montmorency <a target="_blank" href="https://www.facebook.com/adibalkhalidey">Adib Alkhalidey</a>.</p>
+
+<p>Aperçu du résultat 👇</p>
