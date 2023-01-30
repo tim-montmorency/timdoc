25a2e70e2f1c8ffb93e568c7514fe612847dbb65
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-29 20:40:53 -0500

    Intégration web: Box-shadow

---
+++ 582-215MO/css/box-shadow/exercices/reseaux-sociaux/_index.php
@@ -0,0 +1,61 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Box-shadow - Réseaux Sociaux
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devez compléter le CSS d’une barre de pastilles permettant d’accéder à divers réseaux sociaux.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous devez compléter le CSS d’une barre de pastilles permettant d’accéder à divers réseaux sociaux.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="videos/box-shadow-reseaux-sociaux-resultat.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+<doclink href="https://codepen.io/tim-momo/pen/xxJJzVZ?editors=1100">Pen de départ</doclink>
+
+<h3>Couleurs 🎨</h3>
+
+<color>#bbbbbb</color>
+<color>#2c3e50</color>
+<color>#4267B2</color>
+
+<br>
+
+<color>#1DA1F2</color>
+<color>#833AB4</color>
+<color>#000000</color>
+
+<p>Chaque icône que vous voyez dans le Pen de départ est en fait un caractère texte provenant de la librairie <a target="_blank" href="https://fontawesome.com/search?m=free">Font Awesome</a>. Lorsque cette librairie est chargée dans un projet, elle agit comme une police de texte permettant d'afficher certaines icônes à partir de classes CSS spécifiques.</p>
+
+<dots></dots>
+
+
+<grostitre>Requis</grostitre>
+
+<checklist>
+  Faites en sorte que chaque icône affiche une à côté de l'autre dans des pastilles blanches ⚪️ de 80x80px ayant une marge de 10px horizontalement ↔️.
+  Chaque icône doit avoir une taille de 40px, être grise et être centrée horizontalement ↔️ et verticalement ↕️ dans sa pastille.
+  Attribuez une ombre de 20px décalée de 10px vers le bas de couleur gris foncé à chaque pastille.
+  Au survole d'une pastille, faite la descendre de 5px. Ajustez aussi son ombre de sorte qu'il ne soit que de 10px et que son décalage de 5px, donnant ainsi l'impression que la pastille est enfoncée.
+  Finalement, lorsqu'une pastille est survolée, attribuez à son icône la couleur du réseau social lui correspondant.
+</checklist>
+
+<dots></dots>
+
+
+<grostitre>Notes de cours 📚</grostitre>
+
+<doclink href="../../../box-shadow/">Box-shadow</doclink>
+
+<dots></dots>
+
+
+
+
