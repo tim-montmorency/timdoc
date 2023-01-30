e39e12a0d631d68b5db8fed2a329f81116360aba
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-01-30 11:02:24 -0500

    Intégration web: SVG part 1

---
+++ 582-215MO/html/svg/_index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @type     article
+ * @title    SVG
+ * @icon     images/icon.png
+ * @abstract Scalable Vector Graphics
+ */
+?>
+
+<p class="spacer">Il n’y a jamais eu autant de résolutions d’écran différentes qu’en ce moment, ce qui amène son lot de défis. Notamment en termes de gestion des images. D’un côté, tout le monde souhaite avoir des images avec beaucoup de résolution afin qu’elles soient nettes <em>(crisp)</em>, peu importe l’écran sur lequel elles sont affichées. D’un autre, tous veulent des images légères et performantes, capables de se charger rapidement. </p>
+
+<p>Malheureusement, avec des images traditionnelles <em>(matricielles)</em>, ces deux qualités sont difficilement conciliables. Augmenter la résolution d’une image diminue systématiquement sa performance et vice versa. Heureusement, il existe une alternative: les images SVG! Ce format offre une excellente performance et est toujours net, peu importe la résolution d’affichage demandée. Ces qualités font en sorte que le format SVG est un incontournable pour les logos, icônes, illustrations, ainsi que plusieurs autres types d'images sur le web.</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
