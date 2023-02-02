8c046ebfce7e64c024045b31d8af2d68aecd5472
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-02 14:02:42 -0500

    Intégration web: Picture

---
+++ 582-215MO/html/picture/_index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @type     article
+ * @title    Picture
+ * @icon     images/icon.png
+ * @abstract Définir plusieurs sources à une image
+ */
+?>
+
+<p class="spacer">La balise <incode>&lt;picture&gt;</incode> permet de définir plusieurs sources à une balise <incode>&lt;img&gt;</incode>. Cet fonctionnalité permet de spécifier certaines sources d'images mieux adaptées à l'écran de l'utilisateur.</p>
+
+<p>Par exemple, une balise <incode>&lt;img&gt;</incode> de base est utilisée à gauche. Remarquez comment le sujet perd de son importance lorsqu'il est affiché dans un format étroit. Tandis qu'à droite, une balise <incode>&lt;picture&gt;</incode> permet de changer la source de ladite image afin d'afficher une version au cadrage plus serré sur le sujet lorsque l'écran est étroit.</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
