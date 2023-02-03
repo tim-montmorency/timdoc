454e16c45172bd225aef8fffac0ee96f8c636f43
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-02 20:31:40 -0500

    Intégration web: Filters

---
+++ 582-215MO/css/filtres/_index.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @type     article
+ * @title    Filtres
+ * @icon     images/icon.png
+ * @abstract blur, contrast, grayscale, sepia, etc.
+ */
+?>
+
+<p class="spacer">Le terme <em>“filtre”</em> est souvent associé à Instagram et autres plateformes permettant d’altérer une image en lui appliquant un ou plusieurs filtres. Cependant, il est aussi possible d’appliquer des filtres directement en CSS à un élément!</p>
+
+<dots></dots>
+
+
+<grostitre>Filtres disponibles</grostitre>
+
+<ul>
+    <li><incode>none</incode> par défaut.</li>
+    <li><incode>blur()</incode> <em>(flou)</em> Valeur acceptant toutes les unités de mesure</li>
+    <li><incode>brightness()</incode> <em>(luminosité)</em> valeur en pourcentage</li>
+    <li><incode>contrast()</incode> <em>(contraste)</em> valeur en pourcentage</li>
+    <li><incode>drop-shadow()</incode> <em>(ombre portée)</em> même format que box-shadow</li>
+    <li><incode>grayscale()</incode> <em>(niveau de gris)</em> valeur en pourcentage</li>
+    <li><incode>hue-rotate()</incode> <em>(rotation de la teinte)</em> valeur en degrés</li>
+    <li><incode>invert()</incode> <em>(inverser)</em> valeur en pourcentage</li>
+    <li><incode>opacity()</incode> <em>(opacité)</em> valeur en pourcentage</li>
+    <li><incode>saturate()</incode> <em>(saturation)</em> valeur en pourcentage</li>
+    <li><incode>sepia()</incode> <em>(sépia)</em> valeur en pourcentage</li>
+    <li>etc.</li>
+</ul>
+
+<info>Toutes les valeurs en pourcentages <em>(%)</em> peuvent être exprimées en fractions. Par exemple, <incode>50%</incode> peut être écrit <incode>0.5</incode>.</info>
+
+<dots></dots>
+
+
+<grostitre>filter</grostitre>
+
+
+
+
+
+
+
+
+
+
+
+
+
