dbc16651b2a135be6e768e471f3161d118ae9904
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-02 22:09:42 -0500

    Intégration web: Pseudo elements

--- 582-215MO/css/filtres/exercices/instagram/_index.php
+++ 582-215MO/css/filtres/exercices/instagram/_index.php
@@ -13,10 +13,43 @@
 
 <img src="images/filtres-instagram-resultat.jpg">
 
+<bravo>
+    <h5>CRÉDIT PHOTO</h5>
+    <a target="_blank" href="https://unsplash.com/photos/_M6gy9oHgII">Rayul, Unsplash</a>
+</bravo>
+
 <dots></dots>
 
 
 <grostitre>Matériel</grostitre>
+
+<doclink href="https://codepen.io/tim-momo/pen/MWBZZvy?editors=1100">Pen de départ</doclink>
+
+<dots></dots>
+
+
+<grostitre>Requis</grostitre>
+
+<checklist>
+    Utilisez les classes CSS fournies et attribuez-leur les bonnes valeurs de filter afin de recréer sur les images du bas 🎆 ⬇️ le résultat d'Instagram affiché sur l'image au-dessus ⬆️.<br>- <span class="inline-code">.filter-gingham</span><br>- <span class="inline-code">.filter-inkwell</span><br>- <span class="inline-code">.filter-amaro</span><br>- <span class="inline-code">.filter-1977</span>
+</checklist>
+
+<info>Les filtres sont affichés du plus facile vers le plus difficile.</info>
+
+<info>Théoriquement, tous les filtres Instagram peuvent être recréés en CSS.</info>
+
+<dots></dots>
+
+
+<grostitre>Notes de cours 📚</grostitre>
+
+<doclink href="../../../filtres/">Filtres</doclink>
+
+<dots></dots>
+
+
+
+
 
 
 
---
+++ 582-215MO/css/pseudo-elements/exercices/zab-cafe/_index.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Pseudo-éléments - Zab Café
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devez compléter une page de blog du torréfacteur montréalais Zab Café ☕️ en la bonifiant avec des styles de liens riches.
+ */
+?>
