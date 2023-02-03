e099579e7699196df220d9bf3ea19325390021a7
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-02-02 20:55:24 -0500

    navbar et début page carte + exercices

---
+++ 582-424MO/bootstrap/cartes/_index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @type     article
+ * @title    Cartes
+ * @icon     images/icon.png
+ * @abstract Permettent de générer rapidement et facilement des cartes
+ */
+?>
+
+
+<dots></dots>
+<highlight lang="html"></highlight>
+<warning></warning>
+<doclink href="">abc</doclink>
+
+
+
+<grostitre>Introduction</grostitre>
+<p>Les cartes Bootstrap permettent, comme leur nom l'indique, de générer rapidement et facilement des cartes comme celle-ci:</p>
+
+
+
+<exercice href="../exercices/bootstrap-nav-card"></exercice>
+
