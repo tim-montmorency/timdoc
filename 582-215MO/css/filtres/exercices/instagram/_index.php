5512a370f35ad18f4253073e191715ccffdcde61
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-02 21:28:52 -0500

    Intégration web: Exercice Instagram

---
+++ 582-215MO/css/filtres/exercices/instagram/_index.php
@@ -0,0 +1,23 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Filtres - Instagram
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devez recréer quelques-uns des filtres les plus populaires de la plateforme Instagram.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous devez recréer quelques-uns des filtres les plus populaires de la plateforme <a target="_blank" href="https://www.instagram.com/">Instagram</a>.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<img src="images/filtres-instagram-resultat.jpg">
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+
+
+
