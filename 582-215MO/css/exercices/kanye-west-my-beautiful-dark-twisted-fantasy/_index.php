8af2c8ae166d5a0598a688e0e28bc3c07b9815cb
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-03 06:53:02 -0500

    Intégration web: Exercice Kanye West

---
+++ 582-215MO/css/exercices/kanye-west-my-beautiful-dark-twisted-fantasy/_index.php
@@ -0,0 +1,69 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Kanye West - My Beautiful Dark Twisted Fantasy
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devez écrire du HTML et du CSS afin de recréer l’album My Beautiful Dark Twisted Fantasy de Kanye West.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous devez écrire du HTML et du CSS afin de recréer l’album <a target="_blank" href="https://open.spotify.com/album/20r762YmB5HeofjMCiPMLv?si=-c1pflY1SMuJl6etOLIWmw">My Beautiful Dark Twisted Fantasy de Kanye West</a>.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="videos/kanye-west-mbdtf.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+<h3>Couleurs 🎨</h3>
+
+<color>#c4343a</color>
+<color>#6c1b20</color>
+<color>#da3d48</color>
+<br>
+<color>#E7B954</color>
+<color>#D0953F</color>
+
+<h3>Médias</h3>
+
+<mediafile src="images/1.jpg">Original</mediafile>
+<mediafile src="images/2.jpg">Ballerine</mediafile>
+<mediafile src="images/3.jpg">Roi</mediafile>
+
+<dots></dots>
+
+
+<grostitre>Requis</grostitre>
+
+<checklist>
+  Téléchargez une copie des images. Placez-les dans un dossier intitulé <span class="inline-code">images</span> dans votre projet et utilisez ces images plutôt que celles en ligne.
+  Le fond de la page doit avoir un dégradé de haut en bas ⬇️ partant du rouge clair vers le rouge foncé 🔴.
+  L'album doit avoir un fond rouge, une dimension de 500x500px, être positionné à 20px du sommet de la page et être parfaitement centré horizontalement. <em>(Attention, votre page ne doit pas avoir de barre de défilement visible.)</em>
+  L'album doit avoir une ombre noire semi-transparente <em>(50% d'opacité)</em> de 50px, égale de tous les côtés de l'album.
+  Utilisez la balise <span class="inline-code">&lt;picture&gt;</span> afin d'afficher par défaut l'image <span class="inline-code">1.jpg</span>. À partir de 750px, affichez l'image <span class="inline-code">2.jpg</span> et à partir de 1150px, l'image <span class="inline-code">3.jpg</span> 🌄.
+  Ajoutez des bordures jaune de 10px au haut et à la droite de l'image, ainsi que des bordures jaunes ocre de 10px au bas et à gauche de celle-ci.
+  L'image doit avoir une largeur totale de 220px incluant ses bordures et être centrée dans l'album.
+</checklist>
+
+<dots></dots>
+
+
+<grostitre>Ambiance</grostitre>
+
+<clip src="videos/kanye-west-runaway.mp4"></clip>
+
+<dots></dots>
+
+
+<grostitre>Notes de cours 📚</grostitre>
+
+<doclink href="../../linear-gradient/">Linear-gradient</doclink>
+<doclink href="../../box-shadow/">Box-shadow</doclink>
+<doclink href="../../../html/picture/">Picture</doclink>
+
+<dots></dots>
+
+
