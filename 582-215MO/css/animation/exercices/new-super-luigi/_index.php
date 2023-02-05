eaa8e3523b194a7e26d801283c50129f7b1978ec
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-05 18:25:37 -0500

    Intégration web: Animation (part 1)

---
+++ 582-215MO/css/animation/exercices/new-super-luigi/_index.php
@@ -0,0 +1,43 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Animation - New Super Luigi
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet excercice, vous devrez recréer une scène du chateau du niveau Frosted Glacier du jeu New Super Luigi sur la Wii U.
+ */
+?>
+
+<p class="spacer">Pour cet excercice, vous devrez recréer une scène du chateau du niveau Frosted Glacier du jeu <a target="_blank" href="https://en.wikipedia.org/wiki/New_Super_Luigi_U">New Super Luigi sur la Wii U</a>.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="videos/animation-new-super-luigi.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
