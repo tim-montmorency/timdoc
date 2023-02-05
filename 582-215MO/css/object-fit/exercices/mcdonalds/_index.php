e54b63d3de5afab2f55d973369628cbabc1b2766
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-05 06:24:32 -0500

    Intégration web: Object-fit (pre commit)

---
+++ 582-215MO/css/object-fit/exercices/mcdonalds/_index.php
@@ -0,0 +1,59 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Object-fit - McDonald's
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous allez devoir recréer différentes combinaisons de menu déjeuner chez McDonald’s.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous allez devoir recréer différentes combinaisons de menu déjeuner chez McDonald’s. Pour chacune des étapes, un Pen contenant un HTML de base et les éléments visuels requis vous sera fourni. Vous n’aurez qu’à vous faire une copie <em>(fork)</em> de chacun de ces pens.</p>
+
+<p>Malheureusement, les images fournies ne sont pas dans le bon format pour effectuer la mise en page demandée. Il vous faudra utiliser la propriété CSS <incode>object-fit</incode> afin de remédier à ce problème.</p>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+<h3>Couleurs 🎨</h3>
+
+<color>#ffffff</color>
+
+<warning>
+    Il est interdis d’utiliser les propriétés CSS suivantes:<br>
+    <incode>position</incode>, <incode>margin</incode> <em>(à l’exception de sur l’élément <incode>.menu</incode>)</em> et <incode>padding</incode>.
+</warning>
+
+<dots></dots>
+
+
+<grostitre>Requis</grostitre>
+
+<checklist>
+    Chaque élément ayant la classe <span class="inline-code">.menu</span> doit avoir une largeur de 810px, une marge verticale de 20px et être centré horizontalement.
+    Les images ayant la classe <span class="inline-code">.small</span> doivent avoir une dimension de 200x200px.
+    Les images ayant la classe <span class="inline-code">.big</span> doivent avoir une dimension de 400x400px.
+    Chaque image doit avoir un fond blanc.
+</checklist>
+
+<dots></dots>
+
+
+<grostitre>Étapes</grostitre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
