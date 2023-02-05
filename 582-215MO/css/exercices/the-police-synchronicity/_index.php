c6e22098f5c4c933c112f0dccaa4aca37fd1fbcd
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-05 14:06:32 -0500

    Intégration web: The Police - Synchronicity

---
+++ 582-215MO/css/exercices/the-police-synchronicity/_index.php
@@ -0,0 +1,75 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    The Police - Synchronicity
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devez écrire du HTML et du CSS afin de recréer l’une des 36 pochettes de l’album Synchronicity du groupe The Police.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous devez écrire du HTML et du CSS afin de recréer l’une des 36 pochettes de l’album <a target="_blank" href="https://open.spotify.com/album/5W9OT0a5iZlBr83a9WMKFY?si=N7PTFjtrTiqbhO2wM3K3ew">Synchronicity du groupe The Police</a>. L’album devra être responsive en tirant profit des propriétés adéquates.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="videos/the-police-synchronicity-apercu.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+<h3>Fonts</h3>
+
+<doclink href="https://fonts.google.com/specimen/Permanent+Marker">Permanent Marker</doclink>
+
+<h3>Couleurs 🎨</h3>
+
+<color>#26C485</color>
+<color>#ce292f</color>
+<color>#2b5f8f</color>
+
+<h3>Médias</h3>
+
+<mediafile src="images/1.webp">Collage #1</mediafile>
+<mediafile src="images/2.webp">Collage #2</mediafile>
+<mediafile src="images/3.webp">Collage #3</mediafile>
+<mediafile src="images/red.webp">Rouge</mediafile>
+<mediafile src="images/yellow.webp">Jaune</mediafile>
+<mediafile src="images/blue.webp">Bleu</mediafile>
+
+<warning>Aucun dossier de départ ne vous est fourni. Vous devez écrire l’ensemble du code.</warning>
+
+<dots></dots>
+
+
+<grostitre>Requis</grostitre>
+
+<checklist>
+  La page doit avoir un fond vert 🟢, tandis que l'album doit avoir une largeur équivalente à la moitié de la page, être centré horizontalement ↔️ et avoir un fond blanc ⚪️.
+  Chaque bloc de texte doit être noir, utiliser la Goole font <em>Permanent Marker</em>, avoir une taille de police équivalente à 2% de la largeur totale de la fenêtre et une marge intérieure de tous les côtés correspondant à une demi-lettre.
+  Le nom du groupe <em>(The Police)</em> doit avoir une ombre bleue 🔵 de 2px vers le bas et la droite ↘️ alors que le titre de l'album <em>(Synchronicity)</em> doit avoir une ombre identique, mais rouge 🔴.
+  Créez trois <span class="inline-code">div</span> correspondant aux trois bandes de collage photo de l'album. Ces <span class="inline-code">div</span> doivent prendre toute la largeur de l'album et avoir un ratio de 10 de largeur par 3 de hauteur <em>(10/3)</em>.<br><br>Le simple fait d'ajouter ces <span class="inline-code">div</span> dans le flux <em>(flow)</em> de l'album devrait lui donner sa hauteur.
+  Ajoutez ensuite dans ces <span class="inline-code">div</span> une balise image contenant l'image du collage adéquat. Cette image doit prendre toute la largeur et la hauteur de sa bande.<br><br>Attention, certaines de ces images contiennent des icônes de policiers 👮‍♂️sur fond vert 🟢. Ces icônes ne doivent pas être visibles. Il faut donc ajuster les images afin que ceux-ci ne soient pas visibles et que les images ne soient pas déformées.<br><br><img src="images/the-police-icon.png">
+  Une fois vos collages affichés correctement, ajoutez dans chacune des bandes le splash de couleur qui lui est associé et faites en sorte que le collage photo soit visible au travers de celui-ci.
+</checklist>
+
+<dots></dots>
+
+
+<grostitre>Ambiance</grostitre>
+
+<clip src="videos/the-police-every-breath-you-take.mp4"></clip>
+
+<dots></dots>
+
+
+<grostitre>Notes de cours 📚</grostitre>
+
+<?php intlink("../../google-fonts/"); ?>
+<?php intlink("../../unites-de-mesure-avancees/"); ?>
+<?php intlink("../../text/"); ?>
+<?php intlink("../../aspect-ratio/"); ?>
+<?php intlink("../../object-fit/"); ?>
+<?php intlink("../../modes-de-fusion/"); ?>
+
+<dots></dots>
