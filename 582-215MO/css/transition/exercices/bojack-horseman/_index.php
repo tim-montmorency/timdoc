d8eab89aaac90f0798853c679ee77e9fac63095b
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-06 05:24:37 -0500

    Intégration web - Transition (part 1)

---
+++ 582-215MO/css/transition/exercices/bojack-horseman/_index.php
@@ -0,0 +1,46 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Transition - BoJack Horseman
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice nous allons animer les cartes présentant les personnages principaux de la série animée BoJack Horseman 🐴.
+ */
+?>
+
+<p class="spacer">Pour cet exercice nous allons animer les cartes présentant les personnages principaux de la <a target="_blank" href="https://www.imdb.com/title/tt3398228/">série animée BoJack Horseman</a> 🐴.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="videos/transition-bojackhorseman.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
