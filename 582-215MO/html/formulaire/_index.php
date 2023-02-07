480c24494934e8756d5e11127642ce03625b6bd7
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-07 05:41:57 -0500

    Intégration web: Formulaire (part 1)

---
+++ 582-215MO/html/formulaire/_index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @type     article
+ * @title    Formulaire
+ * @icon     images/icon.png
+ * @abstract text, label, form, submit
+ */
+?>
+
+<p class="spacer">Les formulaires sont un incontournable du web, ils sont présents sur la majorité des sites et sont constitués de plusieurs balise. Voyons quelques une des balises de base permettant de réaliser un formulaire.</p>
+
+<dots></dots>
+
+
+<grostitre>Input text</grostitre>
+
+<p>Génère un champ texte sur une seule ligne. Ce champ est parfait pour les informations textuelles à caractère généralistes.</p>
+
+<p>Le type <incode>text</incode> est le type par défaut de la balise <incode>&lt;input&gt;</incode>. Si aucun type n'est spécifié ou que le <incode>type</incode> utilisé n'est pas reconnu par le navigateur, celui-ci assumera automatiquement qu'il s'agit d'un champ texte.</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
