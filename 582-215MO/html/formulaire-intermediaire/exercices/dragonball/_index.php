9498978e83cde40ff6eccdbfc090c94250e2386d
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-11 15:57:32 -0500

    Intégration web: Formulaire intermédiaire (Part 1)

---
+++ 582-215MO/html/formulaire-intermediaire/exercices/dragonball/_index.php
@@ -0,0 +1,42 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Radio - Dragonball
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, nous allons modifier l’action performée par Goku, le personnage principal de Dragonball, en fonction du bouton radio sélectionné.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, nous allons modifier l’action performée par <a target="_blank" href="https://fr.wikipedia.org/wiki/Son_Goku_(Dragon_Ball)">Goku</a>, le personnage principal de Dragonball, en fonction du bouton radio sélectionné.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="videos/radio-dragonball.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
