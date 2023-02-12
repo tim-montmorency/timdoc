631d95ca0b4fdcb665fa91fa2b57a40c4ebe13e7
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-12 02:00:20 -0500

    Intégration web: Formulaire avancé (Part 1)

---
+++ 582-215MO/html/formulaire-avance/exercices/among-us/_index.php
@@ -0,0 +1,40 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Formulaire - Among Us
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice nous allons créer un formulaire permettant de générer un personnage du célèbre jeu Among Us.
+ */
+?>
+
+<p class="spacer">Pour cet exercice nous allons créer un formulaire permettant de générer un personnage du célèbre jeu <a target="_blank" href="https://store.steampowered.com/app/945360/Among_Us/">Among Us</a>.</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
