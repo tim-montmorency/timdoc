ae6cfd5b19245e00e615e85a204011a880bd71da
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-13 06:58:46 -0500

    Intégration web: Outline

---
+++ 582-215MO/css/outline/exercices/skittles/_index.php
@@ -0,0 +1,37 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Outline - Skittles
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice vous devez attribuer aux différents Skittles un outline lorsqu’ils reçoivent le focus.
+ */
+?>
+
+<p class="spacer">Pour cet exercice vous devez attribuer aux différents Skittles un outline lorsqu’ils reçoivent le focus.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="videos/outline-skittles-resultat.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
