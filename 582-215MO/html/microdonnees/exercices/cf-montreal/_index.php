af7708089f9d4f6945f05cf7f26ed9bb6c42dd3f
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-13 12:24:35 -0500

    Intégration web: Microdonnées (Part 1)

---
+++ 582-215MO/html/microdonnees/exercices/cf-montreal/_index.php
@@ -0,0 +1,33 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Microdonnées - CF Montréal
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devrez aider l’équipe marketing de La Presse à améliorer le référencement d’un article à propos du Club de Foot Montréal ⚽️.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous devrez aider l’équipe marketing de <a target="_blank" href="https://www.lapresse.ca/">La Presse</a> à améliorer le référencement d’un article à propos du <a target="_blank" href="https://www.cfmontreal.com/">Club de Foot Montréal</a> ⚽️. Votre rôle sera d’implémenter des microdonnées à l’article afin de l’aider à sortir avant ceux de la compétition sur les moteurs de recherche.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<img src="images/microdonnes-cfm.jpg">
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
