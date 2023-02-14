62c63cc439ee9bfc7f08e8d2e2a4475bc0f3374c
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-14 05:51:54 -0500

    Intégration web: Révision - South Park

---
+++ 582-215MO/css/exercices/south-park/_index.php
@@ -0,0 +1,54 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Révision - South Park
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, nous allons recréer une affiche animée de la populaire série d’animation South Park.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, nous allons recréer une affiche animée de la populaire série d’animation <a target="blank" href="https://www.imdb.com/title/tt0121955/">South Park</a>.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="videos/south-park-preview.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+<h3>Couleurs 🎨</h3>
+
+<color>#FFFFE8</color>
+
+<h3>Médias</h3>
+
+<mediafile src="images/ciel.png">Ciel</mediafile>
+<mediafile src="images/ville-et-montagnes.png">Ville et montagnes</mediafile>
+<mediafile src="images/logo.png">Logo</mediafile>
+<mediafile src="images/kenny.png">Kenny</mediafile>
+<mediafile src="images/kyle.png">Kyle</mediafile>
+<mediafile src="images/stan.png">Stan</mediafile>
+<mediafile src="images/cartman.png">Cartman</mediafile>
+<mediafile src="images/nuage.png">Nuage</mediafile>
+
+<warning>Aucun dossier de départ ne vous est fourni pour cet exercice. Vous devrez donc créer vos fichiers HTML et CSS et les lier ensemble.</warning>
+
+<dots></dots>
+
+
+<grostitre>Requis</grostitre>
+
+<checklist>
+  La page doit avoir un fond beige.
+  L'affiche doit avoir une dimension de 500x500px, un fond blanc et être parfaitement centrée horizontalement et verticalement dans la page.
+  Affichez l'image de ciel en plein centre de votre affiche et faites-la tourner sur elle-même dans le sens des aiguilles d'une montre en l'espace de 20 secondes à vitesse constante et à l'infini. Les parties du ciel dépassant de l'affiche devront être tronquées.
+  Ajoutez un div positionné au bas de l'affiche prenant toute la largeur et une demi-hauteur de celle-ci. Ajoutez-lui un fond blanc afin de simuler un sol enneigé cachant une partie de votre ciel ❄️.
+  Positionnez l'image de la ville et des montagnes fournie par-dessus le ciel et le sol enneigé de façon à ce qu'elle soit parfaitement centrée verticalement au centre de l'affiche 🌆.
+  Le logo de South Park doit être affiché en bas complètement de l'affiche et être parfaitement centré horizontalement ↔️.
+  Positionnez les personnages de la série en fonction de l'image de référence. Assurez-vous d'effectuer une rotation de -10° à Kenny et de 10° à Cartman. Chaque personnage doit monter de 10px en l'espace d'une demi-seconde lorsque survolé. Attention de garder l'angle de Kenny et Cartman lorsqu'ils sont survolés.
+  Affichez l'image du nuage à 50px du haut de l'affiche. Ce dernier devra partir à l'extérieur de l'affiche et la traverser complètement horizontalement ↔️ en l'espace de 10 secondes, à vitesse constante et à l'infini.
+</checklist>
+
+<dots></dots>
