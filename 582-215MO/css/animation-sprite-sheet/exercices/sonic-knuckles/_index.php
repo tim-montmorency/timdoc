1a19793f3ba34ddb69c9d37145428b2902f6f344
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-13 21:23:50 -0500

    Intégration web: Animation Sprite Sheet (Part 2)

---
+++ 582-215MO/css/animation-sprite-sheet/exercices/sonic-knuckles/_index.php
@@ -0,0 +1,43 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Animation Sprite Sheet - Sonic Knuckles
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devrez animer l’ancien rival et maintenant meilleur ami de Sonic: Knuckles!
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous devrez animer l’ancien rival et maintenant meilleur ami de Sonic: <a target="_blank" href="https://fr.wikipedia.org/wiki/Knuckles">Knuckles</a>!</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="videos/sprite-sheet-knuckles.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
