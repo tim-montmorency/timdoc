4cdf99826c3e435ab20867e58e2dbc2d17770136
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-02-19 23:09:45 -0500

    Intégration web: Ajustement wiki

--- 582-215MO/wiki/_index.php
+++ 582-215MO/wiki/_index.php
@@ -101,7 +101,7 @@
 </wiki-page>
 
 <!--  -->
-<wiki-page name="Cours 05A" date="2023-02-21">
+<wiki-page name="Cours 05A" >
     <?php intlink("../css/opacite/"); ?>
     <?php intlink("../css/z-index/"); ?>
     <?php intlink("../css/opacite/exercices/google-agenda/"); ?>
@@ -110,7 +110,7 @@
     <?php intlink("../css/box-shadow/"); ?>
     <?php intlink("../css/overflow/exercices/eclipses/"); ?>
     <?php intlink("../css/box-shadow/exercices/reseaux-sociaux/"); ?>
-    <h2>TP05 <em>(formatif)</em></h2>
+    <h2>TP Complémentaires <em>(formatif)</em></h2>
     <?php intlink("../css/exercices/mindless-self-indulgence-fgwsss/"); ?>
     <?php intlink("../css/exercices/band-de-garage-corpo-trash-vidange/"); ?>
 </wiki-page>
