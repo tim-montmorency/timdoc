dfe379a1b2bbd2494643ab89e67c3fcdf054470c
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-03-03 18:53:49 -0500

    gsap tools, exercices etc

---
+++ 582-424MO/gsap/exercices/gsap-stagger-dominos/_index.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @type     exercice
+ * @title    GSAP - Stagger Dominos
+ * @icon     images/thumb.jpeg
+ * @abstract Pour cet exercice, écrivez une animation GSAP permettant de faire tomber tous les dominos un à la suite de l’autre en tirant profit de la...
+ */
+?>
+
+
+   
+<p>Aperçu du résultat 👇</p>
+<clipasset src='images/gsap-voiture-turquoise.mp4'></clipasset>
+
+
+<grostitre>Matériel</grostitre>
+<doclink href="https://codepen.io/tim-momo/pen/NWLjbBj">Pen de départ</doclink>
+
+<warning>Vous n’avez qu’à ajouter du CSS et du JavaScript <em>(nul besoin de modifier le&nbsp;HTML)</em>.</warning>   
+
+<grostitre>Requis</grostitre>
+
+<checklist>
+ccc
+</checklist>
+
+<dots></dots>
+<grostitre>Notes de cours 📚</grostitre>
+
+<?php intlink("../../bases-de-lanimation/#interpolations-de-base") ?>
+
