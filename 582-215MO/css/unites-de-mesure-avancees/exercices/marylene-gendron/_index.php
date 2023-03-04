1be6731733300884bf1feac07282bfd153d01f08
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-03-04 16:43:47 -0500

    Intégration web: Marylène Gendron (Part 2)

--- 582-215MO/css/unites-de-mesure-avancees/exercices/marylene-gendron/_index.php
+++ 582-215MO/css/unites-de-mesure-avancees/exercices/marylene-gendron/_index.php
@@ -7,18 +7,18 @@
  */
 ?>
 
-<p class="spacer">Pour cet exercice, vous devez styliser un article concernant l’humoriste et ancien étudiant du collège Montmorency <a target="_blank" href="https://www.facebook.com/adibalkhalidey">Adib Alkhalidey</a>.</p>
+<p class="spacer">Pour cet exercice, vous devez styliser un article concernant l’humoriste de la relève <a target="_blank" href="https://www.facebook.com/profile.php?id=100063526361953">Marylène Gendron</a>.</p>
 
 <p>Aperçu du résultat 👇</p>
 
-<clipasset src="videos/vw-et-vh-adib-resultat.mp4"></clipasset>
+<clipasset src="videos/apercu.mp4"></clipasset>
 
 <dots></dots>
 
 
 <grostitre>Matériel</grostitre>
 
-<doclink href="https://codepen.io/tim-momo/pen/BaPONVO?editors=1100">Pen de départ</doclink>
+<doclink href="https://codepen.io/tim-momo/pen/zYJzBKe?editors=1100">Pen de départ</doclink>
 
 <h3>Couleurs 🎨</h3>
 
@@ -29,9 +29,9 @@
 
 <mediafile src="images/heros.jpg">Héros</mediafile>
 
-<p>En web, un héros est une zone contenue prenant toute la largeur d'une page et une partie importante de la hauteur de celle-ci. Il est généralement utilisé en début de page afin de définir l'ambiance désirée. Il se caractérise par une vidéo ou une image d'arrière-plan et la superposition d'un texte par-dessus celle-ci.</p>
+<br>
 
-<p>Par exemple, voyez la <a target="_blank" href="https://www.adidas.ca/fr">page d'accueil d'Adidas</a>.</p>
+<info>En web, un héros est une zone contenue prenant toute la largeur d'une page et une partie importante de la hauteur de celle-ci. Il est généralement utilisé en début de page afin de définir l'ambiance désirée. Il se caractérise par une vidéo ou une image d'arrière-plan et la superposition d'un texte par-dessus celle-ci.<br><br>Par exemple, voyez la <a target="_blank" href="https://www.adidas.ca/fr">page d'accueil d'Adidas</a>.</info>
 
 <dots></dots>
 
@@ -40,9 +40,9 @@
 
 <checklist>
     Faites en sorte que l'élément héros ait un fond noir ⚫️ et un texte de couleur blanc ⚪️. Il devra prendre toute la largeur de la page, ainsi que la moitié de la hauteur de la fenêtre.
-    Attribuez ensuite au héros l'image d'arrière-plan fournie. Assurez-vous que celle-ci couvre l'ensemble du héros et que le visage d'Adib reste bien visible, peu importe la résolution de la fenêtre.
+    Attribuez ensuite au héros l'image d'arrière-plan fournie. Assurez-vous que celle-ci couvre l'ensemble du héros et que le visage de Marylène reste bien visible, peu importe la résolution de la fenêtre.
     L'article doit être centré horizontalement ↔️, avoir une largeur correspondant à 60% de celle de la fenêtre et avoir une marge intérieure de tous les côtés équivalente à 2% de la largeur de la fenêtre.
-    Faites en sorte que la taille de base de tous les textes correspond à 2% de la largeur de la fenêtre. Ainsi, le texte devrait se redimensionner proportionnellement avec la taille de ladite fenêtre.<br><br>Bref, aucun mot ne devrait avoir à changer de ligne lorsque vous redimensionnez votre fenêtre, puisque ceux-ci se redimensionnent en fonction de la fenêtre en question.
+    Faites en sorte que la taille de base de tous les textes correspond à 2% de la largeur de la fenêtre. Ainsi, le texte devrait se redimensionner proportionnellement avec la taille de ladite fenêtre.
     Une fois l'article de base terminé, créez maintenant un <em>"blocker"</em> par-dessus celui-ci, réduisant l'accès à l'article pour les lecteurs non connectés. Pour ce faire, faites en sorte que l'élément <em>"blocker"</em> prenne toute la dimension de la page et ait une couleur de fond noir semi-transparente ⚫️.
     À l'intérieur du <em>"blocker"</em>, faites en sorte que le message ait un fond noir délavé ⚫️, un texte blanc ⚪️, une largeur équivalente à 40% de celle de la page et affichez-le dans le coin inférieur droit de l'écran ↘️.
     Finalement, empêcher la page de défiler en coupant tout ce qui dépasse de celle-ci.
@@ -56,18 +56,9 @@
 <grostitre>Notes de cours 📚</grostitre>
 
 <?php intlink("../../../unites-de-mesure-avancees/"); ?>
+<?php intlink("../../../background/"); ?>
+<?php intlink("../../../overflow/"); ?>
+<?php intlink("../../../margin/"); ?>
+<?php intlink("../../../padding/"); ?>
 
 <dots></dots>
-
-
-
-
-
-
-
-
-
-
-
-
-
