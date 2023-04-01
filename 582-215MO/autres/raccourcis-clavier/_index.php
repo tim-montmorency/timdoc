9e5940ace307494fd21ec068487027d6f73c92ca
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-04-01 18:43:40 -0400

    Inline favicon

---
+++ 582-215MO/autres/raccourcis-clavier/_index.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @type     article
+ * @title    Raccourcis clavier
+ * @icon     images/icon.png
+ * @abstract Absolue, relatif
+ */
+?>
+
+<!-- <p class="spacer">Peu importe le langage utilisé <em>(HTML, CSS, JavaScript)</em>, la façon d'écrire un chemin vers un fichier externe <em>(image, police, stylesheet ou JS)</em> reste la même.</p> -->
+
+<grostitre>Navigation</grostitre>
+
+<h3>Par mot</h3>
+<clipasset src="./videos/navigation-par-mots.mp4"></clipasset>
+<img src="./images/navigation-par-mots.webp" alt="">
+<br>
+
+<h3>Début et fin de ligne</h3>
+<clipasset src="./videos/navigation-ligne-debut-fin.mp4"></clipasset>
+<img src="./images/navigation-ligne-debut-fin.webp" alt="">
+<br>
+
+<h3>Début et fin de fichier</h3>
+<clipasset src="./videos/navigation-fichier-debut-fin.mp4"></clipasset>
+<img src="./images/navigation-fichier-debut-fin.webp" alt="">
+
+<dots></dots>
+
+
+<grostitre>Selection</grostitre>
+
+<h3>Par mot</h3>
+<clipasset src="./videos/selection-par-mots.mp4"></clipasset>
+<img src="./images/selection-par-mots.webp" alt="">
+<br>
+
+<h3>Début et fin de ligne</h3>
+<clipasset src="./videos/selection-ligne-debut-fin.mp4"></clipasset>
+<img src="./images/selection-ligne-debut-fin.webp" alt="">
+<br>
+
+<h3>Début et fin de fichier</h3>
+<clipasset src="./videos/selection-fichier-debut-fin.mp4"></clipasset>
+<img src="./images/selection-fichier-debut-fin.webp" alt="">
+
+<dots></dots>
