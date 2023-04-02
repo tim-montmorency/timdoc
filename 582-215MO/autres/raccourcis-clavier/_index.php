dfc574e43d002d6023fa25efe72eb2b694fdf2c6
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-04-02 12:20:59 -0400

    Raccourcis Clavier

--- 582-215MO/autres/raccourcis-clavier/_index.php
+++ 582-215MO/autres/raccourcis-clavier/_index.php
@@ -3,7 +3,7 @@
  * @type     article
  * @title    Raccourcis clavier
  * @icon     images/icon.png
- * @abstract Absolue, relatif
+ * @abstract Navigation et sélection de code
  */
 ?>
 
@@ -12,16 +12,28 @@
 <grostitre>Navigation</grostitre>
 
 <h3>Par mot</h3>
+<ul>
+    <li>Mot suivant: <incode>CTRL</incode> + <incode>Flèche droite</incode></li>
+    <li>Mot précédent: <incode>CTRL</incode> + <incode>Flèche gauche</incode></li>
+</ul>
 <clipasset src="./videos/navigation-par-mots.mp4"></clipasset>
 <img src="./images/navigation-par-mots.webp" alt="">
 <br>
 
 <h3>Début et fin de ligne</h3>
+<ul>
+    <li>Début de ligne: <incode>Home</incode> <em>(Origine)</em></li>
+    <li>Fin de ligne: <incode>End</incode> <em>(Fin)</em></li>
+</ul>
 <clipasset src="./videos/navigation-ligne-debut-fin.mp4"></clipasset>
 <img src="./images/navigation-ligne-debut-fin.webp" alt="">
 <br>
 
 <h3>Début et fin de fichier</h3>
+<ul>
+    <li>Début de fichier: <incode>CTRL</incode> + <incode>Home</incode> <em>(Origine)</em></li>
+    <li>Fin de fichier: <incode>CTRL</incode> + <incode>End</incode> <em>(Fin)</em></li>
+</ul>
 <clipasset src="./videos/navigation-fichier-debut-fin.mp4"></clipasset>
 <img src="./images/navigation-fichier-debut-fin.webp" alt="">
 
@@ -31,17 +43,59 @@
 <grostitre>Selection</grostitre>
 
 <h3>Par mot</h3>
+<ul>
+    <li>Mot suivant: <incode>CTRL</incode> + <incode>SHIFT</incode> <em>(Maj.)</em> + <incode>Flèche droite</incode></li>
+    <li>Mot précédent: <incode>CTRL</incode> + <incode>SHIFT</incode> <em>(Maj.)</em> + <incode>Flèche gauche</incode></li>
+</ul>
 <clipasset src="./videos/selection-par-mots.mp4"></clipasset>
 <img src="./images/selection-par-mots.webp" alt="">
 <br>
 
 <h3>Début et fin de ligne</h3>
+<ul>
+    <li>Début de ligne: <incode>SHIFT</incode> <em>(Maj.)</em> + <incode>Home</incode> <em>(Origine)</em></li>
+    <li>Fin de ligne: <incode>SHIFT</incode> <em>(Maj.)</em> + <incode>End</incode> <em>(Fin)</em></li>
+</ul>
 <clipasset src="./videos/selection-ligne-debut-fin.mp4"></clipasset>
 <img src="./images/selection-ligne-debut-fin.webp" alt="">
 <br>
 
 <h3>Début et fin de fichier</h3>
+<ul>
+    <li>Début de fichier: <incode>SHIFT</incode> <em>(Maj.)</em> + <incode>CTRL</incode> + <incode>Home</incode> <em>(Origine)</em></li>
+    <li>Fin de fichier: <incode>SHIFT</incode> <em>(Maj.)</em> + <incode>CTRL</incode> + <incode>End</incode> <em>(Fin)</em></li>
+</ul>
 <clipasset src="./videos/selection-fichier-debut-fin.mp4"></clipasset>
 <img src="./images/selection-fichier-debut-fin.webp" alt="">
 
 <dots></dots>
+
+
+<grostitre>Fenêtres</grostitre>
+
+<h3>Changer de fenêtre</h3>
+<ul>
+    <li>Fenêtre précédente: <incode>ALT</incode> + <incode>TAB</incode></li>
+</ul>
+<clipasset src="./videos/fenetre-changement-fenetre.mp4"></clipasset>
+<img src="./images/fenetre-changement-fenetre.webp" alt="">
+<br>
+
+<h3>Changer d'onglet</h3>
+<ul>
+    <li>Onglet précédent: <incode>CTRL</incode> + <incode>TAB</incode></li>
+</ul>
+<clipasset src="./videos/fenetre-changement-tab.mp4"></clipasset>
+<img src="./images/fenetre-changement-tab.webp" alt="">
+
+<dots></dots>
+
+
+
+
+
+
+
+
+
+
