4c223894311cc94078b53bcd8191a1064766f94b
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-04-13 21:28:14 -0400

    Masquage (part 1)

---
+++ 582-215MO/css/masquage/_index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @type     article
+ * @title    Masquage
+ * @icon     images/icon.png
+ * @abstract clip-path, mask-image
+ */
+?>
+
+<p class="spacer">Comme dans Photoshop, il est possible d'empêcher une portion d'un élément d'être affichée en définissant une région de rognage. Seule une zone spécifique de l'élément sera affichée. Pour ce faire, il existes deux méthodes: utiliser une fonction ou une image de référence.</p>
+
+<dots></dots>
+
+
+<grostitre>clip-path</grostitre>
+
+<p>La propriété <incode>clip-path</incode> permet d'utiliser une fonction comme méthode de masquage.</p>
+
+<p>Exemples:</p>
+
+
+<codepen id="XWxXPyz" tab="result"></codepen>
+
+<doclink href="https://developer.mozilla.org/fr/docs/Web/CSS/clip-path">clip-path</doclink>
+<doclink href="https://www.w3schools.com/cssref/css3_pr_clip-path.php">clip-path</doclink>
+
+<dots></dots>
+
+
