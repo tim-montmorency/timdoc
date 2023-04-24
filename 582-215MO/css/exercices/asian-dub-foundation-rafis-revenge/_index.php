d0687cd502231dbcbd4bb9fd8157ff2d5092da9b
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-04-24 13:46:34 -0400

    Wiki + Asian Dub Foundation

---
+++ 582-215MO/css/exercices/asian-dub-foundation-rafis-revenge/_index.php
@@ -0,0 +1,50 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Asian Dub Foundation - Rafi's Revenge
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devez écrire du CSS afin de recréer la pochette de l’album "Rafi's Revenge" du groupe de rap jungle Asian Dub Foundation.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous devez écrire du CSS afin de recréer la pochette de l’album <a target="_blank" href="https://open.spotify.com/album/6bI22G1Vu3g3Qji8Xs8pjx">"Rafi's Revenge"</a> du groupe de rap jungle <a target="_blank" href="https://fr.wikipedia.org/wiki/Asian_Dub_Foundation">Asian Dub Foundation</a>.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="./videos/apercu.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+<doclink href="./asian-dub-foundation-rafis-revenge.zip">Dossier de départ</doclink>
+
+<h3>Polices d'écriture 🚓</h3>
+
+<doclink href="./HelveticaCompressedRegular.zip">Helvetica Compressed Regular</doclink>
+<br>
+<doclink href="./STSharpiePrint.zip">ST Sharpie Print</doclink>
+
+<h3>Couleurs 🎨</h3>
+
+<color>#091b29</color>
+<color>#f2f1eb</color>
+<color>#444444</color>
+
+<h3>Médias 📷</h3>
+
+<mediafile src="./images/bg.webp">Arrière-plan</mediafile>
+<mediafile src="./images/plug.webp">Branchement</mediafile>
+
+
+
+
+
+
+
+
+
+
+
+
