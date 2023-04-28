9bbbbac30c1642f76be52af4d68fd7e9b928bfa9
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-04-28 00:02:15 -0400

    parallax interm

---
+++ 582-424MO/gsap/exercices/gsap-scrollTrigger-parallax2/_index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @type     exercice
+ * @title    GSAP Parallax - Sections et textes
+ * @icon     images/thumb-parallax-sections.jpg
+ * @abstract Pour cet exercice, nous allons exercer au moins 3 effets parallaxe sur différentes sections de la page.
+ */
+?>
+
+<p>Pour cet exercice, nous allons exercer au moins 3 effets parallaxe sur différentes sections de la page.</p>
+   
+<p>Aperçu du résultat 👇</p>
+<clipasset src='images/'></clipasset>
+
+<grostitre>Matériel</grostitre>
+<doclink href="https://codepen.io/tim-momo/pen/">Pen de départ</doclink>
+
+<warning>Vous n’avez qu’à ajouter JavaScript <em>(de pas modifier le&nbsp;HTML ou le CSS)</em>. Par contre, il est fortement conseillé de jeter un oeil au CSS et HTML pour comprendre la structure.</warning>  
+
+<p>Les librairies GSAP et ScrollTrigger sont déjà inclues dans le Pen, vous laissant ainsi vous concentrer sur l'animation GSAP à réaliser.</p>
+
+<grostitre>Requis</grostitre>
+
+<checklist>
+À suivre...
+</checklist>
+
+
+<grostitre>Notes de cours 📚</grostitre>
+<?php intlink("../../parallaxe/") ?>
+<?php intlink("../../scrolltrigger/") ?>
+<?php intlink("../../scrolltrigger-intermediaire/") ?>
+<br>
+<doclink href='https://smnarnold.com/cours/javascript/gestions-des-attributs#getAttribute()'>Récupérer la valeur d'un attribut d'une balise HTML</doclink>
+<doclink href='https://smnarnold.com/cours/css/parallax'>Qu'est-ce que le parallaxe</doclink>
+
+
+
+
+
--- 582-424MO/gsap/parallaxe-intermediaire/_index.php
+++ 582-424MO/gsap/parallaxe-intermediaire/_index.php
@@ -9,7 +9,15 @@
 ?>
 <grostitre>Animation de plusieurs couches</grostitre>
 
+<p>Pour animer en parallaxe, vous le savez, les couches les plus près de nous vont bouger plus rapidement que les couches plus loin de nous.</p>
+<p>Si vous positionnez vos éléments en <code>position: absolute</code> dans son conteur qui a lui, une <code>position: relative</code>, 'oubliez pas de leur accorder un <code>z-index</code> en fonction de l'ordre des couches que vous souhaitez établir (plus le z-index est grand, plus la couche est par dessus).</p>
+<dots></dots>
+<p>Avec GSAP et scrollTrigger, par la suite, il vous suffira d'ajouter un coéficient de vitesse au mouvement désiré. </p>
+<p>Par exemple si un élément bouge vers la droite, sur l'axe des x, 100px. Pour le mouvement de l'arrière plan, vous pourriez multiplier ce 100px par un coéficient moindre, par exemple 0.5. Ainsi l'arrière-plan bougerait 2x moins vite que l'élément en avant-plan. </p>
+
+
 <exercice href="../exercices/gsap-scrollTrigger-parallax/"></exercice>     
+<exercice href="../exercices/gsap-scrollTrigger-parallax2/"></exercice>   
 
 <script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
 
