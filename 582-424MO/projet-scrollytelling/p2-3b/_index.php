5996a58410b1cf1c2a2990f7d7ec6a37be17e5af
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-05-04 18:49:53 -0400

    p2.3b, p2.3c

---
+++ 582-424MO/projet-scrollytelling/p2-3b/_index.php
@@ -0,0 +1,13 @@
+ <?php
+
+   /**
+    * @type     article
+    * @title    P2.3b - Morphage
+    * @icon     images/icon.svg
+    * @abstract Animations d'un morphage entre 2 formes simples
+   * @index    10
+    */
+   ?>
+
+
+
---
+++ 582-424MO/projet-scrollytelling/p2-3c/_index.php
@@ -0,0 +1,12 @@
+ <?php
+
+   /**
+    * @type     article
+    * @title    P2.3c - Optimisation Web
+    * @icon     images/icon.svg
+    * @abstract Optimisation des performances Web
+   * @index    6
+    */
+   ?>
+
+
