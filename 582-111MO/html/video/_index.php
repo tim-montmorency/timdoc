0056da4a90bbb6520789def62d7a55b96e1dcd4a
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2023-06-27 13:16:26 -0400

    Html modifs

---
+++ 582-111MO/html/video/_index.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @type     article
+ * @title    Video
+ * @icon     images/icon.png
+ * @abstract q, blockquote, cite
+ */
+?>
+
+<p class="spacer">Différentes balises HTML permettent de démarquer une citation selon le contexte. Voyons les plus courantes.</p>
+
+<grostitre>Q</grostitre>
+
+<p>La balise <incode>&lt;q&gt;</incode> permet d'insérer dans un texte une courte citation sans créer un nouveau bloc de contenu.</p>
+
+<codepen id="oNPvxXX" tab="html,result" height="340"></codepen>
+
+<p>Par défaut, la majorité des navigateurs ajoutent des guillemets autour de la balise <incode>&lt;q&gt;</incode> afin de la faire ressortir du texte régulier.</p>
+
+<doclink href="https://developer.mozilla.org/fr/docs/Web/HTML/Element/q">q</doclink>
+<doclink href="https://www.w3schools.com/tags/tag_q.asp">q</doclink>
+
+<dots></dots>
+
+
+<grostitre>Blockquote</grostitre>
+
+<p>La balise <incode>&lt;blockquote&gt;</incode> permet de définir un bloc de citation relativement long.</p>
+
+<codepen id="yLxBOYx" tab="html,result" height="340"></codepen>
+
+<p>Par défaut, la majorité des navigateurs indentent les <incode>blockquote</incode> à l'aide d'un <incode>margin-left</incode> afin de faire ressortir leurs contenus du texte régulier.</p>
+
+<doclink href="https://developer.mozilla.org/fr/docs/Web/HTML/Element/blockquote">blockquote</doclink>
+<doclink href="https://www.w3schools.com/tags/tag_blockquote.asp">blockquote</doclink>
+
+<dots></dots>
+
+
+<grostitre>Cite</grostitre>
+
+<p>La balise <incode>&lt;cite&gt;</incode> permet de spécifier la source d'une citation.</p>
+
+<codepen id="BaOBKKw" tab="html,result" height="340"></codepen>
+
+<p>Généralement, cette balise est inclus dans une balise <incode>&lt;footer&gt;</incode> pour indiquer qu'il s'agit en quelque sorte da la note de pied de page de la citation.</p>
+
+<p>Par défaut, la majorité des navigateurs mettent en italique le contenu des balises <incode>&lt;cite&gt;</incode> afin de les différencier de la citation elle-même.</p>
+
+<doclink href="https://developer.mozilla.org/fr/docs/Web/HTML/Element/cite">cite</doclink>
+<doclink href="https://www.w3schools.com/tags/tag_cite.asp">cite</doclink>
+
+<info>N’oubliez pas, malgré que ces balises viennent avec des styles préétablis, il est possible de les modifier aisément en CSS pour leur donner apparence tout autre!</info>
+
+<dots></dots>
