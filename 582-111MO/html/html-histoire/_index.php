5e602fb19a7fe7a758da35957535f915da35767d
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2023-06-28 12:41:01 -0400

    Histoire

---
+++ 582-111MO/html/html-histoire/_index.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * @type     article
+ * @title    HTML - Histoire
+ * @icon     images/icon.png
+ * @abstract L'histoire du html
+ */
+?>
+
+<grostitre>HTML</grostitre>
+
+<h3> Signifie <em>"HyperText Markup Language"</em></h3>
+
+<p>C’est un langage qui permet de composer des pages web.</p>
+
+<p>Il représente en fait une des trois technologies de base que tout développeur web doit connaître car elle permet définir la structure et le contenu de vos pages web.</p>
+
+<p>On parle de <incode style="color:chartreuse">langage de balisage</incode> et non de langage de programmation, car le but du HTML est <incode style="color:chartreuse">d’encadrer</incode> les différents éléments présents dans une page (images, titres, paragraphes...) par des <incode style="color:chartreuse">balises</incode> pour leur <em>donner du sens</em> et aussi leur permettre d’être <incode style="color:chartreuse">mis en forme</incode> via une feuille de style.</p>
+
+<dots></dots>
+
+<grostitre style="text-transform:uppercase; color:gold">Histoire du html</grostitre>
+
+<h4>L'origine du HTML remonte au début du Web.</h4>
+
+<p>En effet, il a été inventé vers les années 1990 afin qu'il puisse présenter les documents qui circulent sur la toile et établir des liens entre eux à travers les liens hypertextes (ou hyperliens).</p>
+
+<img src="images/ligne-du-temps.webp">
+
+<h2 style="text-transform:uppercase; color:yellow">w3c</h2>
+
+<img src="images/w3c.webp">
+
+<h4>W3C ou World Wide Web</h4>
+
+<p>Le <em>W3C ou World Wide Web Consortium</em>, est un organisme international qui développe des <em>standards</em> pour le Web afin que les gens puissent <em>communiquer efficacement</em> à travers Internet, autour de formats ouverts garantissant une meilleure <em>inter-opérabilité </em>(c'est-à-dire une meilleure compréhension des systèmes hétérogènes à travers des données et langages standardisés).</p>
+
+<p>Le consortium existe depuis 1994 et est dirigé par l'inventeur du Web, <em>Tim Berners-Lee</em>.</p>
+
+<p>Il est composé d'une équipe fixe et des membres (dont plusieurs centaines d'entreprises partenaires). Les membres délèguent des ingénieurs au sein du W3C et participent ainsi à <em>l'élaboration des spécifications techniques</em> pour les technologies du Web. De nombreux membres actifs font partie des équipes de développement des navigateurs <em>(Microsoft, Mozilla, Apple, Opera, Google, etc)</em>.</p>
+
+<grostitre>L’inventeur du HTML</grostitre>
+
+<ul>
+    <li>Tim Berners-Lee</li>
+    <li>L’HTML est basé sur le SGML (autre système de balise)</li>
+    <li>Naissance officielle de l’HTML en août 1991</li>
+    <li>HTML était non standardisé et ne comprenait que quelques balises (titres, paragraphes, listes et hyperliens)</li>
+</ul>
+
+
+<grostitre>Les années 90</grostitre>
+
+<ul>
+    <li>Chaque navigateur implémente son propre HTML > incompatibilité</li>
+    <li>Octobre 1994 : Création du W3C</li>
+    <li>Mars 1995 : HTML 2</li>
+    <li>1997 : HTML 3.2 puis HTML 4</li>
+    <li>Les navigateurs prennent beaucoup de liberté avec la norme</li>
+    <li>La guerre des navigateurs!</li>
+</ul>
+
+<grostitre>Les années 2000</grostitre>
+
+<h3 style="color:chartreuse">W3C</h3>
+
+<ul>
+    <li>Abandonne l’HTML</li>
+    <li>Développe le XHTML</li>
+    <li>Structure uniquement</li>
+    <li>Mise en forme confiée au CSS</li>
+</ul>
+
+<h3 style="color:chartreuse">WHATWG</h3>
+
+<ul>
+    <li>Poursuivre le développement de l’HTML</li>
+    <li>Conteste l’autorité et le fonctionnement du W3C</li>
+</ul>
+
+<grostitre>Les années ~2010</grostitre>
+
+<ul>
+    <li>2007 : Abandon de l’XHTML par le W3C</li>
+    <li>Le w3C met en place un groupe de travail HTML5</li>
+    <li>Fin 2016 : Spécification officielle de l’HTML5 (17 ans après l’HTML 4 !!)</li>
+</ul>
+
+<grostitre>Spécificités de l’HTML 5</grostitre>
+
+<ul>
+    <li>Plus de numéro de version au-delà de HTML 5</li>
+    <li>HTML 5 rétrocompatible avec toutes les versions précédentes</li>
+    <li>Nombreuses balises dépréciées toujours dans la norme officielle</li>
+    <li>L’HTML ne s’occupe que de la structure du document, pas de sa mise en forme</li>
+</ul>
