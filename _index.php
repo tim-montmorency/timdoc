685c96e16600b513c709d0ffcbe200d7f7762d84
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2023-08-01 22:56:20 -0400

    tim doc 2

--- _index.php
+++ _index.php
@@ -14,7 +14,7 @@
 
 <grostitre>Cours</grostitre>
 
-<?php intlink("./582-MO/"); ?>
+<?php intlink("./582-111MO/"); ?>
 <?php intlink("./582-215MO/"); ?>
 <?php intlink("./582-424MO/"); ?>
 <?php intlink("./582-434MO/"); ?>
