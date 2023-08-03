0d1ec54442cba4fab7e90597ef0f5d0e6ae99688
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2023-08-03 12:23:58 -0400

    wiki upt

--- 582-111MO/wiki/_index.php
+++ 582-111MO/wiki/_index.php
@@ -9,6 +9,7 @@
 ?>
 
 <wiki-page name="Cours 1">
+    <?php intlink("../presentation-cours"); ?>
     <?php intlink("../autres/histoire-du-web/"); ?>
     <?php intlink("../autres/experience-utilisateur/"); ?>
     <?php intlink("../autres/composantes-web/"); ?>
