07e23aee93dbfecd4a1c6ad30475c37f8eb247d1
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2023-08-03 12:21:18 -0400

    histoire upt

---
+++ 582-111MO/presentation-cours/_index.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * @type     article
+ * @title    Présentation Web 1
+ * @icon     images/icon.png
+ * @abstract Outils & Logiciels
+ */
+?>
+
+<grostitre>Présentation du prof</grostitre>
+
+<img src="images/prof.png">
+
+<grostitre>Tour de classe</grostitre>
+
+<p style="color:#f4754d">Questions pour les étudiants</p>
+
+<list>
+    <ol>
+        <li>Qu'est-ce qui vous a poussé à choisir une formation en multimédia ?</li><br>
+
+        <li>Quels sont vos domaines d'intérêt particuliers en multimédia (design graphique, développement web, animation, etc.) ?</li><br>
+
+        <li>Quels logiciels ou outils multimédias maîtrisez-vous et lesquels aimeriez-vous apprendre davantage ?</li><br>
+
+        <li>Comment voyez-vous votre carrière future dans le domaine du multimédia ?</li><br>
+
+        <li>Quels sont vos projets ou objectifs actuels en lien avec vos études en multimédia ?</li><br>
+
+    </ol>
+</list>
+
+
+<grostitre>Les outils</grostitre>
+
+<grostitre>Procédure pour la rentrée</grostitre>
+
+<img src="images/procedures.png">
+
+<doclink href="https://ccti.cmontmorency.qc.ca/la-rentree/">Procédure pour la rentrée</doclink>
+
+<dots></dots>
+
+<grostitre>Écran</grostitre>
+
+<img src="images/ecran.png">
+
+<doclink href="https://www.cmontmorency.qc.ca/college/ecran/services-a-la-population-etudiante/ressources-en-ligne/">Écran - Services étudiants</doclink>
+
+<grostitre>Col.net</grostitre>
+
+<p style="color:#f4754d">Qu’est-ce que COL.NET au juste?</p>
+
+<p>Col.Net est le portail utilisé par les étudiants et les enseignants du collège. On y retrouve l’horaire, les résultats en cours de session, le QAQ bulletin, les messages de l’administration et tout ce qui vous servira pendant toute la durée de vos études. Prenez l’habitude de vous y connecter souvent!
+</p>
+
+<img src="images/col-net.png">
+
+<doclink href="https://www.cmontmorency.qc.ca/">Collège Montmorency</doclink>
+
+
+<p style="color:#f4754d">Procédure de connexion au système Col.NET </p>
+
+<img src="images//connexion.png">
+
+<p><em>Code d’usager ÉTUDIANT </em></p>
+
+<p>Il s'agit du numéro de demande d'admission (DA) à 9 caractères. Le numéro de DA apparaît sur la carte d'identité, la lettre d'admission ou la confirmation d'inscription. </p>
+
+<p>Lorsque le numéro de DA est à 7 caractères, il doit être précédé de : </p>
+
+<list>
+
+    <li>19 si le numéro se situe entre 6000000 et 9999999, ainsi 8912034 devient 198912034</li><br>
+
+    <li>20 si le numéro se situe entre 0000000 et 5999999, ainsi 0812034 devient 200812034 </li><br>
+
+</list>
+
+<p><em>Mot de passe </em></p>
+
+<p>Dans tous les cas, lors de la première connexion, la date de naissance est requise sous la forme AAAAMMJJ (exemple : 19510923 pour le 23 septembre 1951). Lors de la première identification, Col.NET demande de créer un mot de passe qui sera utilisé lors des connexions subséquentes. </p>
+
+<p>Vous devez : entrer votre ancien mot de passe (votre date de naissance sous la forme AAAA/MM/JJ) entrer votre nouveau mot de passe (entre 6 et 30 caractères, avec au moins une lettre minuscule, une lettre majuscule et un chiffre) et ensuite le saisir à nouveau pour le confirmer:</p>
+
+<doclink href="https://www.cmontmorency.qc.ca/ColNET/etudiant/Messagesgeneraux/ColNET_procedure_connexion.pdf">Procédure de connexion</doclink>
+
+<dots></dots>
+
+<grostitre>Microsoft 365</grostitre>
+
+<youtube src="./videos/microsoft-365.json"></youtube>
