d886e3764e194a6f44147f076e68793d4e7479b0
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-08-03 00:45:59 -0400

    nouveau cours 518 la base

---
+++ 582-518MO/_index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @type     list
+ * @title    Projet Web
+ * @icon     images/icon.png
+ * @abstract 582-518MO
+ */
+
+ print_children();
---
+++ 582-518MO/exercices/cartes-jeu-du-27/_index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @type     exercice
+ * @title    Consignes - Jeu du 27
+ * @icon     images/thumb.png
+ * @abstract Pour cet exercice,...
+ */
+?>
+
+<p>Pour cet exercice, vous devez compléter un jeu de cartes déjà entamé. Le joueur doit piger 3 cartes dans le but d’obtenir un pointage supérieur à celui du casino. Cependant attention, il ne faut pas dépasser 27.</p>
+
+Aperçu du résultat 👇
