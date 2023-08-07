2a4cd9f77e72027a8d753720f2c1a109ffa904b9
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2023-08-07 18:04:25 -0400

    autres + format audio

---
+++ 582-111MO/autres/formats-audio/_index.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @type     article
+ * @title    Formats Audio
+ * @icon     images/icon.png
+ * @abstract MP3, AAC, OGG, AV, FLAC, WebM, M4A
+ */
+?>
+
+<p class="spacer">Il existe plusieurs formats Audio supportées dans les navigateurs.</p>
+
+
+
+<grostitre>MP3</grostitre>
+
+<p><em>MP3 (MPEG Audio Layer III)</em> Le format MP3 est l'un des formats audio les plus répandus sur le web. Il offre une bonne qualité sonore tout en ayant une taille de fichier relativement petite, ce qui le rend idéal pour le streaming en ligne et le téléchargement.</p>
+
+<p>
+    ➕ Taille de fichier réduite<br>
+    ➕ Compatibilité universelle<br>
+    ➕ Diffusion en continu<br><br>
+    ➖ Perte de qualité<br>
+    ➖ Qualité variable<br>
+    ➖ Dégradation successive
+</p>
+
+<info><p>Le MP3 est un format audio largement utilisé en raison de sa taille de fichier réduite et de sa compatibilité universelle, ce qui le rend pratique pour la diffusion en continu et le stockage de musique.</p> 
+<p>Cependant, il présente des compromis en termes de qualité audio par rapport aux formats sans perte. Le choix d'utiliser le MP3 dépend des besoins spécifiques et des préférences en matière de qualité audio.</p></info>
+
+<doclink href="https://en.wikipedia.org/wiki/MP3">MP3</doclink>
+
+<dots></dots>
+
+<grostitre>AAC</grostitre>
+
+<p><em>AAC (Advanced Audio Coding)</em> L'AAC est un format audio de haute qualité qui offre une meilleure compression que le MP3, ce qui permet d'obtenir une meilleure qualité sonore à des débits binaires plus bas. Il est couramment utilisé pour la diffusion en continu et les services de musique en ligne.</p>
+
+<p>
+    ➕ Qualité audio supérieure<br>
+    ➕ Meilleure compression<br>
+    ➕ Économie d'espace<br>
+    ➕ Meilleure gestion des hautes fréquences<br><br>
+    ➖ Complexité<br>
+    ➖ Moins largement utilisé<br>
+    ➖ Dégradation successive
+</p>
+
+<info><p>L'AAC est un format audio qui offre une meilleure qualité sonore que le MP3 tout en maintenant une taille de fichier relativement réduite. Il est largement utilisé pour la diffusion en continu, le téléchargement de musique et d'autres applications audio en ligne.</p> 
+<p>La décision d'utiliser l'AAC dépend des besoins spécifiques en matière de qualité audio, de compatibilité et de capacités matérielles.</p></info>
+
+<doclink href="https://en.wikipedia.org/wiki/Advanced_Audio_Coding">AAC</doclink>
+
+<dots></dots>
+
+<grostitre>OGG</grostitre>
+
+<p><em>OGG (Ogg Vorbis)</em> Le format OGG est un format audio open source qui offre une bonne qualité sonore à des débits binaires plus bas. Il est souvent utilisé pour les flux audio en direct et le téléchargement de fichiers audio.</p>
+
+<p>
+    ➕ Qualité sonore<br>
+    ➕ Format ouvert<br>
+    ➕ Compression efficace<br>
+    ➕ Compatibilité<br>
+    ➕ Meilleure gestion des hautes fréquences<br><br>
+    ➖ Moins largement utilisé<br>
+    ➖ Moins de contenus disponibles<br>
+</p>
+
+<info><p>L'OGG est un format audio open source qui offre une bonne qualité sonore tout en maintenant une taille de fichier réduite. Bien qu'il ne soit pas aussi largement utilisé que d'autres formats tels que le MP3 ou l'AAC, il est de plus en plus pris en charge et offre une alternative viable pour la diffusion en continu, le téléchargement de musique et d'autres applications audio en ligne.</p> 
+<p>La décision d'utiliser l'OGG dépend des besoins spécifiques en matière de qualité audio, de compatibilité et d'objectifs.</p></info>
+
+<doclink href="https://en.wikipedia.org/wiki/Ogg">OGG</doclink>
+
+<dots></dots>
+
+
+<grostitre>WAV</grostitre>
+
+<p><em>WAV (Waveform Audio File Format)</em> Le format WAV est un format audio sans perte qui offre une excellente qualité sonore, mais il produit généralement des fichiers de grande taille. Il est parfois utilisé pour les enregistrements audio de haute qualité, mais il n'est pas aussi courant pour le streaming en ligne en raison de sa taille.</p>
+
+<p>
+    ➕ Qualité audio non altérée<br>
+    ➕ Qualité sonore élevée<br>
+    ➕ Stockage des détails sonores<br>
+    ➕ Compatibilité<br><br>
+    ➖ Taille de fichier élevée<br>
+    ➖ Inefficace pour le streaming<br>
+    ➖ Non adapté aux appareils avec peu d'espace<br>
+    ➖ Compression inexistante<br>
+    ➖ Moins couramment utilisé pour le web<br>
+</p>
+
+<info><p>Le format WAV est privilégié pour sa qualité sonore non altérée et son stockage de détails audio, ce qui en fait un choix populaire pour l'enregistrement professionnel et la production musicale. Cependant, en raison de sa taille de fichier élevée, il peut être moins pratique pour le stockage en ligne, le streaming et l'utilisation sur des appareils avec un espace de stockage limité.</p> 
+<p> La décision d'utiliser le WAV dépend des besoins spécifiques en matière de qualité audio, de compatibilité et d'espace de stockage.</p></info>
+
+<doclink href="https://en.wikipedia.org/wiki/WAV">WAV</doclink>
+
+<dots></dots>
