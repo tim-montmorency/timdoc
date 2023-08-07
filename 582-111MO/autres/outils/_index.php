998b5d0ee6262ac0ff8aa98cec4fbb81ccc5a183
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2023-08-07 16:02:54 -0400

    autres et outils updt

---
+++ 582-111MO/autres/outils/_index.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * @type     article
+ * @title    Les outils
+ * @icon     images/icon.png
+ * @abstract draw.io, vs code, W3C, etc...
+ */
+?>
+
+<p style="font-size: 40px"> AUTONOMIE ET DÉBROUILLARDISE</p>
+
+<dots></dots>
+
+<grostitre>Google & Youtube</grostitre>
+
+<img src="images/draw-io.png">
+
+<p>Google et YouTube sont largement considérés comme parmi les meilleures plateformes pour trouver des explications et des informations sur une grande variété de sujets.</p>
+
+<p><em>GOOGLE</em></p>
+
+<list>
+    <ol>
+        <li><em>Vaste indexation</em> : Google indexe une immense quantité de pages web provenant du monde entier, ce qui signifie qu'il a accès à une multitude d'informations sur pratiquement tous les sujets imaginables.</li>
+
+        <li><em>Algorithme de recherche avancé</em> : Google utilise un algorithme de recherche sophistiqué qui analyse et classe les pages en fonction de leur pertinence et de leur qualité. Cela permet de trouver rapidement des informations fiables et pertinentes.</li>
+
+        <li><em>Diversité des formats</em> : Google peut vous fournir différents types de ressources, tels que des articles, des tutoriels, des présentations, des forums de discussion et bien plus encore.</li>
+
+        <li><em>Fonctionnalités supplémentaires </em> : Google propose des fonctionnalités comme la recherche d'images, de vidéos, de nouvelles, de livres, etc., ce qui permet de trouver des informations dans divers formats.</li>
+    </ol>
+</list>
+
+<p><em>YOUTUBE</em></p>
+
+<list>
+    <ol>
+        <li><em>Contenu visuel</em> :  YouTube est une plateforme de partage de vidéos, ce qui signifie qu'elle offre des explications sous forme de vidéos. Les vidéos peuvent fournir des explications visuelles, des démonstrations, des tutoriels pas à pas et bien plus encore.</li>
+
+        <li><em>Variété des créateurs</em> : Des millions de créateurs de contenu du monde entier partagent leurs connaissances et leurs compétences sur YouTube. Cela signifie que vous pouvez trouver des explications dans une grande variété de domaines, du bricolage à la science en passant par la musique et la cuisine.</li>
+
+        <li><em>Moteur de recherche vidéo</em> : Le moteur de recherche de YouTube est spécialement optimisé pour trouver des vidéos pertinentes en fonction de vos requêtes.</li>
+
+        <li><em>ommentaires et interaction </em> : Les vidéos sur YouTube sont souvent accompagnées de sections de commentaires où vous pouvez poser des questions et interagir avec les créateurs et d'autres spectateurs pour obtenir des éclaircissements supplémentaires.</li>
+    </ol>
+</list>
+
+<warning>Cependant, il est important de noter que, bien que Google et YouTube soient d'excellentes ressources pour trouver des explications, il est toujours conseillé de faire preuve de discernement et de vérifier la crédibilité des sources, surtout pour des sujets complexes ou controversés.</p>
+
+<p> Toujours croiser les informations provenant de différentes sources pour obtenir une vue d'ensemble précise.</p></warning>
+
+
+<doclink href="https://www.google.ca">Google</doclink>
+
+<doclink href="https://www.youtube.com">YouTube</doclink>
+
+<dots></dots>
+
+
+<grostitre>Draw.io</grostitre>
+
+<img src="images/draw-io.png">
+
+<p>Draw.io est un outil en ligne de création de diagrammes et de schémas. Il permet aux utilisateurs de concevoir des diagrammes de flux, des organigrammes, des diagrammes de réseau, des schémas de base de données, des maquettes d'interface utilisateur, des plans d'architecture et bien d'autres types de graphiques visuels.</p>
+<p> L'outil offre une interface conviviale qui facilite la création de diagrammes professionnels, même pour les personnes qui ne sont pas des concepteurs expérimentés.</p>
+
+<doclink href="https://www.drawio.com">Draw.io</doclink>
+
+<dots></dots>
+
+<grostitre>Virtual Studio Code  (VS code)</grostitre>
+
+<img src="images/vs-code.webp">
+
+<p>Visual Studio Code, souvent abrégé en VS Code, est un éditeur de code source développé par Microsoft. Il est conçu pour être léger, rapide et extrêmement personnalisable, ce qui en fait un choix populaire parmi les développeurs pour écrire du code dans une variété de langages de programmation.</p>
+<p> VS Code est gratuit et open source, ce qui signifie que les utilisateurs peuvent l'utiliser, le personnaliser et contribuer à son développement.</p>
+
+<doclink href="https://code.visualstudio.com">VS Code</doclink>
+
+<dots></dots>
+
+<grostitre>W3Schools</grostitre>
+
+<img src="images/W3Schools.webp">
+
+<p>W3Schools est un site web éducatif populaire qui propose des ressources et des tutoriels pour apprendre et comprendre les technologies liées au développement web et à la conception de sites web.</p>
+<p> Fondé en 1998, W3Schools est largement utilisé par les débutants et les développeurs expérimentés comme une ressource de référence pour apprendre, pratiquer et maîtriser divers langages de programmation et technologies web.</p>
+
+<doclink href="https://www.w3schools.com">W3Schools</doclink>
+
+<dots></dots>
+
+<grostitre>WIKI</grostitre>
+
+<img src="images/Microsoft_Teams.webp">
+
+<p>Le "Wiki" dans Microsoft Teams fait référence à une fonctionnalité de collaboration qui permet aux équipes de créer et de partager des pages de contenu, de notes et de documentation au sein de leur espace d'équipe. Cela permet aux membres de l'équipe de rassembler et d'organiser des informations importantes, de collaborer sur des documents et de conserver une base de connaissances partagée.</p>
+
+
+<dots></dots>
+
+<grostitre>TIM-Doc</grostitre>
+
+<img src="images/tim-doc.webp">
+
+<p>TIM-Doc est une ressource en ligne qui regroupe l'ensemble des connaissances que vous allez apprendre au cours de la session. Cette ressource peut jouer un rôle crucial dans la préparation et la compréhension des matières enseignées.</p>
+
+<p>TIM-Doc est un point d'accès centralisé à une collection complète de documents, de supports de cours et de ressources pédagogiques spécifiques au cours de Web 1. Cela signifie que vous pouvez utiliser ce lien pour accéder à toutes les informations pertinentes dont vous aurez besoin pour étudier et réussir votre session académique.</p>
+
+<doclink href="https://tim-montmorency.com/timdoc/582-111MO/">TIM-Doc Web 1</doclink>
+
+<dots></dots>
+
+<grostitre>Codepen</grostitre>
+
+<img src="images/codepen.png">
+
+<p>CodePen est une plateforme en ligne populaire qui permet aux développeurs web de créer, expérimenter et partager du code HTML, CSS et JavaScript en temps réel. C'est un environnement de développement web interactif qui offre un espace de travail virtuel pour tester des idées, créer des maquettes, résoudre des problèmes et collaborer avec d'autres développeurs.</p>
+
+<p> CodePen est souvent utilisé pour la création de prototypes, la démonstration de concepts, l'apprentissage et la création de petites applications web.</p>
+
+<doclink href="https://codepen.io">Codepen</doclink>
+
+<dots></dots>
