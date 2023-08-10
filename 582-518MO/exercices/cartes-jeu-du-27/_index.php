e9a2de1ff2515b39bdef57c9e3f7e1c61aee99e8
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-08-10 18:41:08 -0400

    Ajout medias exercice jeu de carte

--- 582-518MO/exercices/cartes-jeu-du-27/_index.php
+++ 582-518MO/exercices/cartes-jeu-du-27/_index.php
@@ -4,10 +4,16 @@
  * @type     exercice
  * @title    Consignes - Jeu du 27
  * @icon     images/thumb.png
- * @abstract Pour cet exercice,...
+ * @abstract Créer un jeu de cartes en CSS et JJ
  */
 ?>
 
 <p>Pour cet exercice, vous devez compléter un jeu de cartes déjà entamé. Le joueur doit piger 3 cartes dans le but d’obtenir un pointage supérieur à celui du casino. Cependant attention, il ne faut pas dépasser 27.</p>
+<p>Aperçu du résultat 👇</p>
 
-Aperçu du résultat 👇
+
+<clipasset src='medias/cartes-jeu-du-27.mp4'></clipasset>
+
+<dots></dots>
+<grostitre>Matériels</grostitre>
+<doclink href="https://codepen.io/tim-momo/pen/VwEeVdz">Pen de départ</doclink>
