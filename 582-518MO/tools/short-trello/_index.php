03a812b3c01f20c755a4889025cf64a892b853c5
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-08-14 16:26:42 -0400

    procedure temp timdoc mac+autres pages projet web

---
+++ 582-518MO/tools/short-trello/_index.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * @type     tool
+ * @title    Raccourcis clavier Trello
+ * @icon     images/trello-shortcuts.jpg
+ * @url      https://trello.com/shortcuts
+ * @abstract Raccourcis clavier pour accélérer le processus dans Trello
+ */
+?>
+<script>location.href = '<?php echo $PAGE->url; ?>';</script>
