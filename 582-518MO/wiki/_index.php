b6be25c70daae262e0e5a25e3c8d811a9c4f4b83
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-08-17 16:34:51 -0400

    wiki cours 1

--- 582-518MO/wiki/_index.php
+++ 582-518MO/wiki/_index.php
@@ -6,18 +6,42 @@
  * @abstract Contenu du cours - Automne 2023
  */
 ?>
-
 <wiki-page name="Cours 01" date="2023-08-16">
-    <h2>Quiz</h2>
-    Lundi - <a href="https://app.wooclap.com/JFHDYX" target="_blank" rel="noopener noreferrer">Code Wooclap : JFHDYX</a>
+    <h2>Plan de cours pour cette session</h2>
+    <p><a href="#" target="_blank" rel="noopener noreferrer">Lien vers plan de cours</a> <em>(accessible seulement avec votre compte du collège)</em></p>
+    <h2>Sondage</h2>
+    <p> Lundi - <a href="https://app.wooclap.com/JFHDYX" target="_blank" rel="noopener noreferrer">Code Wooclap : JFHDYX</a>
     <br>
     Vendredi - <a href="https://app.wooclap.com/ROTODA" target="_blank" rel="noopener noreferrer">Code Wooclap :
-        ROTODA</a><br>
+        ROTODA</a></p>
+
     <h2>Projet de session (PS)</h2>
 
     <?php intlink("../projet/mise-en-contexte/"); ?>
     <?php intlink("../projet/arborescence/"); ?>
     <?php intlink("../projet/requis-client/"); ?>
+
+    <h2>Gestion de projet</h2>
     <?php intlink("../gestion-de-projet/kanban/"); ?>
+    <?php intlink("../gestion-de-projet/trello/"); ?>
+    <?php intlink("../gestion-de-projet/rapport-de-temps/"); ?>
+
+    <h2>Projet de session (PS)</h2>
+    <?php intlink("../projet/melee-scrum/"); ?>
+
+    <h2>Exercice: Dérouillage CSS et JS</h2>
+    <exercice href='../exercices/cartes-jeu-du-27/'></exercice>
+
+</wiki-page>
+
+<wiki-page name="Cours 02" date="2023-08-16">
+    <h2>Quiz</h2>
+    Lundi - <a href="https://app.wooclap.com/JFHDYX" target="_blank" rel="noopener noreferrer">Code Wooclap : </a>
+    <br>
+    Vendredi - <a href="https://app.wooclap.com/ROTODA" target="_blank" rel="noopener noreferrer">Code Wooclap :
+        </a><br>
+
+    <h2>Projet de session (PS)</h2>
+    <?php intlink("../projet/mise-en-contexte/"); ?>
 
 </wiki-page>
