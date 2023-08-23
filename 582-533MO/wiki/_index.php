7f9c152ebb9435bb6654193a1d20ba1bd7256bbb
LoraBoisvert <loraboisvert@gmail.com>
2023-08-23 13:36:52 -0400

    Infos sur les stages

--- 582-533MO/wiki/_index.php
+++ 582-533MO/wiki/_index.php
@@ -18,3 +18,15 @@
  <doclink href="https://forms.office.com/Pages/ResponsePage.aspx?id=x5Wp_94QyE6V2yjtBXZFXTWNUESufAFKvVivJEYUtYpUNldORE9BRTJNTUdKSk9WSjFTMUxBRjkxMi4u">Questionnaire</doclink>
 
 </wiki-page>
+
+
+<wiki-page name="Cours 02">
+
+<h2>Présentation du cours</h2>
+<doclink href="https://cmontmorency365-my.sharepoint.com/:b:/g/personal/lora_boisvert_cmontmorency_qc_ca/Ebf4qp-R6jZNm09ZDJVkKwcByQzm8933SJt-d1ApPTFnlA?e=VL1aBo">Présentation du cours</doclink>
+
+ <h2>Questionnaire</h2>
+ <doclink href="https://forms.office.com/Pages/ResponsePage.aspx?id=x5Wp_94QyE6V2yjtBXZFXRNpbsE2ELxAl1w7h19byDhUQ1FETVQyREJYREc5UFg4WTVYMTRMNkRLNSQlQCN0PWcu">Questionnaire</doclink>
+
+
+</wiki-page>
