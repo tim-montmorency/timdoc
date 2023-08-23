2c9b9d56f0cabd959bfe77affc69929ff97b6168
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-08-23 02:08:49 -0400

    sass nouvelles pages

--- 582-518MO/sass/fonctions/_index.php
+++ 582-518MO/sass/fonctions/_index.php
@@ -3,7 +3,43 @@
  * @type     article
  * @title    Fonctions
  * @icon     images/icon.png
- * @abstract x
+ * @abstract Gérer des opérations complexes, de manipuler des paramètres...
  * @index 22
  */
 ?>
+<p>Les <code>@functions</code> permettent de gérer des opérations complexes, de manipuler un ou plusieurs paramètres et de retourner des résultats précis. Elles sont généralement utilisées afin de produire des valeurs.</p>
+
+<p>Par exemple, une variable d'espacement <code>$gutter: 26px;</code> pourrait nécessitée d'être divisée en deux lorsque deux éléments l'utilisant se suivent afin d'éviter que l'espacement soit doublé. Pour palier à ce problème, il serait possible de se créer une fonction divisant cette valeur:</p>
+
+<highlight lang='css'>@function half($num) {
+ @return $num / 2;
+}</highlight>
+
+
+
+<p>Ainsi il serait possible d'écrire:</p>
+
+<highlight lang='css'>.card {
+  padding: half($gutter); // 13px
+}</highlight>
+
+<info>Remarquez <code>@return</code> qui indique quel élément dans la fonction doit être&nbsp;retourné.</info>
+<br>
+ <p>Pourquoi ne pas utiliser <code>calc()</code> plutôt? La fonction <code>calc()</code> est interprété dans le navigateur donc demande quelques fractions de seconde de plus à votre navigateur afin d'afficher, tandis que tout calcul effectué directement via SASS est effectué au moment de la compilation et donc n'engendre aucun délais pour l'usager.</p>
+
+<doclink href='https://sass-lang.com/documentation/at-rules/function'>@function</doclink>
+<dots></dots>
+<aside class="exercice-card">
+    <div class="exercice-card__content">
+      <em class="exercice-card__tagline">Exercice</em><br class="exercice-card__break">
+      <a href="/exercices/sass/function-uno" target="_blank" class="exercice-card__title">
+        @function -&nbsp;Uno
+      </a>
+
+            <p class="exercice-card__description">Pour cet exercice, vous devez styler plusieurs cartes Uno. Malheureusement il vous est interdit de modifier le HTML et puisque vous&nbsp;n’ave...</p>
+          </div>
+
+      </aside>
+  
+
+    
--- 582-518MO/sass/mixins/_index.php
+++ 582-518MO/sass/mixins/_index.php
@@ -3,7 +3,108 @@
  * @type     article
  * @title    Mixins
  * @icon     images/icon.png
- * @abstract x
+ * @abstract Créer des extraits de CSS pouvant être réutilisés
  * @index 25
  */
 ?>
+<p>Les mixins permettent de créer des extraits de CSS pouvant être réutilisés à différents endroits grâce à <code>@include</code>.</p>
+
+<p>Par exemple, il est possible de se créer une mixin rendant une image responsive.</p>
+
+<highlight lang='css'>@mixin responsive-img {
+  display: block; 
+  width: 100%;
+  height: auto;
+}</highlight>
+
+<p>Plutôt que de répéter ces trois lignes de code CSS à chaque fois à chaque image doit être responsive, il serait possible d'appelez simplement cette mixin.</p>
+
+<p>Par exemple:</p>
+<highlight lang='css'>.article-img {
+  @include responsive-img;
+}</highlight>
+
+<doclink href='https://sass-lang.com/documentation/at-rules/mixin'>mixin</doclink>
+
+
+
+
+
+
+<dots></dots>
+<grostitre>Arguments</grostitre>
+<p>Les mixins acceptent de recevoir des arguments. Ces derniers sont ensuite disponibles dans le corps de la mixin, rendant ainsi leur usage plus flexible.</p>
+<p>Par exemple, afin d'ajoutez un <code>max-width</code> variant selon différents scénarios, il n'est pas nécessaire de créer plusieurs mixins. L'ajout d'un argument permettra de réutiliser la même.</p>
+
+<highlight lang='css'>@mixin responsive-img($max) {
+  display: block; 
+  width: 100%;
+  height: auto;
+  max-width: $max; // 600px
+}
+
+.article-img {
+  @include responsive-img(600px);
+}</highlight>
+
+
+
+<warning>Si un argument sensé être fournis à un mixin est omis, une erreur se produira au moment de la compilation du code&nbsp;CSS.</warning>
+
+
+<h3 class="heading heading--h3" id="Valeur par défaut">Valeur par défaut</h3>
+
+<p>Afin d'éviter une erreur lorsqu'un argument est omis, il est possible de fournir une valeur par défaut. </p>
+
+<p>Par exemple:</p>
+
+<highlight lang='css'>@mixin responsive-img($max: 100%) {
+  display: block; 
+  width: 100%;
+  height: auto;
+  max-width: $max; // 100%
+}
+
+.article-img {
+  @include responsive-img;
+}</highlight>
+
+<h3 class="heading heading--h3" id="Spécifier l'argument">Spécifier l'argument</h3>
+
+<p>Les arguments sont généralement passés à une mixin selon l'ordre dans lequel ils ont été déclarés dans celle-ci. Cependant, il est possible de spécifier à quel argument une valeur est passée. Ceci permet, d'omettre certains arguments ou simplement de les écrire dans un ordre différent.</p><p>Par exemple:</p>
+
+<highlight lang='css'>@mixin responsive-img($display: block, $max: 100%) {
+  display: $display; 
+  width: 100%;
+  height: auto;
+  max-width: $max; // 300px
+}
+
+.article-img {
+  @include responsive-img($max: 300px);
+}</highlight>
+
+
+
+
+<p>Ainsi, le fait de nommé <code>$max</code> dans l'appel de la mixin permet de spécifier que la valeur 300px n'est pas pour <code>$display</code>, mais bien <code>$max</code>, même si ce dernier est le 2<sup>e</sup> argument.</p>
+
+<doclink href='https://sass-lang.com/documentation/at-rules/mixin#arguments'>Arguments</doclink>
+
+
+
+<dots></dots>
+
+
+              <aside class="exercice-card">
+    <div class="exercice-card__content">
+      <em class="exercice-card__tagline">Exercice</em><br class="exercice-card__break">
+      <a href="https://smnarnold.com/exercices/sass/mixin-grille-bootstrap" target="_blank" class="exercice-card__title">
+        @mixin - Grille&nbsp;Bootstrap
+      </a>
+
+            <p class="exercice-card__description">Dans le cadre de cet exercice, nous allons lever le voile sur l’usage que fait Bootstrap de SCSS afin de générer sa fameuse grille ayant&nbsp;...</p>
+          </div>
+
+
+      </aside>
