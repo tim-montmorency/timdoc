b0d415f1fb177a4bd26d99cb84835d5618118084
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-08-24 19:01:13 -0400

    exercice boucle for en sass 1 à 6

---
+++ 582-518MO/exercices/sass-for-1-a-6/_index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @type     exercice
+ * @title    Images 1 à 6
+ * @icon     images/thumb.png
+ * @abstract Utiliser @for du langage Sass pour afficher les images thématiques 1 à 6.
+ */
+?>
+
+<p>Dans le cadre de cet exercice, vous devez recréer la palette de couleur Pink Paradise afin de présenter les 20 tons de rose disponibles chez un fabricant de peinture. Pour ce faire vous aurez besoin d’une boucle&nbsp;Scss.</p>
+
+<p>Aperçu du résultat 👇</p>
+<img src="images/pink-palette-resultat.png" alt="Palette de différentes teintes rose">
+<grostitre>Matériel</grostitre>
+
+<doclink href='https://codepen.io/tim-momo/pen/bGOVpdE'>Pen de départ</doclink>
+
+<h3>Médias</h3>
+<mediafile src="images/img1.jpg">Image 1</mediafile>
+
+
+<dots></dots>
+<grostitre>Requis</grostitre>
+
+<checklist>abc
+</checklist>
+   
+
+<dots></dots>
+<grostitre>Notes de cours 📚</grostitre>
+
+<?php intlink("../../sass/boucle/"); ?>
+
+    
