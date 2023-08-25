7ca0ffc7b3aa8d404a6d3e59724d37b396ae9786
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2023-08-25 09:50:31 -0400

    exercice semantic

---
+++ 582-111MO/html/semantique/exercices/mes_premieres_balises/_index.php
@@ -0,0 +1,42 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Mes premières balises
+ * @icon     images/thumb.jpg
+ * @abstract  Identifier et appliquer correctement les balises sémantiques HTML pour structurer le contenu d'une page web.
+ */
+?>
+
+<p> Identifier et appliquer correctement les balises sémantiques HTML pour structurer le contenu d'une page web.</p>
+
+<grostitre>Instructions</grostitre>
+
+<p>Considérez le paragraphe de texte suivant et utilisez les balises sémantiques HTML appropriées pour structurer le texte en conséquence. Choisissez les balises qui décrivent au mieux le type de contenu de chaque partie du texte.</p>
+
+<grostitre>Texte</grostitre>
+
+<em>
+    <p>L'importance de l'exercice régulier pour la santé</p>
+    
+    <p>L'exercice régulier joue un rôle crucial dans le maintien d'un mode de vie sain. Il offre de nombreux avantages pour la santé physique et mentale. En incorporant une routine d'exercice dans votre quotidien, vous pouvez améliorer votre condition cardiorespiratoire, renforcer vos muscles et maintenir un poids corporel adéquat.</p>
+    
+    <p>De plus, l'exercice a un impact positif sur la santé mentale en réduisant le stress, l'anxiété et la dépression. Il favorise également la libération d'endorphines, qui sont des hormones du bonheur. S'engager dans une activité physique régulière peut contribuer à augmenter la confiance en soi et à améliorer la qualité du sommeil.</p>
+    
+    <p>Il est recommandé d'adopter une variété d'activités physiques, telles que la marche, la natation, le yoga ou l'entraînement en force. Quelle que soit l'activité choisie, l'essentiel est de maintenir la régularité. Avant de commencer tout nouveau programme d'exercice, il est conseillé de consulter un professionnel de la santé pour s'assurer que les activités sont adaptées à votre condition physique.</p>
+    
+    <p>En conclusion, prendre soin de sa santé physique et mentale devrait inclure une place pour l'exercice régulier. En structurant votre routine quotidienne pour y inclure une activité physique, vous investissez dans votre bien-être général.
+    </p>
+</em>
+
+<grostitre>Tâches</grostitre>
+
+<checklist>
+Identifiez les parties du texte qui correspondent à des titres ou des en-têtes.
+Identifiez les parties du texte qui contiennent des informations importantes et spécifiques.
+Identifiez les parties du texte qui sont des détails supplémentaires ou des exemples.
+Utilisez les balises sémantiques HTML appropriées pour marquer chaque partie du texte selon les critères ci-dessus.
+</checklist>
+
+<grostitre>Résultat attendu</grostitre>
+
+<p>Après avoir terminé l'exercice, votre texte devrait être structuré à l'aide des balises sémantiques HTML appropriées pour refléter la hiérarchie et le type de contenu décrit dans les instructions.</p>
