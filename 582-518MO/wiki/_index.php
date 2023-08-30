4be6795dff4fb3a9fac9448941927b4db700ea32
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-08-30 16:15:17 -0400

    Mise è jour du wiki pour cours 2

--- 582-518MO/wiki/_index.php
+++ 582-518MO/wiki/_index.php
@@ -6,20 +6,43 @@
  * @abstract Contenu du cours - Automne 2023
  */
 ?>
+<!-- 
+    ASCII GENERATOR 
+    http://www.network-science.de/ascii/
+-->
+
+<!--
+  _____                       __ 
+ / ____|                     /_ |
+| |     ___  _   _ _ __ ___   | |
+| |    / _ \| | | | '__/ __|  | |
+| |___| (_) | |_| | |  \__ \  | |
+ \_____\___/ \__,_|_|  |___/  |_|
+
+-->
 <wiki-page name="Cours 01" date="2023-08-16">
     <h2>Plan de cours pour cette session</h2>
-    <p><a href="https://cmontmorency365-my.sharepoint.com/:b:/g/personal/mariem_ouellet_cmontmorency_qc_ca/EVbOJto_YHJMkcEd2DgccjYBiIYQkpUf7pvURT-o_95kFQ?e=530zjT" target="_blank" rel="noopener noreferrer">Plan de cours pour groupe du LUNDI</a> <em>(accessible seulement avec votre compte du collège)</em></p>
-    <p><a href="https://cmontmorency365-my.sharepoint.com/:b:/g/personal/mariem_ouellet_cmontmorency_qc_ca/EezMGYzxzGxGkHwf_GAwdxwBYZXLieiFKYtsLp3Sb3-COQ?e=cOcN8J" target="_blank" rel="noopener noreferrer">Plan de cours pour groupe du VENDREDI</a> <em>(accessible seulement avec votre compte du collège)</em></p>
+    <p><a href="https://cmontmorency365-my.sharepoint.com/:b:/g/personal/mariem_ouellet_cmontmorency_qc_ca/EVbOJto_YHJMkcEd2DgccjYBiIYQkpUf7pvURT-o_95kFQ?e=530zjT"
+            target="_blank" rel="noopener noreferrer">Plan de cours pour groupe du LUNDI</a> <em>(accessible seulement
+            avec votre compte du collège)</em></p>
+    <p><a href="https://cmontmorency365-my.sharepoint.com/:b:/g/personal/mariem_ouellet_cmontmorency_qc_ca/EezMGYzxzGxGkHwf_GAwdxwBYZXLieiFKYtsLp3Sb3-COQ?e=cOcN8J"
+            target="_blank" rel="noopener noreferrer">Plan de cours pour groupe du VENDREDI</a> <em>(accessible
+            seulement avec votre compte du collège)</em></p>
     <h2>Sondage</h2>
     <p>Tutorat - <a href="https://app.wooclap.com/FLBLQP" target="_blank" rel="noopener noreferrer">Code Wooclap :
-    FLBLQP</a>
-        <br></p>
+            FLBLQP</a>
+        <br>
+    </p>
     <p> Lundi - <a href="https://app.wooclap.com/JFHDYX" target="_blank" rel="noopener noreferrer">Code Wooclap :
             JFHDYX</a>
         <br>
         Vendredi - <a href="https://app.wooclap.com/ROTODA" target="_blank" rel="noopener noreferrer">Code Wooclap :
             ROTODA</a>
     </p>
+
+
+    <br><br> <br><br>
+    <hr><br>
 
     <h2>Projet de session (PS)</h2>
 
@@ -37,21 +60,101 @@
 
     <h2>Exercice: Dérouillage CSS et JS</h2>
     Le lien sera fourni en cours.
-    <br><br> <br><br> <hr><br>
+
+
+
+    <br><br> <br><br>
+    <hr><br>
+
     <h2>Devoirs</h2>
     <p>Compléter les requis émis à la fin de ces 3 pages :</p>
     <?php intlink("../projet/mise-en-contexte/"); ?>
     <?php intlink("../projet/melee-scrum/"); ?>
     <?php intlink("../gestion-de-projet/rapport-de-temps/"); ?>
-<br>
+    <br>
     <p>Compléter l'exercice Jeu du 27: Le lien sera fourni en cours.</p>
-    
 
 </wiki-page>
 
+
+
+
+
+
+
+<!--
+  _____                       ___  
+ / ____|                     |__ \ 
+| |     ___  _   _ _ __ ___     ) |
+| |    / _ \| | | | '__/ __|   / / 
+| |___| (_) | |_| | |  \__ \  / /_ 
+ \_____\___/ \__,_|_|  |___/ |____|
+                                                                
+-->
 <wiki-page name="Cours 02" date="2023-08-26">
     <h2>Quiz</h2>
     Lundi - <a href="https://app.wooclap.com/SDQXZU" target="_blank" rel="noopener noreferrer">Code Wooclap : SDQXZU</a>
+    <br>
+    Vendredi - <a href="https://app.wooclap.com/PGDFHY" target="_blank" rel="noopener noreferrer">Code Wooclap : PGDFHY
+    </a><br>
+
+
+    <br><br>
+    <hr>
+
+    <h2>Sass</h2>
+    <?php intlink("../sass/introduction/"); ?>
+    <?php intlink("../sass/imbrication/"); ?>
+    <?php intlink("../sass/variable/"); ?>
+    <?php intlink("../sass/extend/"); ?>
+    <?php intlink("../sass/boucle/"); ?>
+    <p>3 exercices, les liens seront fournis en cours.</p>
+    <br><br>
+    <hr><br><br><br>
+    <?php intlink("../sass/mixins/"); ?>
+    <p>Exercice, le lien sera fourni en cours</p>
+    <del style="opacity: 0.2;"> <?php intlink("../sass/conditions/"); ?> </del>
+    <del style="opacity: 0.2;"> <?php intlink("../sass/fonctions/"); ?> </del>
+   
+    <br><br><br>
+    <hr><br>
+
+    <h2>Devoirs</h2>
+    <p>Compléter les 3 composantes du projet de session suivantes.</p>
+    <?php intlink("../projet/moodboard/"); ?>
+    <?php intlink("../projet/guide-de-styles/"); ?>
+    <?php intlink("../projet/maquette-accueil/"); ?>
+    <br>
+
+    <p>De plus, si ce n'est pas complété en classe, vouus devez terminer les exercices du jour.</p>
+    <ul>
+        <li>SASS - Boucles @for - Images 1 à 6</li>
+        <li>SASS - Boucles @for - Palette Pink Paradise</li>
+        <li>SASS - Boucles @each - Menu Overwatch</li>
+        <li>SASS - Mixins - Grille Bootstrap (Korias)</li>
+    </ul>
+
+
+</wiki-page>
+
+
+
+
+
+
+
+<!--
+  _____                       ____  
+ / ____|                     |___ \ 
+| |     ___  _   _ _ __ ___    __) |
+| |    / _ \| | | | '__/ __|  |__ < 
+| |___| (_) | |_| | |  \__ \  ___) |
+ \_____\___/ \__,_|_|  |___/ |____/ 
+                                          
+-->
+<wiki-page name="Cours 03" date="2023-09-15">
+    <h2>Quiz</h2>
+    Lundi - <a href="https://app.wooclap.com/" target="_blank" rel="noopener noreferrer">Code Wooclap : </a>
     <br>
     Vendredi - <a href="https://app.wooclap.com/" target="_blank" rel="noopener noreferrer">Code Wooclap :
     </a><br>
@@ -60,24 +163,15 @@
     <ol>
         <li>Par équipe, distribuez une lecture par personne.</li>
         <li>Prenez le temps de lire et bien comprendre le sujet qui vous est attitré.</li>
-        <li>Revenez ensuite en groupe et participer à une période de discussion où à tour de rôle chacun présente son sujet et l'explique du mieux possible aux autres afin que ses co-équipiers puisse bien comprendre. Faites des liens entre chaque sujet, discutez-en.</li>
-        <li>Je choisis une personne au hasard dans la classe qui devra m'expliquer en quelques mots tout le processus Agile avec les sujets qui auront été discutés en équipe.</li>
+        <li>Revenez ensuite en groupe et participer à une période de discussion où à tour de rôle chacun présente son
+            sujet et l'explique du mieux possible aux autres afin que ses co-équipiers puisse bien comprendre. Faites
+            des liens entre chaque sujet, discutez-en.</li>
+        <li>Je choisis une personne au hasard dans la classe qui devra m'expliquer en quelques mots tout le processus
+            Agile avec les sujets qui auront été discutés en équipe.</li>
     </ol>
 
-    <br><br><hr>
+    <br><br>
+    <hr>
 
-    <h2>Sass</h2>
-    <?php intlink("../sass/introduction/"); ?>
-    <?php intlink("../sass/imbrication/"); ?>
-    <?php intlink("../sass/variable/"); ?>
-    <?php intlink("../sass/extend/"); ?>
-    <?php intlink("../sass/boucle/"); ?>
-    <p>3 exercices, les liens seront fournis en cours</p>
-    <br><br> <hr><br><br><br>
-    <?php intlink("../sass/mixins/"); ?>
-    <?php intlink("../sass/conditions/"); ?>
-    <?php intlink("../sass/fonctions/"); ?>
-    <p>Exercice, le lien sera fourni en cours</p>
-    <br><br> <hr><br><br><br>
 
 </wiki-page>
