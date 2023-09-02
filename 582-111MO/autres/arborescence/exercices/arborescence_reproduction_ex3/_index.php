594146597b18ba02891f43589229494aedf10e71
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2023-09-02 11:00:10 -0400

    exercices arbo updt

--- 582-111MO/autres/arborescence/exercices/arborescence_reproduction_ex3/_index.php
+++ 582-111MO/autres/arborescence/exercices/arborescence_reproduction_ex3/_index.php
@@ -1,26 +1,12 @@
 <?php 
 /**
  * @type     exercice
- * @title    Arborescence ex 2 & 3
+ * @title    Arborescence ex 3
  * @icon     images/thumb.jpeg
- * @abstract Créer l'arborescence des site web MAC Montréal & Tohu
+ * @abstract Créer l'arborescence du site web dela Tohu
  */
 ?>
 
-<grostitre>MAC Montréal</grostitre>
-
-<p>Votre tâche consiste à créer la structure d'arborescence pour le site web  <em>"MAC Montréal"</em> à l'aide de l'application Draw.io.</p>
-
-<h3>Voici le lien du site</h3>
-
-<doclink href="https://macm.org/">MAC Montréal</doclink>
-
-<checklist>
-    Débuter par le menu de la page d'accueil.
-    N'oubliez pas d'ajouter le menu du footer.
-</checklist>
-
-<dots></dots>
 
 <grostitre>Tohu Montréal</grostitre>
 
