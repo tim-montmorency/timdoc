bab0814d7d6686842020243c862007f2998a6d77
LoraBoisvert <loraboisvert@gmail.com>
2023-09-06 17:22:27 -0400

    Ajout travail 1 VR

--- 582-533MO/wiki/_index.php
+++ 582-533MO/wiki/_index.php
@@ -50,6 +50,24 @@
 
 </wiki-page>
 
+<wiki-page name="Cours 04">
+
+<h2>Liste d'évaluation du CV</h2>
+<p>Créer un document excel avec minimum 20 éléments qui doivent être présents sur le cv de la personne à évaluer. (ex: Nom, prénom, ...) Chaque élément est dans une case. À côté de chaque élément, une case nous permet de mettre un 0 ou un 1 selon la présence ou non de l'élément. À la fin une case fait la somme des bonnes réponses. Une autre transforme le résultat en pourcentage. Exemple:</p>
+<img src="images/excel.png">
+<h2>Temps pour les comités</h2>
+<p>Design: Mise en page d'une invitation pour les employeurs</p>
+<p>Communication: Rédaction de l'invitation pour les employeurs et mise au point de l'horaire final</p>
+<p>Photo: Préparation de la séance photo</p>
+<p>Bouffe: Retour sur le financement</p>
+<p>Studio: Rédaction des courriels pour l'emprunt de matériel. Création de la liste d'équipements</p>
+
+<h2>Rencontres individuelles pour le CV</h2>
+
+<h2>Séance photos</h2>
+
+</wiki-page>
+
 <wiki-page name="Cours 05">
 
 <h2>Visite et conférence chez Sid Lee</h2>
