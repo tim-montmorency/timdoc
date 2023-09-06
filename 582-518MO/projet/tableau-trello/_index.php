270aac6d5570e02563b1bbaeece89b0840b15b6d
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-09-05 23:47:46 -0400

    trello - po

--- 582-518MO/projet/tableau-trello/_index.php
+++ 582-518MO/projet/tableau-trello/_index.php
@@ -7,4 +7,37 @@
  * @index 65
  */
 ?>
-  
+<p>Cette étape concerne le Project Owner <em>(PO)</em> de votre équipe. Vous devrez dans celle-ci créer le tableau <a href="https://trello.com/">Trello</a> qui permettra d’organiser le&nbsp;projet.</p>
+
+<grostitre>Requis</grostitre>
+    
+<checklist>Créez un tableau <a target="_blank" rel="noopener noreferrer" href="https://trello.com/">Trello</a> et nommez le&nbsp;<code>[Nom du client] - [Nom de l'équipe]</code>.<br>ex:&nbsp;<em>Craque Bitume - Les casseroles bruyantes</em>
+Inviter les membres de votre équipe dans le tableau 📊.
+Inviter le client via son adresse courriel personnelle, soit: <em>client@smnarnold.com</em>.
+Créez les cinq colonnes du tableau à partir du modèle <a target="_blank" rel="noopener noreferrer" href="/cours/autres/kanban">Kanban</a> présenté en classe.
+Découpez sous forme de carte les tâches à accomplir afin de réaliser le projet. Lorsqu'une tâche contient des sous-éléments à réaliser, créez une <em>"Checklist"</em> de cesdits éléments. Basez-vous notamment sur <a target="_blank" rel="noopener noreferrer" href="/projets/obnl/requis-clients">les requis client</a>, mais ajoutez aussi toutes autres tâches en lien avec le projet: Démo client, présentation finale, etc.<br><br>Par exemple, dans le cadre d'un jeu, une tâche pourrait s'appeler: <em>"Tableau du joueur"</em> et sa checklist pourrait contenir:<br>- Affichez le nombre de vies restantes ❤️<br>- Affichez le pointage<br>- Affichez le niveau
+Lorsqu'une date limite est connue pour une tâche n'hésitez pas l'inscrire. <br><br>Par exemple, si le tableau avait été réalisé plus tôt, il aurait été possible de créez une carte à propos des maquettes contenant une checklist: <em>"Maquette équipier A"</em>, <em>"Maquette équipier B"</em>, <em>"Maquette équipier C"</em> et ajouter comme date limite la date du cours de la remise des maquettes.
+Créez un libellé intitulé <em>"Bug"</em> afin d'identifier facilement les billets contenant un problème. Créez aussi un libellé intitulé <em>"Changement"</em> afin d'identifier toute demande de changement devant-être porté à l'attention du client. <br><br>Par exemple, si vous changez la police du site, car elle ne supporte pas les accents, il est important d'en aviser le client via une carte libellé <em>"Changement"</em> afin d'éviter que celui-ci assume qu'il s'agisse d'une erreur de la part de votre équipe.  
+</checklist>
+
+
+
+
+<dots></dots>
+<grostitre>Remise</grostitre>
+
+            <p>Il n'y a pas de remise formelle, le fait d'inviter le client constitue la remise en soi.</p>
+
+
+
+
+
+
+
+<dots></dots>
+<grostitre>Notes de cours 📚</grostitre>  
+<?php intlink("../../gestion-de-projet/kanban/") ?>
+<?php intlink("../../gestion-de-projet/trello/") ?>
+
+
+
