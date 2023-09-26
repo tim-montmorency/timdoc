8ba8570f584083f465f8f4064a4c37df7dc743c1
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2023-09-25 21:40:24 -0400

    wiki updt

--- 582-111MO/wiki/_index.php
+++ 582-111MO/wiki/_index.php
@@ -67,8 +67,6 @@
     <?php intlink("../html/formulaire-intermediaire/"); ?>
     <?php intlink("../html/formulaire-intermediaire/exercices/dragonball/"); ?>
     <?php intlink("../html/formulaire-intermediaire/exercices/twitter/"); ?>
-    <?php intlink("../html/emmet/"); ?>
-    <?php intlink("../html/metadonnees/"); ?>
 </wiki-page>
 
 
@@ -79,9 +77,6 @@
     <?php intlink("../html/favicon/exercices/mega-man/"); ?>
     <?php intlink("../html/details-et-summary/"); ?>
     <?php intlink("../html/details-et-summary/exercices/quiz-marc-labreche/"); ?>
-    <?php intlink("../html/open-graph-protocol/"); ?>
-    <?php intlink("../html/microdonnees/"); ?>
-    <?php intlink("../html/microdonnees/exercices/cf-montreal/"); ?>
 </wiki-page>
 
 
