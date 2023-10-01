27cbdb425ce39d15727d2b82dd6358f84a71bf40
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-10-01 19:31:42 -0400

    bg color

---
+++ 582-518MO/exercices/color-run-v219/_index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @type     exercice
+ * @title    Color Run
+ * @icon     images/thumb.png
+ * @abstract Recréer une composante permettant d’afficher le classement final à la Color Run.
+ */
+?>
+
+<p>Pour cet exercice, vous devrez recréer une composante permettant d’afficher le classement final des participants à la <a href="https://thecolorrun.com/" target="_blank">Color&nbsp;Run</a>.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src='medias/color-run-resultat.mp4'></clipasset>
+
+<dots></dots>
+
+<grostitre>Matériel</grostitre>
+<h3  id="Medias 🖼️">Médias 🖼️</h3>
+<mediafile src="https://t.smnarnold.com/color-run/_base.scss">_base.scss</mediafile>
+
+
+<grostitre>Configurations</grostitre>
+<checklist>Créez et nommez un dossier de projet 📁 suivant la nomenclature suivante [nom_prenom_exa337] par exemple <em>ouellet_marie-michelle_exa337</em>.
+Ouvrez maintenant le dossier de projet avec le logiciel Visual Studio Code.
+Dans votre dossier de projet&nbsp;📁, ajoutez un fichier <em>index.html</em>&nbsp;📄 contenant les balises de base d'une page Web.
+Ajoutez-y un sous-dossier <em>css</em>&nbsp;📁, dans lequel se trouvera une feuille de styles <em>styles.css</em>&nbsp;📄 qui sera générée par un fichier <em>.scss</em>&nbsp;📄 du même nom. Vous n'avez PAS le droit d'écrire directement dans le fichier <em>styles.css</em>.
+Collez le code suivant dans votre fichier HTML:</checklist>
+
+<highlight lang='html'></highlight>
+
+
