8f624995e9aba4082f4d39beae4ccd7626898a57
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2023-10-06 12:16:54 -0400

    exam css

---
+++ 582-111MO/html/examens/exam_css/_index.php
@@ -0,0 +1,204 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Examen HTML
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet examen vous allez reproduire un site web.
+ */
+?>
+
+<em>
+    <H2>EXAMEN CSS</H2>
+</em>
+
+<p>Pour cet EXAMEN vous allez reproduire un site web de Clash of clans</p>
+
+<swiper>
+    photos/photo1.webp
+    photos/photo2.webp
+    photos/photo5.webp
+    photos/photo4.webp
+    photos/photo3.webp
+</swiper>
+
+<dots></dots>
+
+<h2>Matériel</h2>
+
+<doclink href="./Gaming Website (examen html).zip">Documents de départ</doclink>
+
+<h2>Vidéo YouTube</h2>
+
+<doclink href="https://www.youtube.com/embed/snqIRxeukm0?si=1KXGxxFvjC2AcCyA">Lien vers la vidéo YouTube</doclink>
+
+<dots></dots>
+
+<h2>Lien pour lié le fichier CSS</h2>
+
+<highlight lang="html"> &lt;link href=&quot;./style.css&quot; rel=&quot;stylesheet&quot;&gt;</highlight>
+
+<h2>Action</h2>
+
+<highlight lang="html">https://tim-montmorency.com/timdoc/582-111MO/html/examens/exam_html/submit/</highlight>
+
+<dots></dots>
+
+<grostitre>Texte</grostitre>
+
+<em>
+        <p>Bienvenue dans notre Communauté de Jeux Vidéo : Où les Mondes Virtuels Deviennent Réalité!</p> 
+        <p>
+            Chez nous, les jeux vidéo ne sont pas seulement un passe-temps, ils sont une passion partagée qui nous unit. Notre communauté est un espace dynamique où les joueurs du
+            monde entier se réunissent pour célébrer leur amour pour les jeux vidéo, quels que soient leurs genres, plateformes ou niveaux d'expérience. Nous sommes bien plus qu'un
+            simple groupe de joueurs, nous sommes une famille virtuelle, unie par notre dévouement à l'univers des jeux vidéo.
+          </p>
+          <br />
+          <p><b>Ce qui fait de notre communauté un endroit spécial :</b></p>
+          <ol>
+            <li>
+              <strong>Diversité des Jeux :</strong> Notre communauté embrasse la richesse de l'univers des jeux vidéo. Que vous soyez fan de RPG, de jeux de tir, de jeux de
+              stratégie, de simulations, de jeux indépendants, ou de tout ce qui se trouve entre les deux, il y a toujours une place pour vous ici. Nous partageons nos expériences,
+              nos découvertes et nos astuces pour explorer ensemble les vastes mondes virtuels qui s'offrent à nous.
+            </li>
+            <br />
+            <li>
+              <strong>Tous Niveaux Confondus :</strong> Peu importe si vous êtes un joueur débutant ou un vétéran chevronné, notre communauté est ouverte à tous. Nous encourageons
+              l'apprentissage, le mentorat et l'entraide. Vous trouverez toujours quelqu'un prêt à vous aider à progresser, à relever des défis ou à partager une partie.
+            </li>
+            <br />
+            <li>
+              <strong>Événements Communautaires :</strong> Nous organisons régulièrement des événements spéciaux, des tournois amicaux et des soirées de jeu en groupe. C'est
+              l'occasion idéale de rencontrer d'autres membres, de <sup>nouer des amitiés</sup> et de découvrir de nouveaux jeux. La compétition est intense, mais l'amusement est
+              notre priorité.
+            </li>
+            <br />
+            <li>
+              <strong>Actualités et Discussions : </strong>Restez informé des dernières actualités du monde du jeu vidéo. Nous fournissons des informations sur les nouvelles
+              sorties, les mises à jour, les astuces et les analyses approfondies. Notre forum est un endroit idéal pour discuter des tendances de l'industrie et des sujets qui
+              vous passionnent.
+            </li>
+            <br />
+            <li>
+              <strong>Respect et Tolérance :</strong> Notre communauté repose sur le respect mutuel et la tolérance. Nous accueillons des joueurs de tous horizons et nous attendons
+              de chaque membre qu'il traite les autres avec <b>respect</b>. Les discriminations et les comportements toxiques n'ont pas leur place ici.
+            </li>
+            <br />
+            <li>
+              Rejoignez-nous dès aujourd'hui et <span>plongez dans un univers où le jeu vidéo devient une <u>expérience sociale</u> inoubliable.</span> Qu'il s'agisse de partager
+              des stratégies, de discuter de la narration d'un jeu, de créer des liens avec des joueurs du monde entier ou de simplement s'amuser, notre communauté vous attend les
+              bras ouverts.
+            </li>
+            <br />
+          </ol>
+          <p>
+            N'oubliez pas de vous présenter sur notre forum et de partager votre histoire de joueur. Vous êtes sur le point de rejoindre une communauté passionnée qui comprend
+            votre amour pour les jeux vidéo. Bienvenue chez vous !
+</em>
+
+<dots></dots>
+
+<grostitre>Les balises principales</grostitre>
+
+<checklist>
+    Ouvrez le document de départ.
+    Dans le fichier index.html, ajouter les balises de départ nécessaire pour un document HTML.
+    Dans la tête de votre fichier index.html, ajouter le titre : MYKD à votre page.
+    Et n'oubliez pas de lié le fichier <em>style.css</em> à votre fichier index.html.
+    Maintenant, dans le <em>body</em>, ajouter les trois balises principales.
+</checklist>
+
+<grostitre>Débutons notre page web</grostitre>
+
+<checklist>
+    Dans l'en-tête, ajoutez l'image <em>logo.png</em>.
+    Suivi d'une <em>navigation</em> (n'oubliez pas les balises de navigation) à l'aide d'une <em>liste non ordonnée </em>qui contiendra 6 balises ancres (a). <small>(N'oubliez pas de mettre le # comme lien href)</small> <em>Accueil, À propos de nous, Tournoi, Pages, Nouvelles et Contact</em>.
+    Maintenant, dans la partie principale <small>(le contenu)</small>, ajouter l'image <em>banner.webp</em>.
+    Ajoutez un titre h5 avec le texte suivant : <em>Membre de notre équipe</em>.
+    Ajoutez un titre h2 avec le texte suivant :<em> Membre actif de notre équipe</em>.
+    Ensuite, ajouter un tableau qui aura <em>2 rangées</em>. La première rangée contient <em>4 colonnes</em> avec les images : team01, team02, team03 et team04.
+    La 2e rangée contient <em>2 colonnes fusionnées</em>. Les deux premières colonnes fusionnées contiennent le texte : <em>Notre meilleur joueur ➠</em> en h2.
+    Les <em>2 dernières colonnes fusionnées</em> contiennent l'image : <em>meilleur.webp</em>
+    Par la suite, créer une <em>div</em>.
+</checklist>
+
+<grostitre>La colonne de gauche</grostitre>
+
+<checklist>
+    Dans la <em>div</em>, ajouter une<em> balise </em>représentant un <em>groupe d'éléments ayant un lien indirect avec le sujet principal</em> de la page.
+    Écrivez le titre en h2 : <em>Nos jeux</em>.
+    Ajoutez une <em>liste non ordonnée</em> qui contiendra 6 balises ancres <em>(a)</em>. <small>(N'oubliez pas de mettre le # comme lien href)</small> <em>Hordes, Kingdom Rush, Happy Wheels, Forge of Empires, Dofus et Slither</em>.
+    Maintenant, créez un bloc de citation contenant le texte suivant entre des balises <em>q</em> : <em>Rien n'est vrai. Tout est permis</em> et citez l'auteur : <em>Assassin’s Creed</em>. (Assurez-vous de bien vérifier comment écrire un bloc de citation)
+    Suivi de la <em>vidéo YouTube</em> ci-dessus avec une <em>largeur de 350</em> et une <em>hauteur de 197</em>
+    Fermez votre <em>groupe d'éléments ayant un lien indirect avec le sujet</em> à l'aide de sa balise fermante.
+</checklist>
+
+<grostitre>La colonne de droite</grostitre>
+
+<checklist>
+    Créez une section.
+    Dans votre section, ajoutez un titre h3 avec le texte : <em>Bienvenue dans notre Communauté de Jeux Vidéo : Où les Mondes Virtuels Deviennent Réalité!</em>
+    Suivi d'un paragraphe : <em>Chez nous, les jeux vidéo ne sont pas seulement un passe-temps, ils sont une passion partagée qui nous unit. Notre communauté est un espace dynamique où les joueurs du monde entier se réunissent pour célébrer leur amour pour les jeux vidéo, quels que soient leurs genres, plateformes ou niveaux d'expérience. Nous sommes bien plus qu'un simple groupe de joueurs, nous sommes une famille virtuelle, unie par notre dévouement à l'univers des jeux vidéo.</em>
+    Ajoutez un <em>saut de ligne</em>, suivi d'un <u>paragraphe en caractère gras</u> : <em>Ce qui fait de notre communauté un endroit spécial :</em>
+    Maintenant, ajoutez une <em>liste ordonnée</em> séparée par des <em>sauts de ligne</em> après chaque élément de la liste.
+    Ajoutez les paragraphes ci-dessus.
+    Dans chacun des paragraphe, les termes :<em> Diversité des Jeux :  Tous Niveaux Confondus :  Événements Communautaires :  Actualités et Discussions : et Respect et Tolérance :</em> sont formattés en <u>Texte important</u> <small>(indique que le texte a une importance particulière ou un certain sérieux voire un caractère urgent.)</small>
+    Dans le 3e élément de la liste, ajoutez le texte : <em>nouer des amitiés</em> c'est un texte mis en exposant.
+    Dans le 5e élément de la liste, ajoutez le texte : <em>respect</em> c'est un texte mis en caractère gras.
+    Dans le 6e élément de la liste, ajoutez le texte : <em>plongez dans un univers où le jeu vidéo devient une expérience sociale inoubliable</em>  c'est un <em>span</em> et à l'intérieur de ce texte, <em>expérience sociale</em> est <u>souligné</u>.
+    Une fois la liste terminée, ajoutez un paragraphe avec le texte : <em>N'oubliez pas de vous présenter sur notre forum et de partager votre histoire de joueur. Vous êtes sur le point de rejoindre une communauté passionnée qui comprend votre amour pour les jeux vidéo. Bienvenue chez vous !</em>
+    N'oubliez pas de fermer votre section et votre <em>div</em>.
+</checklist>
+
+<dots></dots>
+
+<grostitre>L'infolettre</grostitre>
+
+<checklist>
+    Ajoutez un titre en <em>h2</em> et le texte est : <em>Inscrivez-vous à notre infolettre</em>
+    Ajoutez une forme avec l'<em>action</em> ci-dessus.
+    Ajoutez un champ de <em> texte "requis" </em> avec le <u>libellé : Prénom</u> ainsi qu'un<span class="inline-code">id</span>="prenom", <span class="inline-code">name</span>="prenom" et <span class="inline-code">placeholder</span>="Entrez votre prénom"
+    Ajoutez un champ de <em> texte "requis" </em> avec le <u>libellé : Nom</u> ainsi qu'un<span class="inline-code">id</span>="nom", <span class="inline-code">name</span>="nom" et <span class="inline-code">placeholder</span>="Entrez votre nom"
+    Ajoutez un champ de <em>courriel "requis" </em> avec le <u>libellé : Courriel</u> ainsi qu'un<span class="inline-code">id</span>="email", <span class="inline-code">name</span>="email" et <span class="inline-code">placeholder</span>="Entrez votre courriel"
+    Ajoutez un champ <em> date </em> avec le <u>libellé : Date</u> ainsi qu'un<span class="inline-code">id</span>="date", <span class="inline-code">name</span>="date", un minimum "1980-01-01" et un maximum "2000-01-01"
+    Ajoutez une boîtes à cocher </em> avec le <u>libellé : Je veux recevoir l'infolettre.</u> <span class="inline-code">name</span>="remember", <span class="inline-code">id</span>="remember" 
+    Finalement, ajoutez un bouton afin de soumettre le formulaire avec le texte : <em>Soumettre</em> et <span class="inline-code">id</span>="bouton" 
+    
+</checklist>
+
+<dots></dots>
+
+<grostitre>Le pied de page</grostitre>
+
+<checklist>
+    Finalement, ajouter au pied de page la phrase suivante : <em>Tous droits réservés Collège Montmorency 2023</em>.
+    Suivi d'une <em>navigation</em> à l'aide d'une <em>liste non ordonnée </em>qui contiendra 6 balises ancres <em>(a)</em>. <small>(N'oubliez pas de mettre le # comme lien href)</small> <em>Jeux, Produit, Tous les NFT, Réseau social, Support d'aide et Contact.</em>
+</checklist>
+
+<dots></dots>
+
+<grostitre>Remise</grostitre>
+
+<checklist>
+  Renommez votre dossier <em>"exam_p_[nom]_[prenom]"</em>.
+  Zippez votre dossier <em>"exam_p_[nom]_[prenom]"</em>.
+  Joignez votre fichier <em>.zip</em> au devoir <em>"Examen Pratique: HTML"</em>.
+</checklist>
+
+<dots></dots>
+
+<grostitre>Notes de cours 📚</grostitre>
+
+<?php intlink("../../ancre/"); ?>
+<?php intlink("../../balises/"); ?>
+<?php intlink("../../semantique/"); ?>
+<?php intlink("../../div-span/"); ?>
+<?php intlink("../../formatage-html/"); ?>
+<?php intlink("../../image/"); ?>
+<?php intlink("../../iframe/"); ?>
+<?php intlink("../../tableau/"); ?>
+<?php intlink("../../liste/"); ?>
+<?php intlink("../../formulaire/"); ?>
+<?php intlink("../../formulaire-intermediaire/"); ?>
+<?php intlink("../../formulaire-avance/"); ?>
+
+<dots></dots>
