72b5e6871d2c0b79e4ce9cffce7495f0b80f2254
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-10-29 18:30:13 -0400

    cours 10

---
+++ 582-518MO/exercices/assurance-qualite-insoapropriate/_index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @type     exercice
+ * @title    Assurance-qualité - Insoapropriate
+ * @icon     images/thumb-mixin.jpg
+ * @abstract Examiner attentivement une réalisation et vous assurez qu’elle correspond bien aux requis client. 
+ */
+?>
+
+<p>Pour cet exercice vous devrez examiner attentivement une réalisation et vous assurez qu’elle correspond bien aux requis client. Toute anomalie devra être soulignée via un billet de&nbsp;<em>“Bug”</em>.</p>
+
+
+    
+<dots></dots>
+<grostitre>Matériel</grostitre>   
+  
+<doclink href='https://ex.smnarnold.com/qa/insoapropriate/maquettes.zip'>Dossier de départ 📁</doclink>
+
+
+<dots></dots>
+<grostitre>Requis</grostitre>
+
+<checklist>Créez-vous une copie du <a target="_blank" rel="noopener noreferrer" href="https://trello.com/b/CdivA2RL/insoapropriate" onclick="event.stopPropagation()">tableau Trello du projet</a> <em>(Menu en haut à droite &gt; […] &gt; Copier le tableau)</em>. 
+Comparez les requis et les maquettes dans le dossier de départ&nbsp;au <a target="_blank" rel="noopener noreferrer" href="http://ex.smnarnold.com/qa/insoapropriate/"  onclick="event.stopPropagation()">site d'Insoapropriate</a>&nbsp;et entrez les bogues&nbsp;trouvés.</checklist>
+
+<dots></dots>
+<grostitre>Notes de cours 📚</grostitre>
+
+
+<?php intlink("../../gestion-de-projet/assurance-qualite/") ?>
+
