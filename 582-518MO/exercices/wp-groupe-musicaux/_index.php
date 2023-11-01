e09b9273eed2c6d2bbdb965a96cbc7837c2cf58a
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-11-01 19:21:26 -0400

    exercice groupe musicaux

---
+++ 582-518MO/exercices/wp-groupe-musicaux/_index.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @type     exercice
+ * @title    Wordpress - Groupes musicaux
+ * @icon     images/
+ * @abstract Vous evez créer un microsite permettant de présenter les informations de quelques groupes de musique.
+ */
+?>
+
+
+<p>Pour cet exercice vous et votre coéquipier devez créer un microsite permettant de présenter les groupes de musiques suivant: </p>
+
+
+<p>Votre coéquipier a conçu un mockup HTML sur CodePen, ainsi que le thème WordPress du site. Il compte maintenant sur vous pour le rendre dynamique dans&nbsp;WordPress.</p>
+
+<p>Aperçu du résultat 👇</p>
+<clipasset src=''></clipasset>
+
+
+
+
+
+<dots></dots>
+<grostitre>Matériel</grostitre>
+<doclink href='#'>Pen de départ</doclink>
+
+
+<h3 class="heading heading--h3" id="Médias 🖼️">Médias 🖼️</h3>
+<mediafile src="images/"></mediafile>
+<mediafile src="images/"></mediafile>
+<mediafile src="images/"></mediafile>
+
+<dots></dots>
+<grostitre>Requis</grostitre>
+<checklist>Installez en local une copie en <a target="_blank" rel="noopener noreferrer" href="https://fr-ca.wordpress.org/">Français du Canada de WordPress</a> pour votre client.
+Installez le <a target="_blank" rel="noopener noreferrer" href="https://ex.smnarnold.com/wordpress/valet-de-coeur/valet-de-coeur.zip">thème réalisé par votre collègue</a>. Si vous l'avez installé et vous le cherchez, sachez que c'est le thème qui se nomme "Montmorency".
+Installez l'extension <a target="_blank" rel="noopener noreferrer" href="https://fr-ca.wordpress.org/plugins/custom-post-type-ui/">Custom Post Type UI</a> et créez un nouveau type de post intitulé <em>"Jeu"</em>. Ce nouveau type de post contiendra tous les jeux de société disponibles chez Le Valet D'Coeur.
+Installez l'extension <a target="_blank" rel="noopener noreferrer" href="https://fr-ca.wordpress.org/plugins/advanced-custom-fields/">Advanced Custom fields</a> et créez un nouveau groupe de champs ne s'affichant que dans les posts de type <em>"Jeu"</em>. Ce groupe devra contenir des champs permettant de spécifier le prix, la durée moyenne, le nombre de joueurs et l'âge minimal requis pour jouer au jeu.
+</checklist>
+
+      
+
+
+<dots></dots>
+<grostitre>Notes de cours 📚</grostitre>
+<?php intlink("../../wordpress/posts-personnalises/") ?>
+<?php intlink("../../wordpress/advanced-custom-fields/") ?>
+<?php intlink("../../wordpress/menus/") ?>
+<?php intlink("../../wordpress/wp-multilang/") ?>
