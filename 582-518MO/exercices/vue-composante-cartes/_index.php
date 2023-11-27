69aa45f300851800f10f88db11b4259349b6ad9c
kid-synthetique <mariemichelle.ouellet@gmail.com>
2023-11-27 14:11:00 -0500

    exercice composante carte

---
+++ 582-518MO/exercices/vue-composante-cartes/_index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @type     exercice
+ * @title    Vue - Composante - Cartes de produit
+ * @icon     images/result.jpg
+ * @abstract Cartes de produit
+ */
+?>
+
+<p>Pour cet exercice, vous aller générer 3 cartes produits à l'aide de composantes Vue.</p>
+<p>Aperçu du résultat 👇</p>
+<img src="images/result.jpg" alt="">
+<dots></dots>
+<grostitre>Matériel</grostitre>
+<doclink href='https://codepen.io/tim-momo/pen/mdvjpwy'>Pen de départ</doclink>
+
+<dots></dots>
+<grostitre>Requis</grostitre>
+<checklist>Créez une app Vue.
+Faites en sorte que chacune des 3 cartes est une réutilisation d'une composante <em>card</em> que vous allez créer.
+Utilisez les props pour personnaliser le contenu de chaque carte.
+</checklist>         
+
+
+<dots></dots>
+<grostitre>Notes de cours 📚</grostitre>
+<?php intlink("../../javascript/vue-js/creation-app-bases") ?>
+<?php intlink("../../javascript/vue-js/composantes") ?>
