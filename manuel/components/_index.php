b3226ad37c237772074a8506a26cd0aa169a3098
Maxime Larrivée-Roy <zmotrin@gmail.com>
2024-01-18 15:04:05 -0500

    Fix

--- manuel/components/_index.php
+++ manuel/components/_index.php
@@ -12,62 +12,141 @@
 
 <dots></dots>
 
-<grostitre>info / warning / alert</grostitre>
-<highlight lang="html">&lt;info&gt;Ceci est une bulle d'information&lt;/info&gt;</highlight>
+
+<grostitre>Titre de section</grostitre>
+
+<h3>grostitre</h3>
+
+<p>La composante <span class="inline-code">&lt;grostitre&gt;</span> remplace les <span class="inline-code">&lt;h1&gt;</span>, créent une ancre et l'ajoute à la table des matières automatiquement.<br><br>Exemple de cette section:</p>
+
+<highlight lang="html"><grostitre>Titre de section</grostitre></highlight>
+
+
+
+<h3>dots</h3>
+
+<p>La composante <incode>&lt;dots&gt;</incode> est utilisée pour séparer les différentes sections d'une page.</p>
+
+<highlight lang="html">&lt;dots&gt;&lt;/dots&gt;</highlight>
+
+<dots></dots>
+
+
+<grostitre>Bulles d'informations</grostitre>
+
+<h3>info</h3>
+<highlight lang="html"><info>Ceci est une bulle d'information</info></highlight>
 <info>Ceci est une bulle d'information</info>
 <br><br>
-<highlight lang="html">&lt;warning&gt;Ceci est une bulle d'avertissement&lt;/warning&gt;</highlight>
+
+<h3>warning</h3>
+<highlight lang="html"><warning>Ceci est une bulle d'avertissement</warning></highlight>
 <warning>Ceci est une bulle d'avertissement</warning>
 <br><br>
-<highlight lang="html">&lt;alert&gt;Ceci est une bulle d'alerte&lt;/alert&gt;</highlight>
+
+<h3>alert</h3>
+<highlight lang="html"><alert>Ceci est une bulle d'alerte</alert></highlight>
 <alert>Ceci est une bulle d'alerte</alert>
+<br><br>
+
+<h3>thumbsup</h3>
+<highlight lang="html"><thumbsup>Ceci est une bulle d'approbation</thumbsup></highlight>
+<thumbsup>Ceci est une bulle d'approbation</thumbsup>
+<br><br>
+
+<h3>bravo</h3>
+<highlight lang="html"><bravo>Ceci est une bulle d'applaudissement</bravo></highlight>
+<bravo>Ceci est une bulle d'applaudissement</bravo>
+
 <dots></dots>
 
-<grostitre>grostitre</grostitre>
-<p>La composante <span class="inline-code">&lt;grostitre&gt;</span> remplace les <span class="inline-code">&lt;h1&gt;</span>, créent une ancre et l'ajoute à la table des matières automatiquement.<br><br>Exemple de cette section:</p>
-<highlight lang="html">&lt;grostitre&gt;grostitre&lt;/grostitre&gt;</highlight>
-<dots></dots>
 
-<grostitre>mediafile</grostitre>
-<highlight lang="html">&lt;mediafile src="images/icon.png"&gt;Icône&lt;/mediafile&gt;</highlight>
+<grostitre>Liens médias</grostitre>
+
+<h3>mediafile</h3>
+<highlight lang="html"><mediafile src="images/icon.png">Icône</mediafile></highlight>
 <mediafile src="images/icon.png">Icône</mediafile>
+
+<h3>doclink</h3>
+<highlight lang="html">
+<doclink href="https://www.w3schools.com/cssref/css3_pr_flex-direction.php">flex-direction</doclink>
+<doclink href="https://developer.mozilla.org/fr/docs/Web/CSS/flex-direction">flex-direction</doclink>
+</highlight>
+<doclink href="https://www.w3schools.com/cssref/css3_pr_flex-direction.php">flex-direction</doclink>
+<doclink href="https://developer.mozilla.org/fr/docs/Web/CSS/flex-direction">flex-direction</doclink>
+
+<h3>intlink</h3>
+<highlight lang="html">&lt;intlink href=&quot;../../582-215MO/css/exercices/lost-highway/&quot;&gt;&lt;/intlink&gt;</highlight>
+<intlink href="../../582-215MO/css/exercices/lost-highway/"></intlink>
+
+<h3>exercice</h3>
+<highlight lang="html">&lt;exercice href="exercices/flexbox-defense"&gt;&lt;/exercice&gt;</highlight>
+<exercice href="exercices/flexbox-defense"></exercice>
+
+<h3>tool</h3>
+<highlight lang="html">&lt;tool href="tools/css-gradient"&gt;&lt;/tool&gt;</highlight>
+<tool href="tools/css-gradient"></tool>
+
+
+<h3>knowmore</h3>
+<highlight lang="html">
+<knowmore href="https://fr.wikipedia.org/wiki/Monotremata">
+    Les monotrèmes (Monotremata) constituent un ordre animal défini par
+    Charles-Lucien Bonaparte en 1838, regroupant des espèces caractérisées
+    par le fait d'être à la fois ovipares et mammifères : ils pondent des œufs
+    mais allaitent leurs petits.
+</knowmore>
+</highlight>
+<knowmore href="https://fr.wikipedia.org/wiki/Monotremata">
+    Les monotrèmes (Monotremata) constituent un ordre animal défini par
+    Charles-Lucien Bonaparte en 1838, regroupant des espèces caractérisées
+    par le fait d'être à la fois ovipares et mammifères : ils pondent des œufs
+    mais allaitent leurs petits.
+</knowmore>
+
 <dots></dots>
+
+
+<grostitre>Surbrillance</grostitre>
+
+<h3>highlight</h3>
+<highlight lang="html">&lt;highlight lang="javascript"&gt;console.log("Ceci est du javascript");&lt;/highlight&gt;</highlight>
+<highlight lang="javascript">console.log("Ceci est du javascript");</highlight>
+
+<doclink href="https://github.com/highlightjs/highlight.js/blob/main/SUPPORTED_LANGUAGES.md ">Languages supportés</doclink>
+
+<dots></dots>
+
+
+<grostitre>Citation</grostitre>
+
+<h3>quote</h3>
+
+<highlight lang="html">
+<quote author="Gandalf" title="Magicien" photo="./images/gandalf.webp">
+    Un magicien n'est jamais en retard, ni en avance d'ailleurs Frodon Sacquet.
+    Il arrive précisément à l'heure prévue.
+</quote>
+</highlight>
+
+<quote author="Gandalf" title="Magicien" photo="./images/gandalf.webp">
+    Un magicien n'est jamais en retard, ni en avance d'ailleurs Frodon Sacquet. Il arrive précisément à l'heure prévue.
+</quote>
+
+<dots></dots>
+
+
 
 <grostitre>codepen</grostitre>
 <highlight lang="html">&lt;codepen id="gOEwrvG" tab="results"&gt;Voici une remarque pertinente.&lt;/codepen&gt;</highlight>
 <codepen id="gOEwrvG" tab="results">Voici une remarque pertinente.</codepen>
 <dots></dots>
 
-<grostitre>exercice</grostitre>
-<highlight lang="html">&lt;exercice href="exercices/flexbox-defense"&gt;&lt;/exercice&gt;</highlight>
-<exercice href="exercices/flexbox-defense"></exercice>
-<dots></dots>
 
-<grostitre>tool</grostitre>
-<highlight lang="html">&lt;tool href="tools/css-gradient"&gt;&lt;/tool&gt;</highlight>
-<tool href="tools/css-gradient"></tool>
-<dots></dots>
 
-<grostitre>knowmore</grostitre>
-<highlight lang="html">&lt;knowmore href="https://fr.wikipedia.org/wiki/Monotremata"&gt;
-    Les monotrèmes (Monotremata) constituent un ordre animal défini par
-    Charles-Lucien Bonaparte en 1838, regroupant des espèces caractérisées
-    par le fait d'être à la fois ovipares et mammifères : ils pondent des œufs
-    mais allaitent leurs petits.
-&lt;/knowmore&gt;</highlight>
-<knowmore href="https://fr.wikipedia.org/wiki/Monotremata">Les monotrèmes (Monotremata) constituent un ordre animal défini par Charles-Lucien Bonaparte en 1838, regroupant des espèces caractérisées par le fait d'être à la fois ovipares et mammifères : ils pondent des œufs mais allaitent leurs petits.</knowmore>
-<dots></dots>
 
-<grostitre>doclink</grostitre>
-<highlight lang="html">&lt;doclink href="https://www.w3schools.com/cssref/css3_pr_flex-direction.php"&gt;flex-direction&lt;/doclink&gt;
-&lt;doclink href="https://developer.mozilla.org/fr/docs/Web/CSS/flex-direction"&gt;flex-direction&lt;/doclink&gt;</highlight>
-<doclink href="https://www.w3schools.com/cssref/css3_pr_flex-direction.php">flex-direction</doclink>
-<doclink href="https://developer.mozilla.org/fr/docs/Web/CSS/flex-direction">flex-direction</doclink>
-<dots></dots>
 
-<grostitre>dots</grostitre>
-<highlight lang="html">&lt;dots&gt;&lt;/dots&gt;</highlight>
-<dots></dots>
+
 
 <grostitre>color</grostitre>
 <highlight lang="html">&lt;color&gt;#e8b9ff&lt;/color&gt;
@@ -80,7 +159,7 @@
 
 <grostitre>clip</grostitre>
 <highlight lang="html">&lt;clip src="videos/le-boson-de-higgs.mp4"&gt;&lt;/clip&gt;</highlight>
-<clip src="videos/le-boson-de-higgs.mp4"></clip>
+<clip src="videos/le-boson-de-higgs.mp4" title="Qu'est-ce que le boson de Higgs?"></clip>
 <doclink href="../videos">Manuel / Vidéos</doclink>
 <dots></dots>
 
@@ -96,10 +175,7 @@
 <doclink href="../audios">Manuel / Audios</doclink>
 <dots></dots>
 
-<grostitre>highlight</grostitre>
-<highlight lang="html">&lt;highlight lang="javascript"&gt;console.log("Ceci est du javascript");&lt;/highlight&gt;</highlight>
-<highlight lang="javascript">console.log("Ceci est du javascript");</highlight>
-<dots></dots>
+
 
 <grostitre>checklist</grostitre>
 <highlight lang="html">&lt;checklist&gt;
@@ -125,27 +201,27 @@
 
 <grostitre>swiper</grostitre>
 <highlight lang="html">&lt;swiper&gt;
-    photos/01.jpg
-    photos/02.jpg
-    photos/03.jpg
-    photos/04.jpg
-    photos/05.jpg
-    photos/06.jpg
-    photos/07.jpg
-    photos/08.jpg
-    photos/09.jpg
-    photos/10.jpg
+    photos/01.webp
+    photos/02.webp
+    photos/03.webp
+    photos/04.webp
+    photos/05.webp
+    photos/06.webp
+    photos/07.webp
+    photos/08.webp
+    photos/09.webp
+    photos/10.webp
 &lt;/swiper&gt;</highlight>
 <swiper>
-    photos/01.jpg
-    photos/02.jpg
-    photos/03.jpg
-    photos/04.jpg
-    photos/05.jpg
-    photos/06.jpg
-    photos/07.jpg
-    photos/08.jpg
-    photos/09.jpg
-    photos/10.jpg
+    photos/01.webp
+    photos/02.webp
+    photos/03.webp
+    photos/04.webp
+    photos/05.webp
+    photos/06.webp
+    photos/07.webp
+    photos/08.webp
+    photos/09.webp
+    photos/10.webp
 </swiper>
 <dots></dots>
