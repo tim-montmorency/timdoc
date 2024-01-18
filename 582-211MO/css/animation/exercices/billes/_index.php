deac17fd692284b5f1186c94ca041f6b7e08aaf0
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2024-01-18 10:55:51 -0500

    css

---
+++ 582-211MO/css/animation/exercices/billes/_index.php
@@ -0,0 +1,44 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Animation - Billes
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice nous allons animer 5 billes blanches.
+ */
+?>
+
+<p class="spacer">Pour cet exercice nous allons animer 5 billes blanches.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<clipasset src="videos/animation-billes.mp4"></clipasset>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+<doclink href="https://codepen.io/tim-momo/pen/QWBomjM?editors=1100">Pen de départ</doclink>
+
+<warning>Vous n’avez qu’à ajouter du CSS <em>(nul besoin de modifier le HTML ou le JavaScript)</em>.</warning>
+
+<dots></dots>
+
+
+<grostitre>Requis</grostitre>
+
+<checklist>
+  Chaque bille doit descendre de 85% de la hauteur de la fenêtre ⏬ en l'espace d'une seconde ⏱.
+  Les billes doivent s'animer l'une à la suite de l'autre de façon décalée et rester au bas de la fenêtre lorsque leur animation est complétée.
+  Toutes les billes doivent utiliser <strong>la même animation</strong>.
+</checklist>
+
+<dots></dots>
+
+
+<grostitre>Notes de cours 📚</grostitre>
+
+<intlink href="../../../transformation/"></intlink>
+<intlink href="../../../animation/"></intlink>
+
+<dots></dots>
---
+++ 582-211MO/css/background/exercices/candy-crush/_index.php
@@ -0,0 +1,72 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Background - Candy Crush
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devrez recréer des mises en page inspirées du jeux Candy Crush en mettant à profit vos connaissances sur la propriété CSS background.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous devrez recréer des mises en page inspirées du jeux <a target="_blank" href="https://fr.wikipedia.org/wiki/Candy_Crush_Saga">Candy Crush</a> en mettant à profit vos connaissances sur la propriété CSS <incode>background</incode>.</p>
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+<doclink href="https://codepen.io/tim-momo/pen/oNMyKpj?editors=0100">Pen de départ</doclink>
+
+<h3>Médias</h3>
+
+<mediafile src="images/1.png">Bonbon rose</mediafile>
+<mediafile src="images/2.png">Bonbon rouge</mediafile>
+<mediafile src="images/3.png">Bonbon bleu</mediafile>
+<mediafile src="images/4.png">Bonbon jaune</mediafile>
+<mediafile src="images/5.png">Bonbon orange</mediafile>
+<mediafile src="images/bg.png">Fond</mediafile>
+
+<dots></dots>
+
+
+<grostitre>Requis</grostitre>
+
+<p>Le HTML et le CSS de base est fournis dans le Pen de départ. Pour chaque mise en page à réaliser, vous devrez:</p>
+
+<ul>
+  <li>Faire un fork du Pen de départ afin de repartir du début à chaque fois.</li>
+  <li>Nommer chacune des copies en fonction de l'étape actuelle. Par exemple, pour l'étape 1, nommer votre pen: <em>Candy Crush - Étape 1</em>.</li>
+  <li>Pour chaque étape, configurer la propriétés de <incode>background</incode> de l'élément <incode>.game</incode> afin de reproduire la mise en page fournie en capture d'écran.</li>
+  <li>En tout temps, les bonbons doivent avoir une dimension de 50x50px.</li>
+</ul>
+
+<warning>Vous ne devez en aucun cas modifier le HTML!</warning>
+
+<checklist>
+  Étape 1<br><img src="images/candy-crush-1.jpg">
+  Étape 2<br><img src="images/candy-crush-2.jpg">
+  Étape 3<br><img src="images/candy-crush-3.jpg">
+  Étape 4<br><img src="images/candy-crush-4.jpg">
+  Étape 5<br><img src="images/candy-crush-5.jpg">
+  Étape 6<br><img src="images/candy-crush-6.jpg">
+  Étape 7<br><img src="images/candy-crush-7.jpg">
+  Étape 8<br><img src="images/candy-crush-8.jpg">
+  Étape 9<br><img src="images/candy-crush-9.jpg">
+  Étape 10<br>Attention de faire en sorte de respecter la dimension et la position de l'image de fond. Fiez-vous à la position du bateau dans l'image.<br><img src="images/candy-crush-10.jpg">
+</checklist>
+
+<dots></dots>
+
+
+<grostitre>Notes de cours 📚</grostitre>
+
+<intlink href="../../../background/"></intlink>
+
+<dots></dots>
+
+
+
+
+
+
+
+
