212cfe6f3fc267dd3bfe480438f06c37087a6217
Maxime Larrivée-Roy <zmotrin@gmail.com>
2024-01-22 11:45:55 -0500

    Index breadcrumb

--- _includes/article_header.php
+++ _includes/article_header.php
@@ -1,6 +1,9 @@
 
             <div id="subhead">
-                <div id="breadcrumb"><?php print_breadcrumb(); ?></div>
+                <div id="breadcrumb">
+                    <div id="breadcrumb_normal"><?php print_breadcrumb(); ?></div>
+                    <div id="breadcrumb_index"><?php print_breadcrumb_index(); ?></div>
+                </div>
                 <h1 id="title"><img src="<?php echo $PAGE->icon?>">&nbsp;<?php echo $PAGE->title; ?></h1>
             </div>
             <article>
