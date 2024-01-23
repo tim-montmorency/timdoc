b8f2e65c86b9009e4110f4cd23fcdb9e1154d9e8
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2024-01-22 21:50:56 -0500

    autres

---
+++ 582-211MO/autres/actions-photoshop/exercices/kirby/_index.php
@@ -0,0 +1,50 @@
+<?php 
+/**
+ * @type     exercice
+ * @title    Actions Photoshop - Kirby
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice, vous devez télécharger un dossier de départ et traiter plusieurs images à l’aide d’un script Photoshop.
+ */
+?>
+
+<p class="spacer">Pour cet exercice, vous devez télécharger un dossier de départ et traiter plusieurs images à l’aide d’un script Photoshop.</p>
+
+<p>Aperçu du résultat 👇</p>
+
+<img src="images/actions-photoshop-kirby.png">
+
+<dots></dots>
+
+
+<grostitre>Matériel</grostitre>
+
+<doclink href="./kirby.zip">Dossier de départ</doclink>
+
+<dots></dots>
+
+
+<grostitre>Requis</grostitre>
+
+<checklist>
+    Redimensionner chaque paysage afin qu'il soit d'une largeur de 800px.
+    Transformez le paysage en noir et blanc.
+    Ajoutez Kirby par-dessus le paysage. Kirby devra avoir une largeur de 100px, être parfaitement centré horizontalement et se situer à 30px de bas du paysage.
+    Sauvegardez l'image dans un dossier sur votre bureau 📁.
+</checklist>
+
+<dots></dots>
+
+
+<grostitre>Notes de cours 📚</grostitre>
+
+<intlink href="../../../actions-photoshop/"></intlink>
+
+<dots></dots>
+
+
+
+
+
+
+
+
---
+++ 582-211MO/autres/vectoriel-vs-matriciel/_index.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @type     article
+ * @title    Vectoriel vs Matriciel
+ * @icon     images/icon.png
+ * @abstract matrices & vecteurs
+ */
+?>
+
+<grostitre>Vectoriel</grostitre>
+
+<p>Comme leur nom l'indique, les images vectorielles sont constituées de vecteurs. Ces vecteurs peuvent prendre la forme de rectangle, cercle, polygone, etc. Bref, de toutes formes géométriques pouvant être définies avec des coordonnées, des lignes droites ou des courbes de Béziers.</p>
+
+<img src="images/svg-vectorielle.png">
+
+<p>Ces images sont produites à l'aide des logiciels:</p>
+
+<ul>
+    <li><a target="_blank" href="https://www.adobe.com/ca_fr/products/illustrator.html">Illustrator</a></li>
+    <li><a target="_blank" href="https://www.sketch.com/">Sketch</a></li>
+    <li><a target="_blank" href="https://www.figma.com/">Figma</a></li>
+    <li>etc.</li>
+</ul>
+
+<dots></dots>
+
+
+<grostitre>Matriciel</grostitre>
+
+<p>À l'opposée les images dites matricielles <em>(jpg, png, gif, webp, etc.)</em> sont constituées d'une matrice de pixels permettant de reproduire une image.</p>
+
+<img src="images/svg-matricelle.png">
+
+<p>Plus une image est en haute définition, plus elle contient de pixels dans un même espace. Cependant, plus une image contient de pixels, plus sont poids augmente.</p>
+
+<img src="images/batman-hd-4k.jpg">
+
+<p>Ces images sont produites à l'aide des logiciels:</p>
+
+<ul>
+    <li><a target="_blank" href="https://www.adobe.com/ca/products/photoshop/landpa.html">Photoshop</a></li>
+    <li><a target="_blank" href="https://www.gimp.org/">Gimp</a></li>
+    <li><a target="_blank" href="https://ms-paint.fr.softonic.com/">Paint</a></li>
+    <li>etc.</li>
+</ul>
+
+<info>Le nom Pixels est la contraction des abréviations "Pics" <em>(picture)</em> et "el" <em>(elements)</em>.</info>
+
+<dots></dots>
