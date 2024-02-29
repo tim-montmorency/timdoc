598dfb553a1c115be19ea43c7abfbcec051a6cf9
LoraBoisvert <95651493+LoraBoisvert@users.noreply.github.com>
2024-02-29 12:14:08 -0500

    Ajouts exercices typo

---
+++ 582-214MO/exercices_ae/09_3d_lettre/_index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @type     exercice
+ * @title    Lettre en 3D
+ * @icon     images/thumb.jpg
+ * @abstract Cet exercice vous permettra de créer un effet de 3d sur vos textes. 
+ */
+?>
+
+<p class="spacer"> Cet exercice vous permettra de créer un effet de 3d sur vos textes.  </p>
+<dots></dots>
+
+
+<grostitre>Étapes</grostitre>
+<checklist>
+Créer une nouvelle composition.
+Écrire une lettre dans votre composition avec l'outil texte.
+Clic-droit sur le calque de votre lettre, créer, créer des formes à partir du texte.
+Copier-coller le nouveau calque et renommer le contour. 
+Assurez-vous de placer votre calque de contour sous l'autre calque et donnez-lui la couleur de votre choix. 
+Ouvrez le calque contour et sélectionnez le bouton ajouter de contenu, puis répétition.
+Ouvrez Répétition 1, puis ajouter des copies. 
+Ouvrez ensuite Transformer: Répétition 1 et ajustez la position pour diminuer l'espace entre les lettres. 
+Finalement, animez le nombre de copie ainsi que la position des deux lettres pour créer un effet de trois dimension. 
+</checklist>
+
+
+<dots></dots>
+
+
+<grostitre>Tutoriel 📚</grostitre>
+<p><doclink href="https://cmontmorency365-my.sharepoint.com/:v:/g/personal/lora_boisvert_cmontmorency_qc_ca/EbMOl5oFLH9Gn4RJDmORkLAB4jwlI9_UhCDf54XAoFG3og?e=fvFdK2">Lettre en 3D</doclink></p>
+<iframe src="https://cmontmorency365-my.sharepoint.com/personal/lora_boisvert_cmontmorency_qc_ca/_layouts/15/embed.aspx?UniqueId=9a970eb3-2c05-467f-9f84-490e639190b0&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen title="effet_3d_lettre.mp4"></iframe>
---
+++ 582-214MO/exercices_ae/09_modes_couleur/_index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @type     exercice
+ * @title    Modes de fusion
+ * @icon     images/thumb.jpg
+ * @abstract Cet exercice vous permettra de modifier les couleurs de votre texte grâce aux modes de fusion.
+ */
+?>
+
+<p class="spacer"> Cet exercice vous permettra de modifier les couleurs de votre texte grâce aux modes de fusion.  </p>
+<dots></dots>
+
+
+<grostitre>Étapes</grostitre>
+<checklist>
+Créer une nouvelle composition.
+Créez un fond avec minimum deux couleurs différentes. 
+Écrivez un texte avec l'outil texte et donnez-lui la même couleur qu'un des deux fonds. 
+Déplacez votre texte à cheval entre les deux fonds. 
+Sur votre calque de texte, allez modifier le Mode. Si vous n'avez pas accès aux modes, clic droit au-dessus des calques, colonnes, Modes.
+Appliquez le mode différence ou encore essayez les autres modes et déterminez lequel convient mieux à vos besoins!
+</checklist>
+
+
+<dots></dots>
+
+
+<grostitre>Tutoriel 📚</grostitre>
+<p><doclink href="https://cmontmorency365-my.sharepoint.com/:v:/g/personal/lora_boisvert_cmontmorency_qc_ca/Ea71EUXZ7udLojHUDr_LtgkB_19Ym_WslnziX7XaSC55Wg?e=bssAes">Modification des couleurs</doclink></p>
+<iframe src="https://cmontmorency365-my.sharepoint.com/personal/lora_boisvert_cmontmorency_qc_ca/_layouts/15/embed.aspx?UniqueId=4511f5ae-eed9-4be7-a231-d40ebfcbb609&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen title="modification_couleur_lettre.mp4"></iframe>
