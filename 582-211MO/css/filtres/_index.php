88e8bacaf000a905b79e361f708306b126c1445d
Prof-Inter-Web <112566312+Prof-Inter-Web@users.noreply.github.com>
2024-03-01 10:00:02 -0500

    filtres

--- 582-211MO/css/filtres/_index.php
+++ 582-211MO/css/filtres/_index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @type     article
  * @title    Filtres
@@ -16,18 +17,40 @@
 <grostitre>Filtres disponibles</grostitre>
 
 <ul>
-    <li><incode>none</incode> par défaut.</li>
-    <li><incode>blur()</incode> <em>(flou)</em> Valeur acceptant toutes les unités de mesure</li>
-    <li><incode>brightness()</incode> <em>(luminosité)</em> valeur en pourcentage</li>
-    <li><incode>contrast()</incode> <em>(contraste)</em> valeur en pourcentage</li>
-    <li><incode>drop-shadow()</incode> <em>(ombre portée)</em> même format que box-shadow</li>
-    <li><incode>grayscale()</incode> <em>(niveau de gris)</em> valeur en pourcentage</li>
-    <li><incode>hue-rotate()</incode> <em>(rotation de la teinte)</em> valeur en degrés</li>
-    <li><incode>invert()</incode> <em>(inverser)</em> valeur en pourcentage</li>
-    <li><incode>opacity()</incode> <em>(opacité)</em> valeur en pourcentage</li>
-    <li><incode>saturate()</incode> <em>(saturation)</em> valeur en pourcentage</li>
-    <li><incode>sepia()</incode> <em>(sépia)</em> valeur en pourcentage</li>
-    <li>etc.</li>
+  <li>
+    <incode>none</incode> par défaut.
+  </li>
+  <li>
+    <incode>blur()</incode> <em>(flou)</em> Valeur acceptant toutes les unités de mesure
+  </li>
+  <li>
+    <incode>brightness()</incode> <em>(luminosité)</em> valeur en pourcentage
+  </li>
+  <li>
+    <incode>contrast()</incode> <em>(contraste)</em> valeur en pourcentage
+  </li>
+  <li>
+    <incode>drop-shadow()</incode> <em>(ombre portée)</em> même format que box-shadow
+  </li>
+  <li>
+    <incode>grayscale()</incode> <em>(niveau de gris)</em> valeur en pourcentage
+  </li>
+  <li>
+    <incode>hue-rotate()</incode> <em>(rotation de la teinte)</em> valeur en degrés
+  </li>
+  <li>
+    <incode>invert()</incode> <em>(inverser)</em> valeur en pourcentage
+  </li>
+  <li>
+    <incode>opacity()</incode> <em>(opacité)</em> valeur en pourcentage
+  </li>
+  <li>
+    <incode>saturate()</incode> <em>(saturation)</em> valeur en pourcentage
+  </li>
+  <li>
+    <incode>sepia()</incode> <em>(sépia)</em> valeur en pourcentage
+  </li>
+  <li>etc.</li>
 </ul>
 
 <info>Toutes les valeurs en pourcentages <em>(%)</em> peuvent être exprimées en fractions. Par exemple, <incode>50%</incode> peut être écrit <incode>0.5</incode>.</info>
@@ -53,7 +76,7 @@
 
 <highlight lang="css">.element {
   filter: blur(5px) grayscale(100%);
-}</highlight>
+  }</highlight>
 
 <warning>Attention, si un filtre est mal défini, tous les autres filtres appliqués à un élément cesseront de fonctionner.</warning>
 
@@ -65,13 +88,11 @@
 
 <grostitre>backdrop-filter</grostitre>
 
-<p>La propriété <incode>backdrop-filter</incode> permet elle aussi d'ajouter des effets visuels, mais uniquement entre un élément et son arrière-plan. Il est donc nécessaire que l'élément en question soit ne soit pas entièrement opaque afin de pouvoir voir une partie de l'élément arrière-plan.</p>
+<p>La propriété <incode>backdrop-filter</incode> permet elle aussi d'ajouter des effets visuels, mais uniquement entre un élément et son arrière-plan. Il est donc nécessaire que l'élément en question ne soit pas entièrement opaque afin de pouvoir voir une partie de l'élément arrière-plan.</p>
 
 <p>Par exemple, un carré blanc ⚪️ avec une opacité de 30% afin de voir une partie de l'image en arrière-plan, soit <incode>background-color: rgba(255, 255, 255, 0.3);</incode>.</p>
 
 <codepen id="ZEjVmog" tab="result" height="540"></codepen>
-
-<warning>Au moment d’écrire ces lignes, cette propriété n’est pas encore supportée dans le navigateur Firefox. Il est donc préférable pour l’instant de séparer ses éléments et d’utiliser la propriété <incode>filter</incode>.</warning>
 
 <doclink href="https://developer.mozilla.org/fr/docs/Web/CSS/backdrop-filter">backdrop-filter</doclink>
 
@@ -81,7 +102,3 @@
 <exercice href="exercices/instagram/"></exercice>
 
 <dots></dots>
-
-
-
-
