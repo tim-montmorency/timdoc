4eba830dc908028b202f414f4766f8738afd7473
LoraBoisvert <95651493+LoraBoisvert@users.noreply.github.com>
2024-05-29 15:27:52 -0400

    Ajouts wiki 434

--- 582-434MO/wiki/_index.php
+++ 582-434MO/wiki/_index.php
@@ -9,56 +9,68 @@
 
 <wiki-page name="Cours 01">
 
-<h3>Plan de cours</h3>
+<grostitre>Plan de cours</grostitre>
 <doclink href="https://cmontmorency365-my.sharepoint.com/:b:/g/personal/lora_boisvert_cmontmorency_qc_ca/EXTXyncwrp9BsK6fpfriFQ8BJqTVzwK0fFBBiRxOienJnA?e=dZa1wP ">Plan de cours</doclink>
-    
-    <h3>Installation et base de Unity</h3>
-    <intlink href="../installation/compte/"></intlink>
-    <intlink href="../installation/telechargement/"></intlink>
-    <intlink href="../unity/introduction/"></intlink>
-    
-    <dots></dots>
-    <exercice href="../unity/introduction/exercices/intro/"></exercice>
-    <dots></dots>
-    <h3>Sprite, travail 1 et AR</h3>
+<dots></dots>
+
+<grostitre>Sprite et travail 1</grostitre>
     <intlink href="../unity/sprite/"></intlink>
     <intlink href="../consignes/travail1/"></intlink>
-    <intlink href="../realite-augmentee/introduction/"></intlink>
+    <dots></dots>
+
+    <grostitre>Installation et base de Unity</grostitre>
+   <p> <intlink href="../installation/compte/"></intlink>
+    <intlink href="../installation/telechargement/"></intlink>
+    <intlink href="../unity/introduction/"></intlink></p>
+  
+    <exercice href="../unity/introduction/exercices/intro/"></exercice>
+    <dots></dots>
+    
+    <grostitre>Test d'expériences VR</grostitre>
+    <intlink href="../installation/experiences/"></intlink>
     
 </wiki-page>
 
 
 <wiki-page name="Cours 02">
-<h3>Base de Unity</h3>
+<grostitre>Base de Unity</grostitre>
 <intlink href="../unity/composantes/"></intlink>
-<h3>Configuration</h3>
+<dots></dots>
+<grostitre>Configuration</grostitre>
 <intlink href="../installation/configuration/"></intlink>
 <intlink href="../installation/unity-remote/"></intlink>
-<h3>Réalité augmentée</h3>
+<dots></dots>
+<grostitre>Réalité augmentée</grostitre>
+<intlink href="../realite-augmentee/introduction/"></intlink>
 <intlink href="../realite-augmentee/detection/"></intlink>
-<h3>Calques et animation</h3>
+<dots></dots>
+<grostitre>Calques et animation</grostitre>
 <intlink href="../unity/sorting_layers/"></intlink>
 <intlink href="../unity/animation/"></intlink>
-<h3>Avancement du travail 1</h3>
+<dots></dots>
+<grostitre>Avancement du travail 1</grostitre>
 <intlink href="../consignes/travail1/"></intlink>
 
 </wiki-page>
 
 
 <wiki-page name="Cours 03">
-<h3>S'inscrire au Unity student plan</h3>
+<grostitre>S'inscrire au Unity student plan</grostitre>
 <doclink href="https://unity.com/products/unity-student">Student Plan</doclink>
-<h3>Code</h3>
+<dots></dots>
+<grostitre>Code</grostitre>
 <intlink href="../code/variables/"></intlink>
 <intlink href="../code/fonctions/"></intlink>
 <intlink href="../code/base/"></intlink>
 <intlink href="../code/evenements/"></intlink>
 <intlink href="../code/changement_scene/"></intlink>
 <intlink href="../code/quitter_jeu/"></intlink>
-<h3>Menus</h3>
+<dots></dots>
+<grostitre>Menus</grostitre>
 <intlink href="../unity/UI/"></intlink>
 <intlink href="../unity/font/"></intlink>
-<h3>Animation paramètre</h3>
+<dots></dots>
+<grostitre>Animation paramètre</grostitre>
 <intlink href="../unity/animation/"></intlink>
 
 </wiki-page>
@@ -73,7 +85,7 @@
 </wiki-page>
 
 <wiki-page name="Cours 05">
-<h3>Vérifier votre projet Unity en vue de l'évaluation sur la nomenclature</h3>
+<grostitre>Vérifier votre projet Unity en vue de l'évaluation sur la nomenclature</grostitre>
 <p><checklist>
     Classement des fichiers
     Classement des GameObject
@@ -81,51 +93,53 @@
     Nomenclature des GameObject
     Propreté des scripts
 </checklist></p>
-<h3>Débuter l'exercice</h3>
+<dots></dots>
+<grostitre>Débuter l'exercice</grostitre>
 <intlink href="../exercices/scene/"></intlink>
 </wiki-page>
 
 <wiki-page name="Cours 06">
-<h3>Évaluation méthode de travail</h3>
+<grostitre>Évaluation méthode de travail</grostitre>
 <p>Ouvrir votre projet 1 sur votre ordinateur prêt à être corrigé.</p>
-
-<h3>Continuer l'exercice</h3>
+<dots></dots>
+<grostitre>Continuer l'exercice</grostitre>
 <intlink href="../exercices/scene/"></intlink>
 
 </wiki-page>
 
 <wiki-page name="Cours 07">
-<h3>Sortie Zéro Latency</h3>
-<h3>Devoir:Continuer l'exercice 2</h3>
+<grostitre>Sortie Zéro Latency</grostitre>
+<dots></dots>
+<grostitre>Devoir:Continuer l'exercice 2</grostitre>
 <intlink href="../exercices/scene/"></intlink>
-
-<h3>Devoir: Commencer l'idéation du projet final</h3>
+<dots></dots>
+<grostitre>Devoir: Commencer l'idéation du projet final</grostitre>
 </wiki-page>
 
 <wiki-page name="10 octobre">
 
-<h3>Pas de cours</h3>
+<grostitre>Pas de cours</grostitre>
 <p>Journée pédagogique.</p>
 
 </wiki-page>
 
 <wiki-page name="Cours 08">
 
-<h3>Pas de cours</h3>
+<grostitre>Pas de cours</grostitre>
 <p>Remplacé par une conférence d'Aleo VR le 24 octobre de 11h40 à 13h + Rendez-vous en ligne pour discuter de vos idées de projet final.</p>
 
 </wiki-page>
 
 <wiki-page name="Cours 09">
 
-<h3>11h40 à 13h</h3>
+<grostitre>11h40 à 13h</grostitre>
 <p>Conférence Aleo VR</p>
-
-<h3>Présentation des plans de projet</h3>
-
-<h3>Prêt des caques oculus</h3>
-
-<h3>Début de la création du projet final</h3>
+<dots></dots>
+<grostitre>Présentation des plans de projet</grostitre>
+<dots></dots>
+<grostitre>Prêt des caques oculus</grostitre>
+<dots></dots>
+<grostitre>Début de la création du projet final</grostitre>
 <intlink href="../unity/github/"></intlink>
 <intlink href="../unity/quest/"></intlink>
 
@@ -141,7 +155,7 @@
 
 
 
-<h2>Discussion Optimisation</h2>
+<grostitre>Discussion Optimisation</grostitre>
 <p><checklist>
   Low poly
   Compression textures et ui
@@ -149,8 +163,8 @@
   Nombre et type de particules  
   Éclairage statique ou dynamique
 </checklist></p>
-
-<h2>Exercice</h2>
+<dots></dots>
+<grostitre>Exercice</grostitre>
 <intlink href="../exercices/interaction_vr/"></intlink>
 </wiki-page>
 
@@ -158,24 +172,27 @@
 
 
 <wiki-page name="Cours 11">
-<h3>Grimper</h3>
+<grostitre>Grimper</grostitre>
 <p><intlink href="../unity/deplacement_vr/"></intlink></p>
-<p><h3>Autres</h3></p>
+<dots></dots>
+<p><grostitre>Autres</grostitre></p>
 <p><intlink href="../unity/UI/#canvas-en-vr"></intlink>
 <intlink href="../unity/audio_vr/"></intlink>
 <intlink href="../unity/transitions/"></intlink>
 <intlink href="../unity/materiaux/"></intlink>
 <intlink href="../unity/miroir/"></intlink>
 <intlink href="../unity/particules/"></intlink></p>
-<p><h3>Discussion et recherche sur les shaders</h3>
-<h3>Rencontres pour voir les besoins individuels</h3></p>
+<dots></dots>
+<p><grostitre>Discussion et recherche sur les shaders</grostitre>
+<dots></dots>
+<grostitre>Rencontres pour voir les besoins individuels</grostitre></p>
 
 
 </wiki-page>
 
 <wiki-page name="14 novembre">
 
-<h3>Pas de cours</h3>
+<grostitre>Pas de cours</grostitre>
 <p>Journée de rattrapage.</p>
 
 </wiki-page>
@@ -190,19 +207,19 @@
 
 <intlink href="../unity/vr_mains/"></intlink>
 
-<p><h3>Saut</h3>
+<p><grostitre>Saut</grostitre>
 <intlink href="../unity/deplacement_vr/#sauter"></intlink></p>
-
-<p><h3>Plateforme en mouvement</h3>
+<dots></dots>
+<p><grostitre>Plateforme en mouvement</grostitre>
 <intlink href="../unity/deplacement_vr/#ascenseur"></intlink></p>
-
-<p><h3>Aléatoire</h3>
+<dots></dots>
+<p><grostitre>Aléatoire</grostitre>
 <intlink href="../code/base/#aleatoire"></intlink></p>
-
-<p><h3>Tir de projectile</h3>
+<dots></dots>
+<p><grostitre>Tir de projectile</grostitre>
 <intlink href="../unity/tir"></intlink></p>
-
-<p><h3>Physique Pont et porte</h3>
+<dots></dots>
+<p><grostitre>Physique Pont et porte</grostitre>
 <intlink href="../unity/physique"></intlink></p>
 
 
@@ -214,7 +231,7 @@
 
 <wiki-page name="Cours 14">
 
-<h3>Tests des projets finaux</h3>
+<grostitre>Tests des projets finaux</grostitre>
 <p>Contrôle de la qualité des projets et tests de présentation au Grand studio.</p>
 
 </wiki-page>
@@ -222,7 +239,7 @@
 
 <wiki-page name="Cours 15">
 
-<h3>Présentation finale des projets de VR</h3>
+<grostitre>Présentation finale des projets de VR</grostitre>
 <p>Au grand studio</p>
 
 </wiki-page>
