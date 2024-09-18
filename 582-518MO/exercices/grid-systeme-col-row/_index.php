1d7349745285e67a83532f9e254f5f2c4ce6ac33
kid-synthetique <mariemichelle.ouellet@gmail.com>
2024-09-18 19:26:44 -0400

    exercice grid les bases

---
+++ 582-518MO/exercices/grid-systeme-col-row/_index.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @type     exercice
+ * @title    CSS Grid - Système de grille de mise en page
+ * @icon     images/thumb.jpg
+ * @abstract Pour cet exercice recréer le résultat à l'aide des bases de grilles que vous avez vu il y a quelques minutes.
+ */
+?>
+
+<p>Pour cet exercice recréer le résultat à l'aide des bases de grilles que vous avez vu il y a quelques minutes.</p>
+
+
+
+
+
+
+<grostitre>Matériel</grostitre>
+<doclink href="https://codepen.io/tim-momo/pen/Bageppw">Pen de départ</doclink>
+
+
+<alert>Vous ne devez utiliser que les 4 premiers principes qu'on a vus précédemment à cet exercice: grid-template-columns, grid-template-rows, gap (ou column gap ou row-gap) et l'unité fr (ou autre unité au besoin, bien sur).</alert>
+
+
+<dots></dots>
+<grostitre>Grille 01</grostitre>
+<h3>Aperçu du résultat 👇</h3>
+<clipasset src="images/"></clipasset>
+<h3>Requis</h3>
+<checklist>
+    Faite un fork du pen de départ.
+    Renommez votre Pen "EXERC - CSS Grid base 01"
+    Recréez la grille de mise en page suivante à l'aide du système css grid. 
+</checklist>
+
+
+<dots></dots>
+<grostitre>Grille 02</grostitre>
+<h3>Aperçu du résultat 👇</h3>
+<clipasset src="images/"></clipasset>
+<h3>Requis</h3>
+<checklist>
+    Faite un fork du pen de départ.
+    Renommez votre Pen "EXERC - CSS Grid base 02"
+    Recréez la grille de mise en page suivante à l'aide du système css grid. 
+</checklist>
+
+
+
+
+<dots></dots>
+
+<grostitre>Notes de cours 📚</grostitre>
+
+<intlink href="../../css/grid/"></intlink>
+
