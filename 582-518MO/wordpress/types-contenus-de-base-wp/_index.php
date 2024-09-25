1296baa27cd352d943f08e4ddffc5ac0a602a7fe
kid-synthetique <mariemichelle.ouellet@gmail.com>
2024-09-25 17:28:55 -0400

    type contenu wp

---
+++ 582-518MO/wordpress/types-contenus-de-base-wp/_index.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * @type     article
+ * @title    Types de contenus dans WordPress
+ * @icon     ../medias/icon.webp
+ * @abstract Quels sont les types de contenu "built-in" dans WP
+ * @index    99
+ * @ref      web/wordpress
+ */
+?>
