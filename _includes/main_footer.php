81cfc263b0ca41c57fe6f0c0cb0b0095625567c4
kid-synthetique <mariemichelle.ouellet@gmail.com>
2024-10-05 02:01:04 -0400

    Crédit pour Simon Arnold pour les pages de projet web.

--- _includes/main_footer.php
+++ _includes/main_footer.php
@@ -1,7 +1,11 @@
         <!-- MAIN FOOTER SOF -->
         </main>
         <footer>
-            <div id="copyright"><?php echo date('Y'); ?> Collège Montmorency<br>Techniques d'intégration multimédia</div>
+           
+            <div id="copyright"> 
+            Ce contenu a été tiré en grande partie du site web de <a href="https://smnarnold.com/" target="_blank" rel="noopener noreferrer">Simon Arnold</a>.<br>
+            Collège Montmorency | Techniques d'intégration multimédia
+            </div>
         </footer>
         <script>app.mount('body');</script>
     </body>
