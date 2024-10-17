dc68cca2c4300d9814566a099798a1ace3396990
kid-synthetique <mariemichelle.ouellet@gmail.com>
2024-10-16 23:42:41 -0400

    exercice voyage

---
+++ 582-518MO/exercices/wp-voyage/_index.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @type     exercice
+ * @title    Wordpress - Voyage
+ * @icon     images/catan.jpeg
+ * @abstract Vous devez créer un modèle d'une page de conseil voyage avec les champs de base accessible dans WordPress.
+ */
+?>
+
+
+<p>Pour cet exercice vous et votre coéquipier (fictif) devez créer un microsite (pour l'exercice ce site ne sera qu'une page) permettant de présenter des conseils voyage.</p>
+
+
+<p>Votre coéquipier a conçu un mockup HTML sur CodePen, ainsi que le thème WordPress du site. Il compte maintenant sur vous pour le rendre dynamique dans&nbsp;WordPress.</p>
+
+
+<p>Aperçu du résultat 👇</p>
+<clipasset src="images/"></clipasset>
+
+
+
+<dots></dots>
+<grostitre>Matériel</grostitre>
+<doclink href="">Pen de départ</doclink>
+
+
+<h3 class="heading heading--h3" id="Médias 🖼️">Médias 🖼️</h3>
+<mediafile src="images/">Arrière plan du la section héro (image mise en avant)</mediafile>
+
+ 
+<dots></dots>
+<grostitre>Requis</grostitre>
+<checklist>Installez en local une copie en <a target="_blank" rel="noopener noreferrer" href="https://fr-ca.wordpress.org/download/">Français du Canada de WordPress</a> pour votre client.
+Installez le <a target="_blank" rel="noopener noreferrer" href="">thème réalisé par votre collègue</a>. Si vous l'avez installé et vous le cherchez, sachez que c'est le thème qui se nomme "Voyage".
+</checklist>
+
+      
+
+
+<dots></dots>
+<grostitre>Notes de cours 📚</grostitre>
+<intlink href='../wordpress/contenu-dans-modeles/'></intlink>
+<intlink href='../wordpress/images-dans-modeles/'></intlink>
+<intlink href="../../wordpress/posts-personnalises/"></intlink>
+<intlink href="../../wordpress/advanced-custom-fields/"></intlink>
+
