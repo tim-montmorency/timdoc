711cb8ce603fedbef987d5f33b6fa4cf064a3170
kid-synthetique <mariemichelle.ouellet@gmail.com>
2024-10-20 22:52:43 -0400

    exercice en temps et lieu

--- 582-518MO/wiki-2024/_index.php
+++ 582-518MO/wiki-2024/_index.php
@@ -1523,7 +1523,8 @@ va le faire ou pas. Si vous ne le faites pas, vous allez être pénalisés sur v
 
 
     <h3>Exercice en classe</h3>
-    <exercice href='../exercices/wp-valet-de-coeur/'></exercice>
+    <p>Lien à venir</p>
+    <!--<exercice href='../exercices/wp-valet-de-coeur/'></exercice>-->
 
     <p>+</p>
     <p>Je passe individuellement pour vérifier votre github et votre installation en local WP de votre projet de session.</p>
