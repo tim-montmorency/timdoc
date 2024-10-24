ce6e5fd0daf5569c92084f0279a1b3dcefab6f70
kid-synthetique <mariemichelle.ouellet@gmail.com>
2024-10-24 11:01:09 -0400

    remise 4 et remise 5 (PS)

---
+++ 582-518MO/projet/integration-pages-statiques/_index.php
@@ -0,0 +1,34 @@
+<?php 
+/**
+ * @type     article
+ * @title    Pages statiques HTML+SCSS+JS
+ * @icon     images/icon.svg
+ * @abstract Intégration des pages statiques HTML+SCSS+JS
+ * @index 28
+ */
+?>
+
+
+<grostitre>REMISE 5 – MISE EN PAGE STATIQUE</grostitre>
+
+<p>Cette étape se réalise en équipe.</p>
+
+<info>Voici la nouvelles liste des pages à monter :
+    <ol>
+        <li>Accueil : index.html</li>
+        <li>Liste des nouvelles : news-hub.html</li>
+        <li>Article d'une nouvelle : news-article.html (une seule)</li>
+        <li>Liste des services : services-hub.html</li>
+        <li>Page d'un seul des services : service.html (un seul)</li>
+        <li>Page de l'équipe : team.html</li>
+        <li>À propos de nous : about.html</li>
+        <li>Nous joindre: contact-us.html</li>
+        <li>Page erreur 404: 404.html</li>
+    </ol>
+</info>
+
+<warning>- Les pages doivent être responsives pour tous les formats d'écran. <br>- Les animation GSAP doivent avoir été intégrées.</warning>
+
+<dots></dots>
+<grostitre>Remise</grostitre>
+<p>Créez une branche nommée <em>pages-statiques</em>. À partir de la remise, n'y touchez plus et revenez travailler dans la branche <em>main</em>. Dans Teams/Projet&nbsp;Web/Devoir, veuillez remettre le lien vers votre répertoire github vers la branche <em>pages-statiques</em>.</p>
