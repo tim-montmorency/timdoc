2ca2c54bcd810443f84920f1d8c775ef9c185d87
kid-synthetique <mariemichelle.ouellet@gmail.com>
2024-11-27 18:37:51 -0500

    exercice films (non complet)

--- 582-518MO/exercices/vue-collection-films-1/_index.php
+++ 582-518MO/exercices/vue-collection-films-1/_index.php
@@ -2,7 +2,7 @@
 /**
  * @type     exercice
  * @title    Vue - Collection de films - Classes dynamiques
- * @icon     images/thumb.webp
+ * @icon     images/thumb.png
  * @abstract Vouc devez créer une petite application web vous permettant de classer votre collection de films, à voir ou déjà vu.
  */
 ?>
@@ -10,13 +10,14 @@
 <p>Dans le cadre de cet exercice, </p>
 
 <p>Aperçu du résultat 👇</p>
-<clipasset src="images/"></clipasset>
+<clipasset src="images/apercu-part1.mov"></clipasset>
 
 
 
 <dots></dots>
 <grostitre>Matériel</grostitre>
-<doclink href="">Pen de départ</doclink>
+<p>Faites toujours un fork du Pen de départ</p>
+<doclink href="https://codepen.io/tim-momo/pen/wBwvOMg">Pen de départ</doclink>
  
 
 
