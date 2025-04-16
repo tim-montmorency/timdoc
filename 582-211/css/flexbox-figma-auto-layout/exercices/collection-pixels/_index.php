319b5c717032031453ece5f169c9a82ead380849
kid-synthetique <mariemichelle.ouellet@gmail.com>
2025-04-16 01:21:41 -0400

    exercice collections

--- 582-211/css/flexbox-figma-auto-layout/exercices/collection-pixels/_index.php
+++ 582-211/css/flexbox-figma-auto-layout/exercices/collection-pixels/_index.php
@@ -11,7 +11,7 @@
 
 <dots></dots>
 <grostitre>Aperçu du résultat 👇</grostitre>
-<img src="./images/resultat-attendu-desktop.png" alt="">
+<img src="./images/resultat-attendu-desktop.png" alt="" style="box-shadow: 0 0 10px rgba(0,0,0,0.2);">
 
 
 <dots></dots>
@@ -24,16 +24,25 @@
 
 
 <dots></dots>
-
-
 <grostitre>Requis</grostitre>
 
-<checklist>
+<!-- <checklist>
   Démarrez dans Figma.
   Grouper ce qu'il faut ensemble.
   Activer l'auto-layout sur tous les éléments nécessaire et ajuster les propriétés de l'auto-layout.
   Lorsque complété, explorer le code Flexbox généré via le DevMode de Figma.
   Intégrer le tout en HTML+CSS dans VS Code.
+</checklist> -->
+
+
+<checklist>
+  Ouvrir le fichier Figma (voir <a href="#materiel">Matériel</a>). File / Save local copy. Importez ensuite ce fichier .fig dans votre compte Figma personnel.
+  D'abord faites des groupes d'éléments, pour que ce soit plus facile de travailler avec les auto-layouts. <br> <em>Vous pouvez vous inspirer de la structure de la carte de l'exercice de la salle de conférence de la semaine dernière.</em>
+  Après avoir regroupé les éléments qui vont ensemble, préparer les auto-layouts: entête, nav, section héros, une des carte (vous pourrez réutiliser les styles de la première carte sur les autres), groupes de liens, groupes de boutons et autres groupes d'éléments. À vous d'explorer.
+  Analyser le code via Dev Mode.
+  Ouvrir le <em>dossier de départ</em> (voir <a href="#materiel">Matériel</a>) dans VS Code le contenu est déjà dans le HTML. Par contre vous aurez à créer des balises pour regrouper les éléments. 
+  Utilisez l'extention <em>Figma for VS Code</em> pour accéder au CSS généré par le DevMode directement dans VS Code.
+  Utilisez le CSS généré par Dev&nbsp;Mode pour vous guider et faire la mise en page et styliser les éléments.
 </checklist>
 
 
